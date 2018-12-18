d5b1aff0961ea58238abfd68bcf64899841b978b
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-12-18 16:35:01 -0600

    removing converter utility

--- common-api/src/main/java/org/pmiops/workbench/db/model/CommonStorageEnums.java
+++ common-api/src/main/java/org/pmiops/workbench/db/model/CommonStorageEnums.java
@@ -2,7 +2,6 @@ package org.pmiops.workbench.db.model;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.ImmutableBiMap;
-import org.pmiops.workbench.model.DataAccessLevel;
 import org.pmiops.workbench.model.Domain;
 
 public class CommonStorageEnums {
@@ -46,22 +45,6 @@ public class CommonStorageEnums {
 
   public static Domain domainIdToDomain(String domainId) {
     return DOMAIN_ID_MAP.inverse().get(domainId);
-  }
-
-  private static final BiMap<DataAccessLevel, Short> CLIENT_TO_STORAGE_DATA_ACCESS_LEVEL =
-      ImmutableBiMap.<DataAccessLevel, Short>builder()
-          .put(DataAccessLevel.UNREGISTERED, (short) 0)
-          .put(DataAccessLevel.REGISTERED, (short) 1)
-          .put(DataAccessLevel.PROTECTED, (short) 2)
-          .put(DataAccessLevel.REVOKED, (short) 3)
-          .build();
-
-  public static DataAccessLevel dataAccessLevelFromStorage(Short level) {
-    return CLIENT_TO_STORAGE_DATA_ACCESS_LEVEL.inverse().get(level);
-  }
-
-  public static Short dataAccessLevelToStorage(DataAccessLevel level) {
-    return CLIENT_TO_STORAGE_DATA_ACCESS_LEVEL.get(level);
   }
 
 }
