0f73dc4ebf3d1a5fb498b3aa21ccfd52253d4efa
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2018-12-19 10:03:29 -0600

    [risk=no] RW-1763 trimming cdr verison table (#16)
    * trimming cdr verison table
    
    * correcting indentation
    
    * no data access level:
    
    * removing data access level from test
    
    * removing converter utility
    
    * removing converter utility
    
    * removing ref in test
    
    * removing ref in test
    
    * fixing test
    
    * fixing test

--- common-api/src/main/java/org/pmiops/workbench/db/model/CdrVersion.java
+++ common-api/src/main/java/org/pmiops/workbench/db/model/CdrVersion.java
@@ -10,7 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.pmiops.workbench.model.DataAccessLevel;
 
 @Entity
 @Table(name = "cdr_version")
@@ -19,13 +18,8 @@ public class CdrVersion {
   private long cdrVersionId;
   private boolean isDefault;
   private String name;
-  private Short dataAccessLevel;
-  private short releaseNumber;
-  private String bigqueryProject;
-  private String bigqueryDataset;
   private Timestamp creationTime;
   private int numParticipants;
-  private String cdrDbName;
   private String publicDbName;
 
   @Id
@@ -57,51 +51,6 @@ public class CdrVersion {
     this.name = name;
   }
 
-  @Column(name = "data_access_level")
-  public Short getDataAccessLevel() {
-    return dataAccessLevel;
-  }
-
-  public void setDataAccessLevel(Short dataAccessLevel) {
-    this.dataAccessLevel = dataAccessLevel;
-  }
-
-  @Transient
-  public DataAccessLevel getDataAccessLevelEnum() {
-    return CommonStorageEnums.dataAccessLevelFromStorage(getDataAccessLevel());
-  }
-
-  public void setDataAccessLevelEnum(DataAccessLevel dataAccessLevel) {
-    setDataAccessLevel(CommonStorageEnums.dataAccessLevelToStorage(dataAccessLevel));
-  }
-
-  @Column(name = "release_number")
-  public short getReleaseNumber() {
-    return releaseNumber;
-  }
-
-  public void setReleaseNumber(short releaseNumber) {
-    this.releaseNumber = releaseNumber;
-  }
-
-  @Column(name = "bigquery_project")
-  public String getBigqueryProject() {
-    return bigqueryProject;
-  }
-
-  public void setBigqueryProject(String bigqueryProject) {
-    this.bigqueryProject = bigqueryProject;
-  }
-
-  @Column(name = "bigquery_dataset")
-  public String getBigqueryDataset() {
-    return bigqueryDataset;
-  }
-
-  public void setBigqueryDataset(String bigqueryDataset) {
-    this.bigqueryDataset = bigqueryDataset;
-  }
-
   @Column(name = "creation_time")
   public Timestamp getCreationTime() {
     return creationTime;
@@ -120,11 +69,6 @@ public class CdrVersion {
     this.numParticipants = numParticipants;
   }
 
-  @Column(name = "cdr_db_name")
-  public String getCdrDbName() { return cdrDbName; }
-
-  public void setCdrDbName(String cdrDbName) { this.cdrDbName = cdrDbName; }
-
   @Column(name = "public_db_name")
   public String getPublicDbName() { return publicDbName; }
 
@@ -132,8 +76,8 @@ public class CdrVersion {
 
   @Override
   public int hashCode() {
-    return Objects.hash(cdrVersionId, isDefault, name, dataAccessLevel, releaseNumber,
-        bigqueryProject, bigqueryDataset, creationTime, numParticipants, publicDbName, cdrDbName);
+    return Objects.hash(cdrVersionId, isDefault, name,
+          creationTime, numParticipants, publicDbName);
   }
 
   @Override
@@ -145,13 +89,9 @@ public class CdrVersion {
     return new EqualsBuilder().append(this.cdrVersionId, that.cdrVersionId)
         .append(this.isDefault, that.isDefault)
         .append(this.name, that.name)
-        .append(this.dataAccessLevel, that.dataAccessLevel)
-        .append(this.releaseNumber, that.releaseNumber)
-        .append(this.bigqueryProject, that.bigqueryProject)
         .append(this.creationTime, that.creationTime)
         .append(this.numParticipants, that.numParticipants)
         .append(this.publicDbName, that.publicDbName)
-        .append(this.cdrDbName, that.cdrDbName)
         .build();
   }
 }
