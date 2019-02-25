96f8c334f2f22835f25bff9533f00375f028fe02
mhigbyflowers <mhigbyflowers@gmail.com>
2019-02-25 15:41:27 -0600

    spec

--- public-ui/src/app/highlight-search/highlight-search.component.spec.ts
+++ public-ui/src/app/highlight-search/highlight-search.component.spec.ts
@@ -1,7 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import {DebugElement} from '@angular/core';
-import {By} from '@angular/platform-browser';
 import { HighlightSearchComponent } from './highlight-search.component';
 
 describe('HighlightSearchComponent', () => {
