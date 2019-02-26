2d0574770a725ef314e37d6789ab86b7f820b3f6
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-02-26 09:26:17 -0600

    changing api url

--- public-ui/src/environments/environment.prod.ts
+++ public-ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  displayTag: 'PRod',
-  publicApiUrl: 'https://public.api.researchallofus.org',
+  displayTag: '',
+  publicApiUrl: 'https://api-dot-aou-db-prod.appspot.com',
   workbenchUrl: 'https://workbench.researchallofus.org',
   clientId: '684273740878-d7i68in5d9hqr6n9mfvrdh53snekp79f.apps.googleusercontent.com',
   tcellappid: 'AoUProd-35j28',
