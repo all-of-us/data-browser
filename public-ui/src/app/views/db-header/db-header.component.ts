18467e09a249308b88e3cce2c8375031c949b9f8
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2019-03-01 16:25:32 -0600

    Mhigbyflowers/rebrand header - 7 (#51)
    * header created and stubbed out
    
    * scrape reHub head and modify for db
    
    * unused css
    
    * c2save
    
    * styled fullwidth
    
    * icon assets
    
    * full width sub menu
    
    * top padding for main
    
    * add search assest
    
    * click to open + mobile menu
    
    * modifed reseach hub css to fit db
    
    * clean up
    
    * cleanup
    
    * image scale

--- public-ui/src/app/views/db-header/db-header.component.ts
+++ public-ui/src/app/views/db-header/db-header.component.ts
@@ -3,15 +3,21 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-db-header',
   templateUrl: './db-header.component.html',
-  styleUrls: ['./db-header.component.css']
+  styleUrls: ['./db-header.component.css', '../../styles/template.css']
 })
 export class DbHeaderComponent implements OnInit {
   @Input() noMenu = false;
-  logo = '/assets/db-images/All_Of_Us_Logo.svg';
-  dbLogo = '/assets/db-images/Data_Browser_Logo.svg';
+  topMenuItemToggle = false;
+  openTopMenu = false;
+  openAbout = false;
+  openDataTools = false;
+  openSearch = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  public toggleTopMenu() {
+    this.openTopMenu = !this.openTopMenu;
+  }
 }
