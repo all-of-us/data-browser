474d9ff3e92de56729788f5c7609bed03d4fd985
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-03-11 14:13:56 -0500

    addressing codacy

--- public-ui/src/app/data-browser/chart/chart.component.ts
+++ public-ui/src/app/data-browser/chart/chart.component.ts
@@ -390,13 +390,16 @@ export class ChartComponent implements OnChanges {
       if (this.analysis && this.analysis.analysisId === this.dbc.GENDER_ANALYSIS_ID) {
         color = this.dbc.GENDER_COLORS[a.stratum2];
       }
-      if (this.surveyAnalysis && this.surveyAnalysis.analysisId === this.dbc.SURVEY_GENDER_ANALYSIS_ID) {
+      if (this.surveyAnalysis &&
+        this.surveyAnalysis.analysisId === this.dbc.SURVEY_GENDER_ANALYSIS_ID) {
         color = this.dbc.GENDER_COLORS[a.stratum5];
       }
-      if (this.surveyAnalysis && this.surveyAnalysis.analysisId === this.dbc.SURVEY_GENDER_IDENTITY_ANALYSIS_ID) {
+      if (this.surveyAnalysis &&
+        this.surveyAnalysis.analysisId === this.dbc.SURVEY_GENDER_IDENTITY_ANALYSIS_ID) {
         color = this.dbc.GENDER_IDENTITY_COLORS[a.stratum5];
       }
-      if (this.analysis && this.analysis.analysisId === this.dbc.GENDER_IDENTITY_ANALYSIS_ID) {
+      if (this.analysis &&
+        this.analysis.analysisId === this.dbc.GENDER_IDENTITY_ANALYSIS_ID) {
         color = this.dbc.GENDER_IDENTITY_COLORS[a.stratum2];
       }
       data.push({
