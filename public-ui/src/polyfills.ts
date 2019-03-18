2088d78221f3c654636e87903f94f7aade7b9a2d
mhigbyflowers <mhigbyflowers@gmail.com>
2019-03-18 11:12:54 -0500

    classlist.js not needed

--- public-ui/src/polyfills.ts
+++ public-ui/src/polyfills.ts
@@ -34,7 +34,7 @@ import 'core-js/es6/string';
 import 'core-js/es6/symbol';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js';  // Run `npm install --save classlist.js`.
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
