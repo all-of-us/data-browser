2a2fbedeaf3448ba7b1e1f1d201b2e226616a383
mhigbyflowers <mhigbyflowers@gmail.com>
2019-03-20 09:09:09 -0500

    wip

--- public-ui/src/app/components/db-no-results/db-no-results.component.ts
+++ public-ui/src/app/components/db-no-results/db-no-results.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnChanges, Input } from '@angular/core';
-
+import { Component, Input, OnChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataBrowserService, DomainInfosAndSurveyModulesResponse, ApiModule } from 'publicGenerated';
 @Component({
   selector: 'app-db-no-results',
   templateUrl: './db-no-results.component.html',
@@ -7,11 +8,30 @@ import { Component, OnChanges, Input } from '@angular/core';
 })
 export class DbNoResultsComponent implements OnChanges {
   @Input() searchText: string;
-  constructor() { }
+  results;
+  loading;
+  prevSearchText: string;
+  constructor(private api: DataBrowserService, private router: Router) { }
 
   ngOnChanges() {
-    console.log(this.searchText);
-    
+    this.prevSearchText = localStorage.getItem('searchText');
+    if (this.searchText) {
+      this.loading = true;
+      this.api.getDomainSearchResults(this.searchText.value).subscribe(results =>{
+        this.results = results;
+        console.log(this.results, this.searchText.value);
+        this.loading=false;
+      });
+    }
   }
+
+  public viewEhrDomain(r) {
+    console.log(r);
+    
+    localStorage.setItem('ehrDomain', JSON.stringify(r));
+    localStorage.setItem('searchText', this.prevSearchText);
+    // this.router.navigateByUrl('ehr/' + r.domain.toLowerCase());
+  }
+
 
 }
