0b5d3bd4f3f71af0b41c7eab28d01c436d1e11ed
mhigbyflowers <mhigbyflowers@gmail.com>
2019-03-20 10:13:48 -0500

    added spinner component

---
+++ public-ui/src/app/components/db-spinner/db-spinner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-db-spinner',
+  templateUrl: './db-spinner.component.html',
+  styleUrls: ['./db-spinner.component.css']
+})
+export class DbSpinnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
