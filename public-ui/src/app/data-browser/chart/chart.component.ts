e6ae38d36eba15ee5e7a59fba4e6924fffa4491b
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-03-22 15:42:29 -0500

    update pregnancy value chart

--- public-ui/src/app/data-browser/chart/chart.component.ts
+++ public-ui/src/app/data-browser/chart/chart.component.ts
@@ -655,8 +655,8 @@ export class ChartComponent implements OnChanges, AfterViewInit {
     const data = [];
     const cats = [];
 
-    let results = this.analysis.results.concat([]);
-    let seperateResults = {};
+    const results = this.analysis.results.concat([]);
+    const seperateResults = {};
 
     for (const a of results) {
       if (a.stratum4 in seperateResults) {
@@ -665,7 +665,7 @@ export class ChartComponent implements OnChanges, AfterViewInit {
         seperateResults[a.stratum4] = a.countValue;
       }
     }
-    for (const k in seperateResults) {
+    for (let k in seperateResults) {
       data.push({name: k, y: seperateResults[k], thisCtrl: this});
       data.push({name: 'No', y: this.participantCount - seperateResults[k], thisCtrl: this});
     }
