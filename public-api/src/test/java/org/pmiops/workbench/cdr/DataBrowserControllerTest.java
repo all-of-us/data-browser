ec719e6cd2e838d785ccc3e4ab5152aaaca8176a
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-03-26 10:08:23 -0500

    fix test issue

--- public-api/src/test/java/org/pmiops/workbench/cdr/DataBrowserControllerTest.java
+++ public-api/src/test/java/org/pmiops/workbench/cdr/DataBrowserControllerTest.java
@@ -16,6 +16,7 @@ import org.pmiops.workbench.cdr.dao.AchillesAnalysisDao;
 import org.pmiops.workbench.cdr.dao.AchillesResultDao;
 import org.pmiops.workbench.cdr.dao.AchillesResultDistDao;
 import org.pmiops.workbench.cdr.dao.ConceptDao;
+import org.pmiops.workbench.cdr.dao.CriteriaDao;
 import org.pmiops.workbench.cdr.dao.ConceptRelationshipDao;
 import org.pmiops.workbench.cdr.dao.ConceptService;
 import org.pmiops.workbench.cdr.dao.DomainInfoDao;
@@ -371,6 +372,8 @@ public class DataBrowserControllerTest {
     @Autowired
     private ConceptDao conceptDao;
     @Autowired
+    private CriteriaDao criteriaDao;
+    @Autowired
     private CdrVersionDao cdrVersionDao;
     @Autowired
     ConceptRelationshipDao conceptRelationshipDao;
@@ -397,7 +400,7 @@ public class DataBrowserControllerTest {
     public void setUp() {
         saveData();
         ConceptService conceptService = new ConceptService(entityManager, conceptDao);
-        dataBrowserController = new DataBrowserController(conceptService, conceptDao, domainInfoDao,
+        dataBrowserController = new DataBrowserController(conceptService, conceptDao, criteriaDao, domainInfoDao,
             surveyModuleDao, achillesResultDao, achillesAnalysisDao, achillesResultDistDao, entityManager,
             () -> cdrVersion);
     }
