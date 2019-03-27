a546a3f705b12dfa312728cc4660f8e546650b61
mhigbyflowers <mhigbyflowers@gmail.com>
2019-03-27 07:16:49 -0500

    adding loading to source-tree

--- public-ui/src/app/components/source-tree/source-tree.component.ts
+++ public-ui/src/app/components/source-tree/source-tree.component.ts
@@ -4,17 +4,24 @@ import { Component, Input, OnChanges } from '@angular/core';
 @Component({
   selector: 'app-source-tree',
   template:
-    `<app-recursive-tree
+    `<div *ngIf="loading" class="loading-dots">
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+    </div>
+    <app-recursive-tree *ngIf="!loading"
       [node]="rootNode"></app-recursive-tree>
   `,
   styleUrls: ['./source-tree.component.css']
 })
 export class SourceTreeComponent implements OnChanges {
   @Input() rootNode: object;
+  @Input() loading = true;
   open = true;
 
   constructor() {
-    this.rootNode = null;
+  
   }
 
   ngOnChanges() {
