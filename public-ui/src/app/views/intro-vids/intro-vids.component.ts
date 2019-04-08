a53e8eed03073c6dc776eb56cd32274125685688
mhigbyflowers <mhigbyflowers@gmail.com>
2019-04-08 10:38:05 -0500

    wip

--- public-ui/src/app/views/intro-vids/intro-vids.component.ts
+++ public-ui/src/app/views/intro-vids/intro-vids.component.ts
@@ -3,10 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-intro-vids',
   templateUrl: './intro-vids.component.html',
-  styleUrls: ['./intro-vids.component.css']
+  styleUrls: ['../../styles/template.css', './intro-vids.component.css']
 })
 export class IntroVidsComponent implements OnInit {
 
+  videos = [{
+    title: 'Participant Intro',
+    src: [{
+      url: '/assets/videos/big_buck_bunny.mp4',
+      type: 'video/mp4'
+    },
+    {
+      url: '/assets/videos/big_buck_bunny.ogv',
+      type: 'video/ogg'
+    },
+    {
+      url: '/assets/videos/big_buck_bunny.webm',
+      type: 'video/webm'
+    }]
+  },
+  {
+    title: 'Researcher Intro',
+    src: [{
+      url: '/assets/videos/big_buck_bunny.mp4',
+      type: 'video/mp4'
+    },
+    {
+      url: '/assets/videos/big_buck_bunny.ogv',
+      type: 'video/ogg'
+    },
+    {
+      url: '/assets/videos/big_buck_bunny.webm',
+      type: 'video/webm'
+    }]
+  },
+  ];
   constructor() { }
 
   ngOnInit() {
