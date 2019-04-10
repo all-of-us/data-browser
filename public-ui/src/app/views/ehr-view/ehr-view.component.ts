95c01762f2c159687df680cdded586733df2ac61
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2019-04-10 10:52:26 -0500

    Removing race/ethnicity tab on the graphs (#234)

--- public-ui/src/app/views/ehr-view/ehr-view.component.ts
+++ public-ui/src/app/views/ehr-view/ehr-view.component.ts
@@ -115,10 +115,9 @@ export class EhrViewComponent implements OnInit, OnDestroy {
       this.totalParticipants = this.ehrDomain.participantCount;
       if (this.ehrDomain.name.toLowerCase() === 'measurements') {
         this.graphButtons = ['Values', 'Biological Sex',
-          'Gender Identity', 'Race / Ethnicity', 'Age', 'Sources'];
+          'Gender Identity', 'Age', 'Sources'];
       } else {
-        this.graphButtons = ['Biological Sex', 'Gender Identity',
-          'Race / Ethnicity', 'Age', 'Sources'];
+        this.graphButtons = ['Biological Sex', 'Gender Identity', 'Age', 'Sources'];
       }
       this.initSearchSubscription = this.searchDomain(this.prevSearchText)
         .subscribe(results => this.searchCallback(results));
