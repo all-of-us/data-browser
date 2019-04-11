321d4c27fc7426d0f3ed8a0045ae3eca768897f8
mhigbyflowers <mhigbyflowers@gmail.com>
2019-04-11 13:12:24 -0500

    wip

--- public-ui/src/app/components/recursive-tree/recursive-tree.component.ts
+++ public-ui/src/app/components/recursive-tree/recursive-tree.component.ts
@@ -9,12 +9,16 @@ import { DataBrowserService } from '../../../publicGenerated/api/dataBrowser.ser
 })
 export class RecursiveTreeComponent implements OnChanges, OnDestroy {
   @Input() node: any;
-  opened = false;
+  @Input() opened: boolean;
+  @Input() childIndex: number;
   loading = true;
+  subOpened = false;
   private subscriptions: ISubscription[] = [];
   constructor(private api: DataBrowserService) { }
 
   ngOnChanges() {
+    console.log(this.opened, this.node);
+
     if (this.node && this.node.group) {
       this.subscriptions.push(this.api.getCriteriaChildren(this.node.id)
         .subscribe({
@@ -25,12 +29,18 @@ export class RecursiveTreeComponent implements OnChanges, OnDestroy {
             }
           }
         })
-        );
-      }
+      );
+    }
   }
   ngOnDestroy() {
     for (const s of this.subscriptions) {
       s.unsubscribe();
+    }
+  }
+  openUp(i, node, nodes) {
+    this.childIndex = nodes.findIndex(x => x);
+    if (i === this.childIndex) {
+      this.subOpened = !this.subOpened;
     }
   }
 
--- public-ui/src/app/components/source-tree/source-tree.component.ts
+++ public-ui/src/app/components/source-tree/source-tree.component.ts
@@ -5,22 +5,16 @@ import { Component, Input, OnChanges } from '@angular/core';
   selector: 'app-source-tree',
   template:
     `<app-db-spinner [loading]="loading" [dots]="true"></app-db-spinner>
-    <app-recursive-tree *ngIf="!loading"
-      [node]="rootNode"></app-recursive-tree>
+    <span class="tree-row"
+    [ngClass]="[rootNode.group ? 'hasChildren':'noChildren' , opened ? 'opened':'']"
+    (click)="opened=!opened"><span>{{rootNode.name}}</span> <span class="blue-text">
+    ({{rootNode.count | number}})</span></span>
+    <app-recursive-tree *ngIf="!loading" [opened]="opened" [node]="rootNode"></app-recursive-tree>
   `,
   styleUrls: ['./source-tree.component.css']
 })
-export class SourceTreeComponent implements OnChanges {
+export class SourceTreeComponent {
   @Input() rootNode: object;
   @Input() loading = true;
-  open = true;
-
-  constructor() {
-  }
-
-  ngOnChanges() {
-  }
-
-
-
+  opened = false;
 }
