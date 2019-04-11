72df0a02a26d7c5b52832c4a1ab6edaf3b4e7c23
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2019-04-11 13:52:42 -0500

    [DB-526] [risk=low] updating help text on q2 (#245)
    * updating help text on q2
    
    * addressing codacy

--- public-ui/src/app/utils/tooltip.service.ts
+++ public-ui/src/app/utils/tooltip.service.ts
@@ -116,6 +116,12 @@ export class TooltipService {
     medical concept in the hierarchy, but never more than once for the same concept. As a result,
      the sum total of counts for the descendants in the tree may be more than the count
      returned for the top level concept.`;
+  raceEthnicityQuestionHelpText = `The All of Us Research Program defines race as a person’s
+  self-identification with one or more social groups. An individual can report as White, Black or
+  African American, Asian, American Indian and Alaska Native, Native Hawaiian and Other
+  Pacific Islander, or some other race. Survey respondents may report multiple races.
+  Ethnicity determines whether a person is of Hispanic origin or not. Hispanics may report
+  as any race.`;
   constructor() { }
 
 }
