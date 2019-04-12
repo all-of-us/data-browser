f4a81f420e618d01054a6366a36f9308ece5849b
mhigbyflowers <mhigbyflowers@gmail.com>
2019-04-12 15:55:33 -0500

    change back

--- public-ui/src/app/components/recursive-tree/recursive-tree.component.ts
+++ public-ui/src/app/components/recursive-tree/recursive-tree.component.ts
@@ -40,7 +40,7 @@ export class RecursiveTreeComponent implements OnChanges, OnDestroy {
       const item = nodes[i];
       if (item === node && i === index) {
         this.childIndex = i;
-        this.subOpened = true;
+        this.subOpened = !this.subOpened;
       }
     }
   }
