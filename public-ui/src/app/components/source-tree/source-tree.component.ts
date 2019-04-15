96ebe9fa12aea265d5a879d1c4387461a83a8b6d
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2019-04-15 11:28:17 -0500

    [risk=no][DB-502]simplified tree ui (#260)
    * reverted back an simplifed tree ui
    
    * bug fix

--- public-ui/src/app/components/source-tree/source-tree.component.ts
+++ public-ui/src/app/components/source-tree/source-tree.component.ts
@@ -1,22 +1,22 @@
-import { Component, Input, OnChanges } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-source-tree',
   template:
-    `<app-db-spinner [loading]="loading" [dots]="true"></app-db-spinner>
-    <span class="tree-row"
-    [ngClass]="[rootNode.group ? 'hasChildren':'noChildren' , opened ? 'opened':'']"
-    (click)="$event.stopPropagation();opened=!opened">
-      <span>{{rootNode.name}}</span>
-    </span>
-    <span class="blue-text">({{rootNode.count | number}})</span>
-      <app-recursive-tree *ngIf="!loading" [opened]="opened" [node]="rootNode">
-      </app-recursive-tree>`,
+    `<app-recursive-tree
+      [node]="rootNode" [opened]="true"></app-recursive-tree>`,
   styleUrls: ['./source-tree.component.css']
 })
 export class SourceTreeComponent {
-  @Input() rootNode;
-  @Input() loading = true;
-  opened = true;
+  @Input() rootNode: object;
+  @Input() loading: boolean;
+  open = true;
+
+  constructor() {
+    this.rootNode = null;
+  }
+
+
+
+
 }
