06f800f17869e5021ca0e2fe1a5d7434a994b000
mhigbyflowers <mhigbyflowers@gmail.com>
2019-04-19 09:50:45 -0500

    global beta banner

--- public-ui/src/app/components/beta-bar/beta-bar.component.ts
+++ public-ui/src/app/components/beta-bar/beta-bar.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-beta-bar',
   templateUrl: './beta-bar.component.html',
   styleUrls: ['./beta-bar.component.css']
 })
 export class BetaBarComponent implements OnInit {
-
+  allOfUsUrl: string;
   constructor() { }
 
   ngOnInit() {
+    this.allOfUsUrl = environment.researchAllOfUsUrl;
   }
 
 }
