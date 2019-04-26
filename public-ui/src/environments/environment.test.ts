305a961b3eec6ca3e9b388a20cf4718c9b1a9a16
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-04-26 16:00:54 -0500

    adding gtm id

--- public-ui/src/environments/environment.test.ts
+++ public-ui/src/environments/environment.test.ts
@@ -4,6 +4,7 @@ export const environment = {
   ...testEnvironmentBase,
   displayTag: 'Test',
   debug: false,
+  gtmId: 'GTM-NQ9XDTW',
   gtmAuth: 'kMy-ZUX8XsBfuanMZzP_5A',
   gtmPreview: 'env-25'
 };
