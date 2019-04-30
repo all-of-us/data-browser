b8624bfbaf81da0e02d79ea76c91354a2baad20b
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-04-30 10:35:30 -0500

    adding prefix data browser to db events

--- public-ui/src/environments/environment.ts
+++ public-ui/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   displayTag: 'Local->Test',
   workbenchUrl: 'http://localhost:4200',
   debug: true,
-  gtmId: "GTM-NQ9XDTW",
-  gtmAuth: "kMy-ZUX8XsBfuanMZzP_5A",
-  gtmPreview: "env-25"
+  gtmId: 'GTM-NQ9XDTW',
+  gtmAuth: 'kMy-ZUX8XsBfuanMZzP_5A',
+  gtmPreview: 'env-25'
 };
