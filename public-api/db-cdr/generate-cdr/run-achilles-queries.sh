3db787770f04b77e4e69a3cbd54c0c08a1e186cb
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-05-07 16:21:01 -0500

    get unit from similar mapping table we created

--- public-api/db-cdr/generate-cdr/run-achilles-queries.sh
+++ public-api/db-cdr/generate-cdr/run-achilles-queries.sh
@@ -57,17 +57,13 @@ if [[ "$tables" == *"_mapping_"* ]]; then
     echo "CREATE VIEWS - v_ehr_measurement"
     bq --quiet --project=$BQ_PROJECT query --nouse_legacy_sql \
     "CREATE OR REPLACE VIEW \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.v_ehr_measurement\` AS
-    select m.* from \`${BQ_PROJECT}.${BQ_DATASET}.measurement\` m join \`${BQ_PROJECT}.${BQ_DATASET}._mapping_measurement\` mm
+    select m.measurement_id, m.person_id, m.measurement_concept_id, m.measurement_date, m.measurement_datetime, m.measurement_type_concept_id,
+     m.operator_concept_id, m.value_as_number. m.value_as_concept_id, (case when suc.destination_unit_concept is not null then suc.destination_unit_concept else m.unit_concept_id end) as unit_concept_id,
+     m.range_low, m.range_high, m.provider_id, m.visit_occurrence_id, m.measurement_source_value, m.measurement_source_concept_id, m.unit_source_value, m.value_source_value
+     from \`${BQ_PROJECT}.${BQ_DATASET}.measurement\` m join \`${BQ_PROJECT}.${BQ_DATASET}._mapping_measurement\` mm
     on m.measurement_id = mm.measurement_id
+    left outer join `all-of-us-ehr-dev.test_vocabulary_ppi.similar_unit_concepts` suc on suc.source_unit_concept = m.unit_concept_id
     where mm.src_dataset_id=(select distinct src_dataset_id from \`${BQ_PROJECT}.${BQ_DATASET}._mapping_measurement\` where src_dataset_id like '%ehr%')
-    and (m.measurement_concept_id > 0 or m.measurement_source_concept_id > 0)"
-
-    echo "CREATE VIEWS - v_rdr_measurement"
-    bq --quiet --project=$BQ_PROJECT query --nouse_legacy_sql \
-    "CREATE OR REPLACE VIEW \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.v_rdr_measurement\` AS
-    select m.* from \`${BQ_PROJECT}.${BQ_DATASET}.measurement\` m join \`${BQ_PROJECT}.${BQ_DATASET}._mapping_measurement\` mm
-    on m.measurement_id = mm.measurement_id
-    where mm.src_dataset_id=(select distinct src_dataset_id from \`${BQ_PROJECT}.${BQ_DATASET}._mapping_measurement\` where src_dataset_id like '%rdr%')
     and (m.measurement_concept_id > 0 or m.measurement_source_concept_id > 0)"
 
     echo "CREATE VIEWS - v_ehr_condition_occurrence"
