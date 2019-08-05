b6cd9de06fce6bef6fa23ea74ccc8a13d3f71b02
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-08-05 12:45:27 -0500

    correcting codacy errors

--- public-ui/src/app/views/survey-chart/survey-chart.component.ts
+++ public-ui/src/app/views/survey-chart/survey-chart.component.ts
@@ -1,8 +1,8 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {AchillesResult, DataBrowserService, SurveyQuestionAnalysis} from '../../../publicGenerated';
 import { DbConfigService } from '../../utils/db-config.service';
 import { GraphType } from '../../utils/enum-defs';
 import {TooltipService} from '../../utils/tooltip.service';
-import {AchillesResult, DataBrowserService, SurveyQuestionAnalysis} from '../../../publicGenerated';
 import { ISubscription } from 'rxjs/Subscription';
 
 @Component({
@@ -11,7 +11,6 @@ import { ISubscription } from 'rxjs/Subscription';
   styleUrls: ['./survey-chart.component.css', '../../styles/template.css']
 })
 export class SurveyChartComponent implements OnInit {
-  
   @Input() graphButtons: string[];
   @Input() question: any;
   @Input() answer: any;
@@ -27,18 +26,18 @@ export class SurveyChartComponent implements OnInit {
   private subscriptions: ISubscription[] = [];
   genderPercentageAnalysis: any;
   agePercentageAnalysis: any;
-  
+
   constructor(private tooltipText: TooltipService,
               public dbc: DbConfigService,
               private api: DataBrowserService) { }
-  
+
   ngOnInit() {
   }
-  
+
   public resetSelectedGraphs() {
     this.graphToShow = GraphType.None;
   }
-  
+
   public selectGraphType(g, q: any, answer: any) {
     this.resetSelectedGraphs();
     this.graphToShow = g;
@@ -64,7 +63,7 @@ export class SurveyChartComponent implements OnInit {
         break;
     }
   }
-  
+
   public selectGraph(sg: any, q: any, answer: any) {
     q.graphDataToShow = sg;
     if (q.graphDataToShow === 'Percentage (%)') {
@@ -119,7 +118,7 @@ export class SurveyChartComponent implements OnInit {
       }
     }
   }
-  
+
   public showToolTip(g: string) {
     if (g === 'Sex Assigned at Birth') {
       return this.tooltipText.biologicalSexChartHelpText;
@@ -137,13 +136,14 @@ export class SurveyChartComponent implements OnInit {
       return this.tooltipText.sourcesChartHelpText;
     }
   }
+
   public toolTipPos(g) {
     if (g === 'Sex Assigned at Birth') {
       return 'bottom-right';
     }
     return 'bottom-left';
   }
-  
+
   public hoverOnTooltip(q: any, a: any, g, event: string) {
     this.dbc.triggerEvent('tooltipsHover', 'Tooltips', 'Hover',
       this.surveyName + ' - Q' +  q.actualQuestionNumber
@@ -151,7 +151,7 @@ export class SurveyChartComponent implements OnInit {
       ' - ' + g, null,
       'Survey Chart Tooltip');
   }
-  
+
   public isPercentageAnalysis(question: any) {
     if(question.graphDataToShow && question.graphDataToShow.toLowerCase().indexOf('percentage') >= 0) {
       return true;
