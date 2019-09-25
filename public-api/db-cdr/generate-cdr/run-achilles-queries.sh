8d948d0fd54110dd2ef8c5e16e686fc768935c68
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-09-25 17:07:43 -0500

    query correction

--- public-api/db-cdr/generate-cdr/run-achilles-queries.sh
+++ public-api/db-cdr/generate-cdr/run-achilles-queries.sh
@@ -241,7 +241,7 @@ for index in "${!domain_names[@]}"; do
     select 0 as id,3000 as analysis_id,CAST(co1.${source_concept_id} AS STRING) as stratum_1, \"${domain_stratum}\" as stratum_3,
     COUNT(distinct co1.PERSON_ID) as count_value,COUNT(distinct co1.PERSON_ID) as source_count_value
     from \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.${domain_table_name}\` co1 join \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.concept\` c on
-    c.concept_id=co1.{source_concept_id}
+    c.concept_id=co1.${source_concept_id}
     where co1.${source_concept_id} not in (select distinct ${concept_id} from \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.${domain_table_name}\`)
     and co1.${source_concept_id} != ${exclude_concept_id}
     group by co1.${source_concept_id}"
@@ -315,7 +315,7 @@ for index in "${!domain_names[@]}"; do
     COUNT(distinct co1.person_id) as count_value,
     COUNT(distinct co1.person_id) as source_count_value from \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.${domain_table_name}\` co1 join ehr_age_stratum ca
     on co1.${table_id} = ca.${table_id}
-    join \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.concept\` c on c.concept_id=co1.{source_concept_id}
+    join \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.concept\` c on c.concept_id=co1.${source_concept_id}
     where co1.${source_concept_id} not in (select distinct ${concept_id} from \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.${domain_table_name}\`)
     group by co1.${source_concept_id}, stratum_2"
 
