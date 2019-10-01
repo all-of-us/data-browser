5644a1715603f847a918a6a4cc571f09c1e4c4fa
mhigbyflowers <mhigbyflowers@gmail.com>
2019-10-01 15:14:57 -0500

    adding souces component

--- public-ui/src/app/data-browser/data-browser.module.ts
+++ public-ui/src/app/data-browser/data-browser.module.ts
@@ -23,6 +23,7 @@ import { PhysicalMeasurementsComponent } from './views/pm/pm.component';
 import { QuickSearchComponent } from './views/quick-search/quick-search.component';
 import { SurveyChartComponent } from './views/survey-chart/survey-chart.component';
 import { SurveyViewComponent } from './views/survey-view/survey-view.component';
+import { SourcesComponent } from './sources/sources.component';
 
 
 
@@ -49,6 +50,7 @@ import { SurveyViewComponent } from './views/survey-view/survey-view.component';
     ChartTopResultsComponent,
     ChartBaseComponent,
     DbTableComponent,
+    SourcesComponent,
   ],
   exports: [
     DataBrowserRoutingModule,
