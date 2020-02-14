907569e691fd04754c82adaf0f842b1a51e04a92
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2020-02-14 10:27:18 -0600

    removing unneeded code

--- public-api/src/main/java/org/pmiops/workbench/config/WebSecurityConfig.java
+++ public-api/src/main/java/org/pmiops/workbench/config/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.web.cors.CorsConfiguration;
 
 
 @EnableWebSecurity
