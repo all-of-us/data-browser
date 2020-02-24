65f69ef72abf18137f45438de18c9be0918708fc
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2020-02-24 16:58:03 -0600

    fixing survey graph check

--- public-ui/src/app/data-browser/views/survey-view/survey-view.component.ts
+++ public-ui/src/app/data-browser/views/survey-view/survey-view.component.ts
@@ -378,6 +378,7 @@ export class SurveyViewComponent implements OnInit, OnDestroy {
     this.api.getMainSurveyQuestionResults(this.surveyConceptId, q.conceptId, q)
       .subscribe({
         next: results => {
+          console.log(results);
           q.countAnalysis = results.countAnalysis;
           q.genderAnalysis = results.genderAnalysis;
           q.ageAnalysis = results.ageAnalysis;
@@ -602,6 +603,7 @@ export class SurveyViewComponent implements OnInit, OnDestroy {
     this.api.getSurveyQuestionResults(a.stratum1, a.stratum2, a.stratum3, level)
       .subscribe({
         next: results => {
+          console.log(results);
           a.subQuestions = results.items;
           for (const subQuestion of a.subQuestions) {
             subQuestion.selectedAnalysis = subQuestion.genderAnalysis;
