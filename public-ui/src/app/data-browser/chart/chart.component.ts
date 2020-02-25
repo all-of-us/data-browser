224e8b86dbc7c05acc71b1bdc90a6849d58bb718
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2020-02-25 15:10:35 -0600

    changing group padding to match biological sex and age chart bar space

--- public-ui/src/app/data-browser/chart/chart.component.ts
+++ public-ui/src/app/data-browser/chart/chart.component.ts
@@ -698,7 +698,7 @@ export class ChartComponent implements OnChanges, AfterViewInit {
             '<br/> % with Medical Concept in EHR: <b>' +
             ((a.countValue / ageResult.countValue) * 100).toFixed() + '</b> %';
         } else {
-          const percentage = Number(((a.countValue/ageResult.countValue)*100).toFixed());
+          const percentage = Number(((a.countValue / ageResult.countValue) * 100).toFixed());
           totalToolTipHelpText = ageHelpText + ': '  + '<b>' + a.analysisStratumName + '</b>' +
             '</b>' + '<br/> Medical Concept Count: ' + '<b>' + a.countValue + '</b>' +
             '<br/> Total With EHR Count: ' + '<b>' + ageResult.countValue + '</b>' +
@@ -722,12 +722,13 @@ export class ChartComponent implements OnChanges, AfterViewInit {
         ageResult = this.surveyCountAnalysis.ageCountAnalysis.results.
         filter(x => x.stratum2 === a.stratum5)[0];
         if (ageResult.countValue > 20) {
-          totalToolTipHelpText = '<b>' + this.getSurveyAnswerText(a.stratum4) + '</b>' + '<br/>' + a.analysisStratumName +
+          totalToolTipHelpText = '<b>' + this.getSurveyAnswerText(a.stratum4) +
+            '</b>' + '<br/>' + a.analysisStratumName +
             ', Selected Answer: ' + '<b>' + a.countValue + '</b>' + '<br/>' + a.analysisStratumName +
             ', Took Survey: ' + '<b>' + ageResult.countValue + '</b>' +
-            '<br/> % Answered: <b>' + ((a.countValue/ageResult.countValue)*100).toFixed() + '</b> %';
+            '<br/> % Answered: <b>' + ((a.countValue / ageResult.countValue) * 100).toFixed() + '</b> %';
         } else {
-          const percentage = Number(((a.countValue/ageResult.countValue)*100).toFixed());
+          const percentage = Number(((a.countValue / ageResult.countValue) * 100).toFixed());
           totalToolTipHelpText = '<b>' + this.getSurveyAnswerText(a.stratum4) + '</b>' + '<br/>' + a.analysisStratumName +
             ', Selected Answer: ' + '<b>' + a.countValue + '</b>' + '<br/>' + a.analysisStratumName +
             ', Took Survey: ' + '<b>' + ageResult.countValue + '</b>' +
