38c64577d51df15a31d137bf95c8f21dc1675553
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2020-08-10 18:25:51 -0500

    Revert "[risk=low] setting up iap on test (#610)"
    This reverts commit 8624a3bb1ac15afd55a3b213407b425de9d250cf.

--- public-ui/src/environments/test-env-base.ts
+++ public-ui/src/environments/test-env-base.ts
@@ -2,7 +2,7 @@
 // environments.
 export const testEnvironmentBase = {
   clientId: '602460048110-5uk3vds3igc9qo0luevroc2uc3okgbkt.apps.googleusercontent.com',
-  publicApiUrl: 'https://aou-db-test.appspot.com',
+  publicApiUrl: 'https://api-dot-aou-db-test.appspot.com',
   workbenchUrl: 'https://all-of-us-workbench-test.appspot.com',
   researchAllOfUsUrl: 'https://staging.researchallofus.org',
   workBenchIsBeta: true,
