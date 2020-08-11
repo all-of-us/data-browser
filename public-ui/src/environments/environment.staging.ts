16f66cffcb05d5c8b6ad516cdf9be953150dd6e9
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2020-08-11 15:44:15 -0500

    set up iap on staging (#613)
    Co-authored-by: SrushtiGangireddy <srushti.gangireddy@vumc.org>

--- public-ui/src/environments/environment.staging.ts
+++ public-ui/src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   displayTag: 'Staging',
-  publicApiUrl: 'https://api-dot-aou-db-staging.appspot.com',
+  publicApiUrl: 'https://aou-db-staging.appspot.com',
   workbenchUrl: 'https://all-of-us-rw-staging.appspot.com',
   researchAllOfUsUrl: 'https://staging.researchallofus.org',
   clientId: '657299777109-kvb5qafr70bl01i6bnpgsiq5nt6v1o8u.apps.googleusercontent.com',
