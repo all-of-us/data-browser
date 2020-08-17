ef3f569b30ae1012916177fb5b6fa2f262e106f5
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2020-08-17 15:23:45 -0500

    ehr sticky header

--- public-ui/src/app/data-browser/db-table/db-table.component.ts
+++ public-ui/src/app/data-browser/db-table/db-table.component.ts
@@ -216,8 +216,10 @@ export class DbTableComponent implements OnChanges, OnDestroy {
 
   public scrollTo(id: string) {
     const el = this.elm.nativeElement.querySelector(id);
+    const parentEl = this.elm.nativeElement.querySelector(".tbl-body");
     if (el !== null) {
-      el.scrollIntoView({ behavior: 'smooth' });
+      el.scrollIntoView(true);
+      window.scrollBy(0, -60);
     } else {
       console.log('Scroll failed ID:', id);
 
@@ -266,8 +268,8 @@ export class DbTableComponent implements OnChanges, OnDestroy {
     if (r.graphToShow === GraphType.Sources &&
       ((r.domainId === 'Condition' && (r.vocabularyId === 'SNOMED' || r.vocabularyId === 'ICD9CM'
         || r.vocabularyId === 'ICD10CM')) || (r.domainId === 'Procedure'
-        && (r.vocabularyId === 'SNOMED' || r.vocabularyId === 'CPT4' || r.vocabularyId === 'ICD9CM'
-        || r.vocabularyId === 'ICD10PCS' || r.vocabularyId === 'ICD9Proc')))) {
+          && (r.vocabularyId === 'SNOMED' || r.vocabularyId === 'CPT4' || r.vocabularyId === 'ICD9CM'
+            || r.vocabularyId === 'ICD10PCS' || r.vocabularyId === 'ICD9Proc')))) {
       this.loadSourceTree(r);
     }
   }
