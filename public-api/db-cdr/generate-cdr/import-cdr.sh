f4a873db9fe75835775fafa831a5f8b06369b256
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2020-09-16 15:09:16 -0500

    remove docker on generate-cloudsql-db

--- public-api/db-cdr/generate-cdr/import-cdr.sh
+++ public-api/db-cdr/generate-cdr/import-cdr.sh
@@ -11,3 +11,9 @@ export BUCKET=$6
 
 JSON_FMT='{"import_cdr_data": '$IMPORT_CDR_DATA',"project": "'"$PROJECT"'", "instance": "'"$INSTANCE"'", "database": "'"$DATABASE"'", "bucket": "'"$BUCKET"'"}'
 CURL_DATA='{"branch": "'"$BRANCH"'", "parameters": '$JSON_FMT'}'
+
+curl -X \
+-H "Accept: application/json" \
+-H "Content-Type:application/json" \
+-H "Circle-Token: $(cat ~/.circle-creds/key.txt)" \
+-X POST --data "$(echo $CURL_DATA)" "https://circleci.com/api/v2/project/github/all-of-us/data-browser/pipeline"
