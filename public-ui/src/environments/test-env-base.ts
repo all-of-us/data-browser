85dbb646b4fb362c4aebb69675d1eec637838a67
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2020-09-16 15:39:55 -0500

    removing staging from RH . its not accuate

--- public-ui/src/environments/test-env-base.ts
+++ public-ui/src/environments/test-env-base.ts
@@ -4,7 +4,7 @@ export const testEnvironmentBase = {
   clientId: '602460048110-5uk3vds3igc9qo0luevroc2uc3okgbkt.apps.googleusercontent.com',
   publicApiUrl: 'https://api-dot-aou-db-test.appspot.com',
   workbenchUrl: 'https://all-of-us-workbench-test.appspot.com',
-  researchAllOfUsUrl: 'https://staging.researchallofus.org',
+  researchAllOfUsUrl: 'https://researchallofus.org',
   gaId: 'UA-116298798-4',
   copeFlag: false,
   preCopeFlag: true
