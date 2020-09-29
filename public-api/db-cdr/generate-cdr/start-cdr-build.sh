aefceccad5d392a9cc9482ac6a1ca1695efbb16a
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2020-09-29 13:27:17 -0500

    wip

--- public-api/db-cdr/generate-cdr/start-cdr-build.sh
+++ public-api/db-cdr/generate-cdr/start-cdr-build.sh
@@ -15,8 +15,8 @@ current_date_time="`date +%Y%m%d%H%M%S`";
 JSON_FMT='{"build_cdr_indices": '$BUILD_CDR_INDICES',"cdr_source_project": "'"$CDR_SOURCE_PROJECT"'", "cdr_source_dataset": "'"$CDR_SOURCE_DATASET"'", "cdr_sql_bucket": "'"$CDR_SQL_BUCKET"'", "cdr_dest_project": "'"$CDR_DESTINATION_PROJECT"'", "cdr_version_db_name": "'"$CDR_VERSION_DB_NAME"'"}'
 CURL_DATA='{"branch": "'"$BRANCH"'", "parameters": '$JSON_FMT'}'
 
-curl -X \
+curl -k -X POST \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
 -H "Circle-Token: $(cat ~/.circle-creds/key.txt)" \
--X POST --data "$(echo $CURL_DATA)" "https://circleci.com/api/v2/project/github/all-of-us/data-browser/pipeline"
+-d "$(echo $CURL_DATA)" "https://circleci.com/api/v2/project/github/all-of-us/data-browser/pipeline"
