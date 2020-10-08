2b755bc8f25f2f05be13e6ae630e5b7790e32a88
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2020-10-08 15:52:08 -0500

    [RW-5529][risk=no] cope disclaimer (#655)
    * had to add styles here to it would overide
    
    * cope disclaimer
    
    * cleaned up
    
    * turned disclaimer off
    
    * adding var
    
    * touchup
    
    * addressing comments

--- public-ui/src/app/data-browser/views/quick-search/quick-search.component.ts
+++ public-ui/src/app/data-browser/views/quick-search/quick-search.component.ts
@@ -140,9 +140,9 @@ export class QuickSearchComponent implements OnInit, OnDestroy {
             this.displayDomainTotalsErrorMessage = false;
           },
           error: err => {
-            let errorBody = {'message': ''};
+            let errorBody = { 'message': '' };
             try {
-                errorBody = JSON.parse(err._body);
+              errorBody = JSON.parse(err._body);
             } catch (e) {
             }
             this.displayDomainTotalsErrorMessage = true;
@@ -164,9 +164,9 @@ export class QuickSearchComponent implements OnInit, OnDestroy {
           this.displayDomainTotalsErrorMessage = false;
         },
         error: err => {
-          let errorBody = {'message': ''};
+          let errorBody = { 'message': '' };
           try {
-              errorBody = JSON.parse(err._body);
+            errorBody = JSON.parse(err._body);
           } catch (e) {
           }
           this.displayDomainTotalsErrorMessage = true;
@@ -188,9 +188,9 @@ export class QuickSearchComponent implements OnInit, OnDestroy {
             this.displayDomainTotalsErrorMessage = false;
           },
           error: err => {
-            let errorBody = {'message': ''};
+            let errorBody = { 'message': '' };
             try {
-                errorBody = JSON.parse(err._body);
+              errorBody = JSON.parse(err._body);
             } catch (e) {
             }
             this.displayDomainTotalsErrorMessage = true;
@@ -232,7 +232,7 @@ export class QuickSearchComponent implements OnInit, OnDestroy {
     this.surveyResults = results.surveyModules;
     // TODO remove this filter when the feature flag is turned off or to debug
     if (!this.cope) {
-        this.surveyResults = results.surveyModules.filter(sm => sm.conceptId !== 1333342);
+      this.surveyResults = results.surveyModules.filter(sm => sm.conceptId !== 1333342);
     }
 
     this.surveyResults.forEach(result => {
