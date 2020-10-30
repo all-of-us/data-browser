ff9e75864bc877d669dfb96749c422c849ef4613
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2020-10-30 11:51:22 -0500

    [RW-5601][risk=no] adding fitbit env var and removing workbench url (#673)
    * adding fitbt env var and removing workbench url
    
    * flipped var
    
    * adding fitbit flag in local env
    
    Co-authored-by: SrushtiGangireddy <srushti.gangireddy@vumc.org>

--- public-ui/src/environments/environment.stable.ts
+++ public-ui/src/environments/environment.stable.ts
@@ -1,7 +1,6 @@
 export const environment = {
   displayTag: 'Stable',
   publicApiUrl: 'https://public.api.stable.fake-research-aou.org',
-  workbenchUrl: 'https://all-of-us-rw-stable.appspot.com',
   researchAllOfUsUrl: 'https://researchallofus.org',
   clientId: '56507752110-ovdus1lkreopsfhlovejvfgmsosveda6.apps.googleusercontent.com',
   debug: false,
@@ -10,5 +9,6 @@ export const environment = {
   gtmAuth: '87R8_4UCXpVCroAA7bGkAg',
   gtmPreview: 'env-27',
   copeFlag: false,
-  preCopeFlag: false
+  preCopeFlag: false,
+  fitbit: false
 };
--- public-ui/src/environments/environment.ts
+++ public-ui/src/environments/environment.ts
@@ -3,7 +3,6 @@ import {testEnvironmentBase} from 'environments/test-env-base';
 export const environment = {
   ...testEnvironmentBase,
   displayTag: 'Local->Test',
-  workbenchUrl: 'http://localhost:4200',
   debug: true,
   gtmId: 'GTM-NQ9XDTW',
   gtmAuth: 'kMy-ZUX8XsBfuanMZzP_5A',
