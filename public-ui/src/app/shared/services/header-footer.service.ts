0cc7c74a3ac6f382c55cf2c201c5fd708d75b760
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2020-11-02 09:43:23 -0600

    footer layout

--- public-ui/src/app/shared/services/header-footer.service.ts
+++ public-ui/src/app/shared/services/header-footer.service.ts
@@ -24,18 +24,8 @@ export class HeaderFooterService {
           submenu: false,
         },
         {
-          title: 'Researcher Workshops',
-          url: 'https://www.researchallofus.org/researchers-as-partners/researcher-workshops-and-public-input/',
-          submenu: false
-        },
-        {
           title: 'Privacy & Security Protocols',
           url: 'https://www.researchallofus.org/privacy-security-protocols/',
-          submenu: false
-        },
-        {
-          title: 'Institutional Agreements',
-          url: 'https://www.researchallofus.org/institutional-agreements/',
           submenu: false
         },
         {
@@ -48,14 +38,15 @@ export class HeaderFooterService {
         title: 'Data & Tools',
         url: 'https://www.researchallofus.org/data-snapshots/',
         submenu: true,
-        sub0: [{
+        sub0: [
+          {
+            title: 'Data Browser',
+            url: 'https://databrowser.researchallofus.org/',
+            submenu: false
+          },
+          {
           title: 'Data Snapshots',
           url: 'https://www.researchallofus.org/data-snapshots/',
-          submenu: false
-        },
-        {
-          title: 'Data Sources',
-          url: 'https://www.researchallofus.org/data-sources/',
           submenu: false
         },
         {
@@ -64,23 +55,18 @@ export class HeaderFooterService {
           submenu: false
         },
         {
-          title: 'Data Access',
-          url: 'https://www.researchallofus.org/data-access/',
+          title: 'Data Access & Use',
+          url: 'https://www.researchallofus.org/data-tools/data-access/',
           submenu: false
         },
         {
-          title: 'Data Use Policies',
-          url: 'https://www.researchallofus.org/data-use-policies',
+          title: 'Data Sources',
+          url: 'https://www.researchallofus.org/data-tools/data-sources/',
           submenu: false
         },
         {
-          title: 'Survey Explorer',
-          url: 'https://www.researchallofus.org/data-sources/survey-explorer/',
-          submenu: false
-        },
-        {
-          title: 'Data Browser',
-          url: 'https://databrowser.researchallofus.org/',
+          title: 'Data Methodology',
+          url: 'https://www.researchallofus.org/data-tools/methods/',
           submenu: false
         },
         {
@@ -89,10 +75,11 @@ export class HeaderFooterService {
           submenu: false,
         },
         {
-          title: 'Guides & Support',
-          url: 'https://www.researchallofus.org/guides-support/',
-          submenu: false,
-        }]
+          title: 'Survey Explorer',
+          url: 'https://www.researchallofus.org/data-sources/survey-explorer/',
+          submenu: false
+        }
+      ]
       },
       {
         title: 'Discover',
