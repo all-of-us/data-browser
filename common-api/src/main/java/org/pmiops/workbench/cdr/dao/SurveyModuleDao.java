23750910afb64448cae2c4ce668efa19f2058171
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2020-11-10 13:49:21 -0600

    fixing search tile counts

--- common-api/src/main/java/org/pmiops/workbench/cdr/dao/SurveyModuleDao.java
+++ common-api/src/main/java/org/pmiops/workbench/cdr/dao/SurveyModuleDao.java
@@ -15,11 +15,11 @@ public interface SurveyModuleDao extends CrudRepository<SurveyModule, Long> {
    */
   @Query(nativeQuery=true, value= "select m.name, m.description, m.concept_id, COUNT(DISTINCT a.concept_id) as question_count, m.participant_count, m.order_number, m.can_show from \n" +
           "survey_module m join question_concept sqm2 on m.concept_id = sqm2.survey_concept_id join \n" +
-          "(select distinct c.* from question_concept c where (match(c.question_string) against (?1 in boolean mode) > 0 or match(c.concept_name) against ('pregnant' in boolean mode) > 0) \n" +
+          "(select distinct c.* from question_concept c where (match(c.question_string) against (?1 in boolean mode) > 0 or match(c.concept_name) against (?1 in boolean mode) > 0) \n" +
           "and c.sub=0 and c.generate_counts=1\n" +
           "union distinct \n" +
           "select distinct c.* from question_concept c where concept_id in (select distinct SUBSTRING_INDEX(sqm.path, '.', 1) from question_concept sqm \n" +
-          "where (match(sqm.question_string) against (?1 in boolean mode) > 0 or match(sqm.concept_name) against ('pregnant' in boolean mode) > 0) and sqm.sub=1 and sqm.generate_counts=1)\n" +
+          "where (match(sqm.question_string) against (?1 in boolean mode) > 0 or match(sqm.concept_name) against (?1 in boolean mode) > 0) and sqm.sub=1 and sqm.generate_counts=1)\n" +
           "union distinct \n" +
           "select distinct c.* from question_concept c where concept_id in (?3) and (match(question_string) against(?1 in boolean mode) > 0 or match(concept_name) against(?1 in boolean mode) > 0)\n" +
           "union distinct\n" +
