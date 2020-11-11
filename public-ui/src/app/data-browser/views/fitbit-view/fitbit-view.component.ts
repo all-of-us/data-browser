628a1df4bf95e3f6b6a8bba8ed775594d1538ddc
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2020-11-11 10:38:48 -0600

    [RW-5876][risk-no]bugbash fitbit route (#685)
    * adding ® to all fitbit strings
    
    * fixing capitalizations in fitbit
    
    * lint

--- public-ui/src/app/data-browser/views/fitbit-view/fitbit-view.component.ts
+++ public-ui/src/app/data-browser/views/fitbit-view/fitbit-view.component.ts
@@ -9,7 +9,6 @@ import { DbConfigService } from '../../../utils/db-config.service';
   styleUrls: ['../../../styles/template.css', './fitbit-view.component.css']
 })
 export class FitbitViewComponent implements OnInit {
-  title = 'Fitbit Data';
   private subscriptions: ISubscription[] = [];
   loadingStack: any = [];
   fitbitConcepts: any = [];
@@ -17,10 +16,13 @@ export class FitbitViewComponent implements OnInit {
   isLoading = false;
   analyses: any;
   selectedAnalyses: any;
-  selectedItem = 'All Fitbit Data';
-  selectedDisplay = 'All Fitbit Data';
+  selectedItem: string;
+  selectedDisplay: string;
 
-  constructor(private api: DataBrowserService, public dbc: DbConfigService) { }
+  constructor(private api: DataBrowserService, public dbc: DbConfigService) {
+    this.selectedItem = 'all Fitbit data';
+    this.selectedDisplay = 'all Fitbit® data';
+  }
 
   loading() {
     return this.loadingStack.length > 0;
@@ -29,23 +31,23 @@ export class FitbitViewComponent implements OnInit {
   ngOnInit() {
     this.searchText = localStorage.getItem('searchText');
     this.fitbitConcepts.push({
-      id: 1, displayName: 'All Fitbit Data',
+      id: 1, displayName: 'all Fitbit® data',
       conceptName: 'All Fitbit Data', icon: 'fa-watch-fitness'
     });
     this.fitbitConcepts.push({
-      id: 2, displayName: 'Heart rate by zone summary',
+      id: 2, displayName: 'heart rate by zone summary',
       conceptName: 'Heart Rate (Summary)', icon: 'fa-heartbeat'
     });
     this.fitbitConcepts.push({
-      id: 3, displayName: 'Heart rate (minute-level)',
+      id: 3, displayName: 'heart rate (minute-level)',
       conceptName: 'Heart rate (minute-level)', icon: 'fa-monitor-heart-rate'
     });
     this.fitbitConcepts.push({
-      id: 4, displayName: 'Activity (daily summary)',
+      id: 4, displayName: 'activity (daily summary)',
       conceptName: 'Activity (daily summary)', icon: 'fa-running'
     });
     this.fitbitConcepts.push({
-      id: 5, displayName: 'Activity intraday steps (minute-level)',
+      id: 5, displayName: 'activity intraday steps (minute-level)',
       conceptName: 'Activity intraday steps (minute-level)', icon: 'fa-walking'
     });
 
@@ -55,20 +57,20 @@ export class FitbitViewComponent implements OnInit {
         next: result => {
           this.analyses = result.items;
           for (const item of result.items) {
-              const fitbitConcept = this.fitbitConcepts.filter(concept =>
+            const fitbitConcept = this.fitbitConcepts.filter(concept =>
               concept.conceptName.toLowerCase().includes(item.conceptId.toLowerCase()))[0];
-              fitbitConcept['ageAnalysis'] = item.ageAnalysis;
-              fitbitConcept['genderAnalysis'] = item.genderAnalysis;
-              fitbitConcept['countAnalysis'] = item.countAnalysis;
-              fitbitConcept['participantCountAnalysis'] = item.participantCountAnalysis;
+            fitbitConcept['ageAnalysis'] = item.ageAnalysis;
+            fitbitConcept['genderAnalysis'] = item.genderAnalysis;
+            fitbitConcept['countAnalysis'] = item.countAnalysis;
+            fitbitConcept['participantCountAnalysis'] = item.participantCountAnalysis;
           }
           if (this.searchText) {
             const matchingConcepts = this.fitbitConcepts.filter(concept =>
-            concept.conceptName.toLowerCase().includes(this.searchText.toLowerCase()));
+              concept.conceptName.toLowerCase().includes(this.searchText.toLowerCase()));
             if (matchingConcepts && matchingConcepts.length > 0) {
-                this.selectedItem = matchingConcepts[0].conceptName;
-                this.selectedDisplay = matchingConcepts[0].displayName;
-                this.selectedAnalyses = matchingConcepts[0];
+              this.selectedItem = matchingConcepts[0].conceptName;
+              this.selectedDisplay = matchingConcepts[0].displayName;
+              this.selectedAnalyses = matchingConcepts[0];
             }
           }
           this.selectedAnalyses = result.items[0];
