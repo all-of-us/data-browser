abd38d3611cb71c27b84fe12f60964e65fa9c2f8
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2020-12-03 12:32:44 -0600

    removing console

--- public-ui/src/app/data-browser/views/survey-view/survey-view.component.ts
+++ public-ui/src/app/data-browser/views/survey-view/survey-view.component.ts
@@ -348,7 +348,6 @@ export class SurveyViewComponent implements OnInit, OnDestroy {
                   }
                 });
               });
-              console.log(this.surveyVersions);
             },
             error: err => {
               console.error('Observer got an error: ' + err);
