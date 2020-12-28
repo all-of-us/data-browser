a6f9d325074f2c0bb3b737b5856dc08827b30f6a
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2020-12-28 16:35:05 -0600

    [risk=no] [RW-6078] API improvement - mapper for question concept (#744)
    * mapper for question concept
    
    * mapper for question concept
    
    * fixing codacy
    
    * fixing codacy
    
    * fixing codacy
    
    * fixing failing tests
    
    * codacy import fix
    
    * fix failing tests
    
    Co-authored-by: SrushtiGangireddy <srushti.gangireddy@vumc.org>

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/QuestionConceptDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/QuestionConceptDao.java
@@ -1,20 +1,20 @@
 package org.pmiops.workbench.cdr.dao;
-import org.pmiops.workbench.cdr.model.QuestionConcept;
+import org.pmiops.workbench.cdr.model.DbQuestionConcept;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface QuestionConceptDao extends CrudRepository<QuestionConcept, Long> {
+public interface QuestionConceptDao extends CrudRepository<DbQuestionConcept, Long> {
     @Query(nativeQuery=true, value="select * from question_concept where survey_concept_id=?1 and generate_counts=1 and sub=0")
-    List<QuestionConcept> getSurveyQuestions(Long survey_concept_id);
+    List<DbQuestionConcept> getSurveyQuestions(Long survey_concept_id);
 
     @Query(nativeQuery=true, value="select * from question_concept where concept_id in \n" +
             "(select distinct SUBSTRING_INDEX(SUBSTRING_INDEX(path,'.',1),'.',-1) from question_concept where survey_concept_id=?1 and (match(question_string) against(?2 in boolean mode) > 0 or match(concept_name) against(?2 in boolean mode) > 0)) and generate_counts=1;")
-    List<QuestionConcept> getMatchingSurveyQuestions(Long survey_concept_id, String search_word);
+    List<DbQuestionConcept> getMatchingSurveyQuestions(Long survey_concept_id, String search_word);
 
     @Query(nativeQuery=true, value="select distinct * from question_concept c where concept_id in (?1)")
-    List<QuestionConcept> getFMHQuestions(List<String> questionConceptIds);
+    List<DbQuestionConcept> getFMHQuestions(List<String> questionConceptIds);
 
     @Query(nativeQuery=true, value="select distinct c.* from question_concept c where concept_id in (?1) and (match(concept_name) against(?2 in boolean mode) > 0 or match(question_string) against(?2 in boolean mode) > 0)\n" +
             "union distinct\n" +
@@ -23,13 +23,13 @@ public interface QuestionConceptDao extends CrudRepository<QuestionConcept, Long
             "join question_concept ar2 on ar1.stratum_3=ar2.concept_id\n" +
             "where ar1.stratum_2 in (?1) and ar1.analysis_id=3110 \n" +
             "and (match(ar2.concept_name) against(?2 in boolean mode) > 0 or match(ar2.question_string) against(?2 in boolean mode) > 0);")
-    List<QuestionConcept> getMatchingFMHQuestions(List<String> questionConceptIds, String search_word);
+    List<DbQuestionConcept> getMatchingFMHQuestions(List<String> questionConceptIds, String search_word);
 
     @Query(nativeQuery=true, value="select distinct * from question_concept where sub=1 and SUBSTRING_INDEX(SUBSTRING_INDEX(path, '.', 1), '.', -1)=?1 and \n" +
             "SUBSTRING_INDEX(SUBSTRING_INDEX(path, '.', 2), '.', -1)=?2 and survey_concept_id=?3 and LENGTH(path) - LENGTH(REPLACE(path, '.', ''))=2")
-    List<QuestionConcept> getSubQuestionsLevel1(String questionConceptId, String answerConceptId, String surveyConceptId);
+    List<DbQuestionConcept> getSubQuestionsLevel1(String questionConceptId, String answerConceptId, String surveyConceptId);
 
     @Query(nativeQuery=true, value="select distinct * from question_concept where sub=1 and \n" +
             "SUBSTRING_INDEX(SUBSTRING_INDEX(path, '.', 4), '.', -1)=?1 and LENGTH(path) - LENGTH(REPLACE(path, '.', ''))=4")
-    List<QuestionConcept> getSubQuestionsLevel2(String answerConceptId);
+    List<DbQuestionConcept> getSubQuestionsLevel2(String answerConceptId);
 }
