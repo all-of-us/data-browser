ffd48b5853713012dc3cfa0e950ee2619a6b1608
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2021-01-08 13:02:17 -0600

    lint

--- public-ui/src/testing/stubs/server-config-service-stub.ts
+++ public-ui/src/testing/stubs/server-config-service-stub.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/observable';
 
 import {ConfigResponse} from 'publicGenerated';
 
