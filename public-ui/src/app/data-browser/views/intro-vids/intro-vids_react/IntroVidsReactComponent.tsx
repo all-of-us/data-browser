971ea18d4287d8caedb86a674975e852ae959a7d
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2021-01-23 00:46:17 -0600

    WIP

--- public-ui/src/app/data-browser/views/intro-vids/intro-vids_react/IntroVidsReactComponent.tsx
+++ public-ui/src/app/data-browser/views/intro-vids/intro-vids_react/IntroVidsReactComponent.tsx
@@ -1,29 +1,30 @@
 import * as React from 'react';
 import { FunctionComponent, useEffect, useRef, useState } from 'react';
-import {Video} from '../../../services/video.service'
+import { Video } from '../../../services/video.service'
 
 
 export const IntroVidReactComponent: FunctionComponent<Video> =
     (props) => {
-        console.log('IntroVidReactComponent');     
         return <span>
-            <h2 className="secondary-display">{props.title}</h2>
-            <div className="vid-container">
+            <h2 className='secondary-display'>{props.title}</h2>
+            <div className='vid-container'>
                 <video poster={props.poster} controls >
                     {
                         props.src.map((source) => {
-                            return <source key={source.url} src={source.url} type={source.type} />
+                            return <source key={source.url} src={source.url} type={source.type} />;
                         })
                     }
                     {
                         props.subtitles.map((sub) => {
-                            return <track key={sub.label} default={sub.default} label={sub.label} kind='subtitles' lang={sub.lang} src={sub.url}></track>
+                            return <track 
+                            key={sub.label} default={sub.default} label={sub.label} kind='subtitles' 
+                            lang={sub.lang} src={sub.url}></track>;
                         })
                     }
-                     Sorry, your browser doesn't support embedded videos,
+                    Sorry, your browser doesn't support embedded videos,
                      but don't worry, you can <a href={props.downloadUrl}>download it</a>
-                     and watch it with your favorite video player!
+                    and watch it with your favorite video player!
                 </video>
             </div>
-        </span>
+        </span>;
     };
