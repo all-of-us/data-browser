7e38e13e8a6f1c588f38c2da05772cc8062418ea
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2021-02-10 07:15:30 -0600

    adding decoraters back

--- public-ui/src/app/data-browser/base-react/base-react.wrapper.tsx
+++ public-ui/src/app/data-browser/base-react/base-react.wrapper.tsx
@@ -1,21 +1,30 @@
 import {
     AfterViewInit,
+    Component,
     ElementRef,
     Injector,
     OnChanges,
     OnDestroy,
     SimpleChanges,
-    ViewChild
+    ViewChild,
+    ViewEncapsulation
 } from '@angular/core';
+import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 const containerElementName = 'myReactComponentContainer';
 
-
-// tslint:disable-next-line: component-class-suffix
+@Component({
+    // tslint:disable-next-line: component-selector
+    selector: 'base-react-wrapper',
+    template: `<span #${containerElementName}></span>`,
+    styleUrls: ['../../styles/template.css'],
+    encapsulation: ViewEncapsulation.None,
+})
+  // tslint:disable-next-line: component-class-suffix
 export class BaseReactWrapper implements OnChanges, OnDestroy, AfterViewInit {
     @ViewChild(containerElementName, { static: false }) containerRef: ElementRef;
 
-    constructor(public injector: Injector) { }
+    constructor(public injector: Injector) {}
 
     ngOnChanges(changes: SimpleChanges): void {
         this.render();
@@ -28,9 +37,9 @@ export class BaseReactWrapper implements OnChanges, OnDestroy, AfterViewInit {
     ngOnDestroy(): void {
         ReactDOM.unmountComponentAtNode(this.containerRef.nativeElement);
     }
+
     render() {
         // this will be overwritten by the extended wrapper
     }
-
 }
 
