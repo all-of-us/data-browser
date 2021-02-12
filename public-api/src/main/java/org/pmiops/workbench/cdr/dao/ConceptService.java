0d6ba46292f22394c1ff5cd974cd8fc9d54507fe
srushti <srushti.gangireddy@gmail.com>
2021-02-11 18:30:52 -0600

    revert

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/ConceptService.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/ConceptService.java
@@ -135,7 +135,7 @@ public class ConceptService {
     public static final String CLASSIFICATION_CONCEPT_CODE = "C";
 
     public Slice<DbConcept> searchConcepts(String query, StandardConceptFilterVal standardConceptFilter, List<Long> conceptIds, List<String> vocabularyIds, String domainId, int limit, int minCount, int page,
-                                         int measurementTests, int measurementOrders) {
+                                           int measurementTests, int measurementOrders) {
 
         Specification<DbConcept> conceptSpecification =
                 (root, criteriaQuery, criteriaBuilder) -> {
@@ -159,7 +159,7 @@ public class ConceptService {
                     }
 
                     if (standardConceptFilter.equals(StandardConceptFilterVal.STANDARD_CONCEPTS)) {
-                            predicates.add(criteriaBuilder.or(standardConceptPredicates.toArray(new Predicate[0])));
+                        predicates.add(criteriaBuilder.or(standardConceptPredicates.toArray(new Predicate[0])));
                     } else if (standardConceptFilter.equals(StandardConceptFilterVal.NON_STANDARD_CONCEPTS)) {
                         predicates.add(
                                 criteriaBuilder.or(
