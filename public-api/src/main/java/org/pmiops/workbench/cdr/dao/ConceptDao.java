b9e83f4a7bc82a1d44e408efbe7c799658aead3e
srushti <srushti.gangireddy@gmail.com>
2021-02-15 22:12:01 -0600

    fixing test

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/ConceptDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/ConceptDao.java
@@ -20,20 +20,6 @@ public interface ConceptDao extends CrudRepository<DbConcept, Long> {
             "by c.count_value desc",nativeQuery=true)
     List<DbConcept> findSourceConcepts(@Param("conceptId") long conceptId,@Param("minCount") Integer minCount);
 
-    @Query(value = "select c.* from concept c " +
-            "join concept_relationship rel on rel.concept_id_2 = c.concept_id " +
-            "and rel.concept_id_1 = :conceptId and rel.relationship_id = 'maps to' " +
-            "where c.concept_id != :conceptId order by c.count_value desc",
-            nativeQuery = true)
-    List<DbConcept> findConceptsMapsToParents(@Param("conceptId") long conceptId);
-
-    List<DbConcept> findByConceptName(String conceptName);
-
-    @Query(value = "select c.* from concept c " +
-            "where c.vocabulary_id in ('Gender', 'Race', 'Ethnicity')",
-            nativeQuery = true)
-    List<DbConcept> findGenderRaceEthnicityFromConcept();
-
     /**
      * Return the number of standard concepts in each vocabulary for the specified domain matching the
      * specified expression, matching concept name, synonym, ID, or code.
@@ -49,57 +35,6 @@ public interface ConceptDao extends CrudRepository<DbConcept, Long> {
             "group by c.vocabularyId\n" +
             "order by c.vocabularyId\n")
     List<VocabularyCount> findVocabularyStandardConceptCounts(String matchExp, String domainId);
-
-    /**
-     * Return the number of concepts (standard or non-standard) in each vocabulary
-     * for the specified domain matching the specified expression, matching concept name, synonym,
-     * ID, or code.
-     * @param matchExp SQL MATCH expression to match concept name or synonym
-     * @param domainId domain ID to use when filtering concepts
-     * @return per-vocabulary concept counts
-     */
-    @Query(value = "select c.vocabularyId as vocabularyId, count(*) as conceptCount from DbConcept c\n" +
-            "where (c.countValue > 0 or c.sourceCountValue > 0) and\n" +
-            "matchConcept(c.conceptName, c.conceptCode, c.vocabularyId, c.synonymsStr, ?1) > 0 and\n" +
-            "c.domainId = ?2\n" +
-            "group by c.vocabularyId\n" +
-            "order by c.vocabularyId\n")
-    List<VocabularyCount> findVocabularyAllConceptCounts(String matchExp, String domainId);
-
-    /*
-    @Query(value = "select distinct c1.* from concept_relationship cr " +
-            "join concept c1 on (cr.concept_id_2 = c1.concept_id " +
-            "and cr.concept_id_1 in (" +
-            "select distinct c.concept_id from criteria c\n" +
-            "where c.type = 'DRUG' " +
-            "and c.subtype in ('BRAND') " +
-            "and c.is_selectable = 1 " +
-            "and match(c.name, c.code) against(?1 in boolean mode) > 0 " +
-            "order by c.name asc) " +
-            "and c1.concept_class_id = 'Ingredient' and (c1.has_counts > 0)) ", nativeQuery = true)
-    List<DbConcept> findDrugIngredientsByBrand(String query);
-
-    @Query(value = "select distinct c.concept_id from cb_criteria c\n" +
-            "inner join ( \n" +
-            "select cr.concept_id_2 from cb_criteria_relationship cr \n" +
-            "join concept c1 on (cr.concept_id_2 = c1.concept_id\n" +
-            "and cr.concept_id_1 in (select distinct concept_id from cb_criteria c where c.domain_id='DRUG' and c.type='BRAND' and match(c.name, c.code) against(?1 in boolean mode) > 0)\n" +
-            "and c1.concept_class_id = 'Ingredient')) cr1 on c.concept_id = cr1.concept_id_2\n" +
-            "and c.domain_id = 'DRUG' and c.type = 'RXNORM' and match(c.synonyms) against('+[drug_rank1]' in boolean mode)", nativeQuery= true)
-    List<Long> findDrugIngredientsByBrand(String query);
-
-    @Query(value = "select distinct c1.* from concept_relationship cr " +
-            "join concept c1 on (cr.concept_id_2 = c1.concept_id " +
-            "and cr.concept_id_1 in (" +
-            "select distinct c.concept_id from criteria c\n" +
-            "where c.type = 'DRUG' " +
-            "and c.subtype in ('BRAND') " +
-            "and c.is_selectable = 1 " +
-            "and match(c.name, c.code) against(?1 in boolean mode) > 0 " +
-            "order by c.name asc) " +
-            "and c1.concept_class_id = 'Ingredient') and cr.concept_id_2 not in (?2) and (c1.has_counts > 0)", nativeQuery = true)
-    List<DbConcept> findDrugIngredientsByBrandNotInConceptIds(String query, List<Long> conceptIds);
-    */
 
     @Query(value = "select distinct cr.concept_id_2 from cb_criteria_relationship cr \n" +
             "join concept c1 on (cr.concept_id_2 = c1.concept_id\n" +
