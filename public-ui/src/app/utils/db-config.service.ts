272e4be4aa8c9cf29351da764865e6d40fa28e59
srushti <srushti.gangireddy@gmail.com>
2021-02-18 14:52:10 -0600

    fixing cope visit from url

--- public-ui/src/app/utils/db-config.service.ts
+++ public-ui/src/app/utils/db-config.service.ts
@@ -157,7 +157,7 @@ export class DbConfigService {
     { conceptId: 43529712, conceptName: 'Personal Medical History' },
     { conceptId: 43528895, conceptName: 'Health Care Access and Utilization' },
     { conceptId: 43528698, conceptName: 'Family Health History' },
-    { conceptId: 1333342, conceptName: 'COVID-19 Participant Experience' }
+    { conceptId: 1333342, conceptName: 'COVID-19 Participant Experience (COPE)' }
   ];
 
   // chart options
