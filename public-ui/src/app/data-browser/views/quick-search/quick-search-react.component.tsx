01aa883aab6bbda3e81a51521120f056c9d04355
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2021-02-22 12:11:57 -0600

    wip

--- public-ui/src/app/data-browser/views/quick-search/quick-search-react.component.tsx
+++ public-ui/src/app/data-browser/views/quick-search/quick-search-react.component.tsx
@@ -5,7 +5,7 @@ import {
     ViewChild,
     ViewEncapsulation
 } from '@angular/core';
-import { TooltipService } from 'app/utils/tooltip.service';
+
 import { environment } from 'environments/environment';
 import { Configuration, DataBrowserApi } from 'publicGenerated/fetch';
 import * as React from 'react';
@@ -13,6 +13,17 @@ import { FunctionComponent } from 'react';
 import * as ReactDOM from 'react-dom';
 import { BaseReactWrapper } from '../../base-react/base-react.wrapper';
 const containerElementName = 'myReactComponentContainer';
+const api = new DataBrowserApi(new Configuration({ basePath: environment.publicApiUrl }));
+async function  getDomainInfo(): Promise<any> {
+     return await  api.getDomainTotals('', 1, 1).then(
+        async result => {
+            return  await {
+                surveyInfo: result.surveyModules,
+                domainInfo: result.domainInfos
+            };
+        }
+    );
+}
 
 const ResultLinksComponent: FunctionComponent<any> =
     ({
@@ -51,6 +62,57 @@ const ResultLinksComponent: FunctionComponent<any> =
 
     };
 
+const domainInfo: any = getDomainInfo();
+console.log(domainInfo,'wtf');
+const physicalMeasurementDomainInfo = domainInfo.domainInfo.filter(domain => {
+    return domain.name.toLowerCase() === 'physical measurements'
+        || domain.name.toLowerCase() === 'fitbit';
+});
+
+
+
+domainInfo.domainInfo = domainInfo.domainInfo.filter(
+    domain => domain.name.toLowerCase() !== 'physical measurements' &&
+        domain.name.toLowerCase() !== 'fitbit');
+
+const dBHomeComponent = ():any => {
+    <section className='results'>
+        <h5 className='result-heading secondary-display'> EHR Domains:</h5>
+        <div id='survey' className='result-boxes'>
+            {
+                domainInfo.domainInfo.map((domain, index) => {
+                    const key = 'domain' + index;
+                    return <ResultLinksComponent key={key} {...domain} />;
+
+                })
+
+            }
+        </div>
+        <h5 className='result-heading secondary-display'>Survey Questions:</h5>
+        <div className='result-boxes'>
+            {
+                domainInfo.surveyInfo.map((survey, index) => {
+                    const key = 'survey' + index;
+                    return <ResultLinksComponent key={key} {...survey} />;
+                })
+
+            }
+        </div>
+        <h5 className='result-heading secondary-display'>
+            Physical Measurements and Wearables:</h5>
+        <div className='result-boxes'>
+            {
+                physicalMeasurementDomainInfo.map((phyMeasurements, index) => {
+                    const key = 'phyMeasurements' + index;
+                    return <ResultLinksComponent key={key} {...phyMeasurements} />;
+                })
+
+            }
+        </div>
+    </section>
+}
+
+
 @Component({
     // tslint:disable-next-line: component-selector
     selector: 'react-db-home',
@@ -61,75 +123,13 @@ const ResultLinksComponent: FunctionComponent<any> =
 
 export class DbHomeWrapperComponent extends BaseReactWrapper {
     @ViewChild(containerElementName, { static: false }) containerRef: ElementRef;
-    api = new DataBrowserApi(new Configuration({ basePath: environment.publicApiUrl }));
-    // oldCom = new QuickSearchComponent();
+
     domainInfo: any;
-    constructor(public tooltipService: TooltipService, public injector: Injector) {
-        super(injector);
+    constructor() {
+        super(dBHomeComponent, []);
     }
 
-    public async getDomainInfo() {
-        return await this.api.getDomainTotals('', 1, 1).then(
-            result => {
-                return {
-                    surveyInfo: result.surveyModules,
-                    domainInfo: result.domainInfos
-                };
 
-            }
-        );
-    }
-    public async render() {
-        const domainInfo = await this.getDomainInfo();
-        const physicalMeasurementDomainInfo = domainInfo.domainInfo.filter(domain => {
-            return domain.name.toLowerCase() === 'physical measurements'
-                || domain.name.toLowerCase() === 'fitbit';
-        });
-
-
-        domainInfo.domainInfo = domainInfo.domainInfo.filter(
-            domain => domain.name.toLowerCase() !== 'physical measurements' &&
-                domain.name.toLowerCase() !== 'fitbit');
-        return ReactDOM.render(
-            <React.Fragment>
-                <section className='results'>
-                    <h5 className='result-heading secondary-display'> EHR Domains:</h5>
-                    <div id='survey' className='result-boxes'>
-                        {
-                            domainInfo.domainInfo.map((domain, index) => {
-                                const key = 'domain' + index;
-                                return <ResultLinksComponent key={key} {...domain} />;
-
-                            })
-
-                        }
-                    </div>
-                    <h5 className='result-heading secondary-display'>Survey Questions:</h5>
-                    <div className='result-boxes'>
-                        {
-                            domainInfo.surveyInfo.map((survey, index) => {
-                                const key = 'survey' + index;
-                                return <ResultLinksComponent key={key} {...survey} />;
-                            })
-
-                        }
-                    </div>
-                    <h5 className='result-heading secondary-display'>
-                        Physical Measurements and Wearables:</h5>
-                    <div className='result-boxes'>
-                        {
-                            physicalMeasurementDomainInfo.map((phyMeasurements, index) => {
-                                const key = 'phyMeasurements' + index;
-                                return <ResultLinksComponent key={key} {...phyMeasurements} />;
-                            })
-
-                        }
-                    </div>
-                </section>
-
-            </React.Fragment >, this.containerRef.nativeElement);
-
-    }
 }
 
 
