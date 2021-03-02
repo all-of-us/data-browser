87a1f373c58c610d0b1711f9e5c04c0a55367d13
srushti <srushti.gangireddy@gmail.com>
2021-03-02 13:41:41 -0600

    Adding one test

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaDao.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CBCriteriaDao extends CrudRepository<DbCriteria, Long> {
+    // TODO Rewrite this method in JPQL.
     @Query(value = "select c.id, c.parent_id, c.domain_id, c.is_standard, c.type, c.subtype, c.concept_id, c.code, c.name, c.value, c.est_count, c.is_group, c.is_selectable, c.has_attribute, c.has_hierarchy, c.has_ancestor_data, c.path, c.synonyms, co.can_select" +
             " from cb_criteria c join concept co on c.concept_id=co.concept_id where c.concept_id = :conceptId and match(c.synonyms) against(:matchWord in boolean mode) and c.domain_id=:domain\n" +
             "union all\n" +
