aae05c37dd6d5d42e74cadb7ad685c20b89d4e5c
srushti <srushti.gangireddy@gmail.com>
2021-03-02 12:58:33 -0600

    Adding one test

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaDao.java
@@ -1,7 +1,6 @@
 package org.pmiops.workbench.cdr.dao;
 
 import org.pmiops.workbench.cdr.model.DbCriteria;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -14,10 +13,10 @@ public interface CBCriteriaDao extends CrudRepository<DbCriteria, Long> {
             "union all\n" +
             "select c.id, c.parent_id, c.domain_id, c.is_standard, c.type, c.subtype, c.concept_id, c.code, c.name, c.value, c.est_count, c.is_group, c.is_selectable, c.has_attribute, c.has_hierarchy, c.has_ancestor_data, c.path, c.synonyms, co.can_select\n" +
             " from cb_criteria c join concept co on c.concept_id=co.concept_id where parent_id in (select distinct id from cb_criteria where concept_id=:conceptId and match(synonyms) against(:matchWord in boolean mode) and domain_id=:domain)\n" +
-            "order by cast(est_count as unsigned) desc", nativeQuery = true)
+            "order by est_count desc", nativeQuery = true)
     List<DbCriteria> findParentCounts(@Param("conceptId") String conceptId,@Param("domain") String domain,@Param("matchWord") String matchWord);
 
     @Query(value = "select c.id, c.parent_id, c.domain_id, c.is_standard, c.type, c.subtype, c.concept_id, c.code, c.name, c.value, c.est_count, c.is_group, c.is_selectable, c.has_attribute, c.has_hierarchy, c.has_ancestor_data, c.path, c.synonyms, co.can_select" +
-            " from cb_criteria c join concept co on co.concept_id=c.concept_id  where parent_id = :parentId order by cast(est_count as unsigned) desc", nativeQuery=true)
+            " from cb_criteria c join concept co on co.concept_id=c.concept_id  where parent_id = :parentId order by est_count desc", nativeQuery=true)
     List<DbCriteria> findCriteriaChildren(@Param("parentId") Long parentId);
 }
