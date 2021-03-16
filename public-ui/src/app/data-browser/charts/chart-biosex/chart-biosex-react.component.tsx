7385df04feeb331028b0eb7dee3b22a92135be61
srushti <srushti.gangireddy@gmail.com>
2021-03-16 10:49:17 -0500

    wip

--- public-ui/src/app/data-browser/charts/chart-biosex/chart-biosex-react.component.tsx
+++ public-ui/src/app/data-browser/charts/chart-biosex/chart-biosex-react.component.tsx
@@ -39,6 +39,15 @@ export class BioSexChartReactComponent extends React.Component<Props, State> {
   }
 
   getChartOptions() {
+    baseOptions.chart.type = 'bar';
+    baseOptions.plotOptions.series.pointWidth = 50;
+    baseOptions.yAxis.title.text = 'Participant Count';
+    baseOptions.xAxis.title.text = '';
+    baseOptions.yAxis.title.style.fontSize = '14px';
+    baseOptions.xAxis.title.style.fontSize = '14px';
+    baseOptions.yAxis.title.style.color = '#262262';
+    baseOptions.yAxis.gridLineWidth = 1;
+    baseOptions.yAxis.gridLineColor = '#F0F0F0';
     this.setState({options: baseOptions});
   }
 
--- public-ui/src/app/data-browser/charts/react-base-chart/base-chart.component.tsx
+++ public-ui/src/app/data-browser/charts/react-base-chart/base-chart.component.tsx
@@ -4,15 +4,116 @@ import * as highCharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
 export const baseOptions = {
+  style: {
+        fontFamily: 'GothamBook, Arial, sans-serif',
+        fontSize: '14px',
+  },
   chart: {
-    type: 'spline'
+        type: '',
+        backgroundColor: 'transparent'
   },
-  title: {
-    text: 'My chart'
+  tooltip: {
+      followPointer: true,
+      formatter: function () {
+        if (this.point.y <= 20) {
+          if (this.point.analysisId === 3101 || this.point.analysisId === 3102) {
+            this.point.toolTipHelpText =
+              this.point.toolTipHelpText.replace('Medical Concept, Count:</b> 20',
+                'Medical Concept, Count:</b> &le; 20');
+          } else if (this.point.analysisId === 'topConcepts' || this.point.analysisId === 'sources') {
+            this.point.toolTipHelpText =
+              this.point.toolTipHelpText.replace('Participant Count: <b>20',
+                'Participant Count: <b>&le; 20 </b>');
+          }
+        }
+        return '<div class="tooltip-container" style="position: absolute; z-index: 220;">'
+          + this.point.toolTipHelpText + '</div>';
+      },
+      positioner: function(width, height, point) {
+        const columnWidth = this.chart.series[0].options.pointWidth;
+        return {
+          x: point.plotX + this.chart.plotLeft,
+          y: point.plotY - columnWidth / 2 + this.chart.plotTop - height
+        };
+      },
+      useHTML: true,
+      enabled: true,
+      borderColor: '#262262',
+      borderRadius: '1px',
+      backgroundColor: '#FFFFFF',
+      style: {
+        color: '#302C71',
+      }
+    },
+    colors: ['#2691D0'],
+    title: {
+               text: '',
+               style: {
+                 color: '#666',
+                 fontSize: '12px',
+               }
+    },
+    xAxis: {
+        categories: [],
+        labels: {
+        style: {
+            fontSize: '12px',
+            whiteSpace: 'wrap',
+            textOverflow: 'ellipsis',
+            color: '#262262'
+        },
+    },
+    title: {
+               text: '',
+               style: {
+                 color: '#262262',
+                 fontWeight: 'bold',
+                 whiteSpace: 'wrap',
+                 textOverflow: 'ellipsis',
+                 fontSize: ''
+               }
+             },
+    tickLength: 0,
+    lineWidth: 1,
+    lineColor: '#979797',
   },
-  series: [
-    {
-      data: [1, 2, 1, 4, 3, 6]
-    }
-  ]
+  yAxis: {
+          title: {
+                     text: '',
+                     style: {
+                       color: '#262262',
+                       fontSize: '12px',
+                       whiteSpace: 'wrap',
+                       textOverflow: 'ellipsis'
+                     }
+                   },
+          tickLength: 0,
+          lineWidth: 1,
+          lineColor: '#979797',
+          gridLineColor: 'transparent',
+          gridLineWidth: 0,
+          labels: {
+            style: {
+              fontSize: '12px',
+              whiteSpace: 'wrap',
+              textOverflow: 'ellipsis',
+              color: '#262262'
+            },
+          }
+  },
+  legend: { enabled: false },
+  credits: { enabled: false },
+  plotOptions: {
+          series: {
+            animation: {
+              duration: 100,
+            },
+            pointWidth: 0
+          },
+          bar: {
+                   shadow: false,
+                   borderColor: null,
+                 }
+        },
+  series: [{ data: [] }]
 };
