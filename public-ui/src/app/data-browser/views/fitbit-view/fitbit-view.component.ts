8e4b9b5e23712ccb18896f48a7341eb3e66e3a95
srushti <srushti.gangireddy@gmail.com>
2021-03-22 15:41:41 -0500

    fitbit component age chart

--- public-ui/src/app/data-browser/views/fitbit-view/fitbit-view.component.ts
+++ public-ui/src/app/data-browser/views/fitbit-view/fitbit-view.component.ts
@@ -106,7 +106,6 @@ export class FitbitViewComponent implements OnInit {
   }
 
   setGraphs(conceptObj) {
-    console.log('am i called abcc');
     this.analyses.forEach(concept => {
       if (conceptObj.conceptName === concept.conceptId) {
         this.selectedAnalyses = concept;
