2eda5077feb6b8c0b95692e680838f9caed48fae
srushti <srushti.gangireddy@gmail.com>
2021-04-08 14:52:04 -0500

    creating react component

---
+++ public-ui/src/app/utils/spinner.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+
+export const spinnerCss = `
+.spinner-container {
+    width:100%;
+    display: flex;
+    height: auto;
+    align-content: center;
+    align-items: center;
+    justify-content: center;
+}
+
+.loading-dots {
+    margin-top:.25rem;
+    width:2rem;
+    display:flex;
+    height:auto;
+    justify-content:space-between;
+  }
+  .loading-dots .dot {
+    width:.25rem;
+    height:.25rem;
+    background:transparent;
+    border-radius: 50%;
+    animation:load 1s linear infinite alternate;
+  }
+  .loading-dots .dot:first-of-type{
+    animation-delay: .25s;
+  }
+  .loading-dots .dot:nth-of-type(2){
+    animation-delay: .5s;
+  }
+  .loading-dots .dot:nth-of-type(3){
+    animation-delay: .75s;
+  }
+  .loading-dots .dot:nth-of-type(4){
+    animation-delay: 1s;
+  }
+  a:link,a:visited,a{
+    color:#2aa3d8;
+  }
+  @keyframes load {
+    from{background:transparent}
+    to{background:#302c70}
+  }
+`;
+
+
+interface SpinnerProps {
+  loading: boolean;
+  dots: boolean;
+}
+
+export const Spinner = ({loading, dots}: SpinnerProps) => {
+    return <React.Fragment>
+        <style>{spinnerCss}</style>
+        {loading && !dots ?
+            <div className='spinner-container'>
+                <span className='spinner'></span>
+            </div>
+        :
+            loading && dots ?
+                <div className='loading-dots'>
+                    <div className='dot'></div>
+                    <div className='dot'></div>
+                    <div className='dot'></div>
+                    <div className='dot'></div>
+                </div> : null}
+    </React.Fragment>;
+};
