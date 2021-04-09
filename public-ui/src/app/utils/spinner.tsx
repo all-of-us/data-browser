e4626540d3b711a4d2d4179ae94e467ae97e0ffb
srushti <srushti.gangireddy@gmail.com>
2021-04-09 09:31:23 -0500

    seperating spinner and loading

--- public-ui/src/app/utils/spinner.tsx
+++ public-ui/src/app/utils/spinner.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
 
+export const spinnerContainerCss = `
+a:link,a:visited,a{
+    color:#2aa3d8;
+}
+@keyframes load {
+    from{background:transparent}
+    to{background:#302c70}
+}`;
+
 export const spinnerCss = `
 .spinner-container {
     width:100%;
@@ -8,63 +17,61 @@ export const spinnerCss = `
     align-content: center;
     align-items: center;
     justify-content: center;
-}
+}`;
 
+export const loadingCss = `
 .loading-dots {
     margin-top:.25rem;
     width:2rem;
     display:flex;
     height:auto;
     justify-content:space-between;
-  }
-  .loading-dots .dot {
+}
+.loading-dots .dot {
     width:.25rem;
     height:.25rem;
     background:transparent;
     border-radius: 50%;
     animation:load 1s linear infinite alternate;
-  }
-  .loading-dots .dot:first-of-type{
+}
+.loading-dots .dot:first-of-type{
     animation-delay: .25s;
-  }
-  .loading-dots .dot:nth-of-type(2){
+}
+.loading-dots .dot:nth-of-type(2){
     animation-delay: .5s;
-  }
-  .loading-dots .dot:nth-of-type(3){
+}
+.loading-dots .dot:nth-of-type(3){
     animation-delay: .75s;
-  }
-  .loading-dots .dot:nth-of-type(4){
+}
+.loading-dots .dot:nth-of-type(4){
     animation-delay: 1s;
-  }
-  a:link,a:visited,a{
-    color:#2aa3d8;
-  }
-  @keyframes load {
-    from{background:transparent}
-    to{background:#302c70}
-  }
-`;
+}`;
 
-
-interface SpinnerProps {
-  loading: boolean;
-  dots: boolean;
+export const Spinner = () => {
+    return <React.Fragment>
+            <style>{spinnerCss}</style>
+            <div className='spinner-container'>
+            <span className='spinner'></span>
+           </div>
+           </React.Fragment>;
 }
 
-export const Spinner = ({loading, dots}: SpinnerProps) => {
+export const LoadingDots = () => {
     return <React.Fragment>
-        <style>{spinnerCss}</style>
-        {loading && !dots ?
-            <div className='spinner-container'>
-                <span className='spinner'></span>
-            </div>
-        :
-            loading && dots ?
-                <div className='loading-dots'>
-                    <div className='dot'></div>
-                    <div className='dot'></div>
-                    <div className='dot'></div>
-                    <div className='dot'></div>
-                </div> : null}
-    </React.Fragment>;
+            <style>{loadingCss}</style>
+            <div className='loading-dots'>
+            <div className='dot'></div>
+                <div className='dot'></div>
+                <div className='dot'></div>
+                <div className='dot'></div>
+           </div>
+           </React.Fragment>;
+}
+
+export const SpinnerContainer = ({loading, dots}) => {
+    return <React.Fragment>
+            <style>{spinnerContainerCss}</style>
+            {loading && !dots && <Spinner/>}
+            {loading && dots && <LoadingDots/>}
+           </React.Fragment>;
 };
