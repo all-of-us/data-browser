03ab8c24e4aceb6b4739d40993161b26e4120d6e
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2021-04-09 10:25:00 -0500

    [risk=no] [RW-6563] replacing spinner component with react one (#827)
    * replacing spinner component with react one
    
    * replacing spinner component with react one
    
    * creating react component
    
    * creating react component
    
    * adding eol
    
    * adding eol
    
    * seperating spinner and loading
    
    * seperating spinner and loading
    
    * seperating spinner and loading

---
+++ public-ui/src/app/utils/spinner.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+
+export const spinnerContainerCss = `
+a:link,a:visited,a{
+    color:#2aa3d8;
+}
+@keyframes load {
+    from{background:transparent}
+    to{background:#302c70}
+}`;
+
+export const spinnerCss = `
+.spinner-container {
+    width:100%;
+    display: flex;
+    height: auto;
+    align-content: center;
+    align-items: center;
+    justify-content: center;
+}`;
+
+export const loadingCss = `
+.loading-dots {
+    margin-top:.25rem;
+    width:2rem;
+    display:flex;
+    height:auto;
+    justify-content:space-between;
+}
+.loading-dots .dot {
+    width:.25rem;
+    height:.25rem;
+    background:transparent;
+    border-radius: 50%;
+    animation:load 1s linear infinite alternate;
+}
+.loading-dots .dot:first-of-type{
+    animation-delay: .25s;
+}
+.loading-dots .dot:nth-of-type(2){
+    animation-delay: .5s;
+}
+.loading-dots .dot:nth-of-type(3){
+    animation-delay: .75s;
+}
+.loading-dots .dot:nth-of-type(4){
+    animation-delay: 1s;
+}`;
+
+export const Spinner = () => {
+    return <React.Fragment>
+            <style>{spinnerCss}</style>
+            <div className='spinner-container'>
+            <span className='spinner'></span>
+           </div>
+           </React.Fragment>;
+};
+
+export const LoadingDots = () => {
+    return <React.Fragment>
+            <style>{loadingCss}</style>
+            <div className='loading-dots'>
+            <div className='dot'></div>
+                <div className='dot'></div>
+                <div className='dot'></div>
+                <div className='dot'></div>
+           </div>
+           </React.Fragment>;
+};
