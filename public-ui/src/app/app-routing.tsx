6377d856805acb796c922a6edd59ca85dd0ba88e
Will Dolbeer <WillDolbeer@VICTRs-MacBook-Pro.local>
2021-04-26 09:50:26 -0500

    use class instead of function component

--- public-ui/src/app/app-routing.tsx
+++ public-ui/src/app/app-routing.tsx
@@ -2,22 +2,17 @@ import {Component as AComponent} from '@angular/core';
 import * as React from 'react';
 
 import {AppRoute, AppRouter} from 'app/components/app-router';
-import {withRouteData} from 'app/components/app-router';
 import {BaseReactWrapper} from 'app/data-browser/base-react/base-react.wrapper';
 import {IntroVidReactComponent} from './data-browser/views/intro-vids/intro-vids-react.component';
 
-const IntroVideosPage = withRouteData(IntroVidReactComponent);
-
-export const AppRoutingComponent: React.FunctionComponent<{}> = () => {
+export const AppRoutingComponent: React.FunctionComponent = () => {
   return <AppRouter>
     <AppRoute
       path='/introductory-videos'
-      component={() => IntroVideosPage(
+      component={() => IntroVidReactComponent(
         {routeData: {
             title: 'Introductory Videos',
-            breadcrumb: {
-              value: 'introductory videos'
-            }
+            breadcrumb: 'Introductory Videos'
           }})}
     />
   </AppRouter>;
--- public-ui/src/app/data-browser/views/intro-vids/intro-vids-react.component.tsx
+++ public-ui/src/app/data-browser/views/intro-vids/intro-vids-react.component.tsx
@@ -3,30 +3,35 @@ import * as React from 'react';
 import {withRouteData} from 'app/components/app-router';
 import { Video, videos } from 'app/data-browser/services/video.service';
 
-export const IntroVidReactComponent: any = withRouteData(() => <div className='db-container'>
-    <h1 className='primary-display'
-         style={{textAlign: 'center', padding: '1rem'}}> Introductory Videos </h1>
-    {videos.map((video: Video, index) => <span key={index}>
-        <h2 className='secondary-display'>{video.title}</h2>
-        <div style={{width: '100%', textAlign: 'center', padding: '1rem', paddingBottom: '2rem'}}>
-            <video style={{width: 'calc((100%/12)*10)', height: 'auto', outline: 'none'}}
-                   poster={video.poster}
-                   controls>
-                {video.src.map((source) => <source key={source.url}
-                                                   src={source.url}
-                                                   type={source.type}/>)
-                }
-                {video.subtitles.map((sub) => <track key={sub.label}
-                                                     default={sub.default}
-                                                     label={sub.label}
-                                                     lang={sub.lang}
-                                                     src={sub.url}/>)
-                }
-                Sorry, your browser doesn't support embedded videos,
-                 but don't worry, you can <a href={video.downloadUrl}>
-                    download this video here</a>
-                and watch it with your favorite video player!
-            </video>
-        </div>
-     </span>)}
-</div>);
+export const IntroVidReactComponent = withRouteData(class extends React.Component {
+    render() {
+        return <div className='db-container'>
+            <h1 className='primary-display' style={{textAlign: 'center', padding: '1rem'}}>
+                Introductory Videos
+            </h1>
+            {videos.map((video: Video, index) => <span key={index}>
+                <h2 className='secondary-display'>{video.title}</h2>
+                <div style={{width: '100%', textAlign: 'center', padding: '1rem', paddingBottom: '2rem'}}>
+                    <video style={{width: 'calc((100%/12)*10)', height: 'auto', outline: 'none'}}
+                           poster={video.poster}
+                           controls>
+                        {video.src.map((source) => <source key={source.url}
+                                                           src={source.url}
+                                                           type={source.type}/>)
+                        }
+                        {video.subtitles.map((sub) => <track key={sub.label}
+                                                             default={sub.default}
+                                                             label={sub.label}
+                                                             lang={sub.lang}
+                                                             src={sub.url}/>)
+                        }
+                        Sorry, your browser doesn't support embedded videos,
+                         but don't worry, you can <a href={video.downloadUrl}>
+                            download this video here</a>
+                        and watch it with your favorite video player!
+                    </video>
+                </div>
+            </span>)}
+        </div>;
+    }
+});
