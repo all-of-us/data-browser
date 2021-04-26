2db02c14eecd9136d04f11bc1ccd5144edb74085
srushti gangireddy <srushti.gangireddy@gmail.com>
2021-04-26 14:08:36 -0500

    identity test

--- public-ui/src/app/data-browser/views/survey-view/survey-view.component.ts
+++ public-ui/src/app/data-browser/views/survey-view/survey-view.component.ts
@@ -276,7 +276,6 @@ export class SurveyViewComponent implements OnInit, OnDestroy {
           aCount.subQuestionFetchComplete = false;
         }
       }
-      console.log(q);
       this.addMissingResults(q, aCount, totalCount);
     });
     q.countAnalysis.results.push(this.addDidNotAnswerResult(q.conceptId, q.countAnalysis.results,
