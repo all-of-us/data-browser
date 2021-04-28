2dafa67b65c6757bdef7bd645c380974d443cd2b
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2021-04-28 14:26:46 -0500

    wip click event

--- public-ui/src/app/data-browser/views/survey-view/components/survey-question-react.component.tsx
+++ public-ui/src/app/data-browser/views/survey-view/components/survey-question-react.component.tsx
@@ -12,50 +12,16 @@ const api = new DataBrowserApi(new Configuration({ basePath: environment.publicA
 // const dbc = new DbConfigService(api);
 const styleCss =
     `
-    .survey-tbl {
-        width: 100%;
+    .see-answers {
+        font-size: 14px;
+        cursor: pointer;
+        color: #216fb4;
     }
-    .survey-tbl *{
-        font-family: "GothamBook", "Arial", sans-serif,
-        font-size: 1em
-    }
-    .survey-tbl-r.survey-tbl-head {
-        /* padding-bottom: 0; */
-        background: rgba(218, 230, 237, 0.7);
-        border: #cccccc 1px solid;
-        border-bottom: none;
-    }
-    .survey-sub-tbl .survey-tbl-exp-r:last-of-type {
-        border-bottom: none;
-    }
-    .survey-row-icon {
-        height: 1.5rem;
-        width: 1.5rem;
-        color: #2691D0;
-      }
-    .sub-question-text {
-        text-align: left;
-        font-weight: bold;
-        margin-left: 1em;
-        font-family: GothamBold, Arial, Helvetica, sans-serif;
-    }
-    .survey-row-expanded {
-        margin-top: -1em;
-        border-left: #cccccc 1px solid;
-        border-right: #cccccc 1px solid;
-        background: #f6f6f8;
-    }
-    .active-row {
-        background: #f6f6f8;
-    }
-    .sub-table-1 .sub-question-text, .sub-table-2 .sub-question-text {
-        padding-top: 1rem;
-    }
-
-    .survey-sub-table {
-        padding-left: 2em;
-        padding-bottom: 1em;
-        width: 98%;
+    
+    .see-answers clr-icon {
+        width: 1.3em;
+        height: 1.3em;
+        color: #216fb4;
     }
 `;
 
@@ -68,27 +34,41 @@ interface Props {
     question: object;
     particpantCount: number;
     level: number;
-    searchTerm:string;
+    searchTerm: string;
 }
 
 interface State {
+    showAnswers: boolean;
 
 }
 
 export class SurveyQuestionReactComponent extends React.Component<Props, State> {
     constructor(props: Props, state: State) {
         super(props);
-        state = {
-
+        this.state = {
+            showAnswers: false
         }
+    }
+    showAnswers(e?) {
+        if (e && e.key != 'Enter') {
+            return
+        }
+        this.setState({
+            showAnswers: !this.state.showAnswers
+        });
     }
 
     render() {
-        const {question,searchTerm} = this.props;
-        console.log(searchTerm,'searchterm');
-        
-      
-        return <div><HighlightReactComponent searchTerm={searchTerm} text={question.conceptName}/>{searchTerm}search</div>
+        const { question, searchTerm } = this.props;
+        const { showAnswers } = this.state;
+        return <div>
+            <style>{styleCss}</style>
+            <HighlightReactComponent searchTerm={searchTerm} text={question.conceptName} />
+            <div className="see-answers body-lead" tabIndex="0" onClick={() => this.showAnswers()} onKeyPress={(e) => this.showAnswers(e)}>
+                See Answers <ClrIcon shape='caret' dir={showAnswers ? 'down' : 'right'} />
+
+            </div>
+        </div>
     }
 }
 
@@ -104,8 +84,9 @@ export class SurveyQuestionWrapperComponent extends BaseReactWrapper {
     @Input() isCopeSurvey: boolean;
     @Input() question: any;
     @Input() level: number;
+    @Input() searchTerm: string;
     constructor() {
-        super(SurveyQuestionReactComponent, ['isCopeSurvey', 'question', 'level']);
+        super(SurveyQuestionReactComponent, ['isCopeSurvey', 'question', 'level', 'searchTerm']);
     }
 
 
