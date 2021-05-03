bdeb6cc73324b82e51262846b98ac611547a09c5
srushti <srushti.gangireddy@gmail.com>
2021-05-03 10:06:48 -0500

    wip

--- public-ui/src/app/components/db-no-results/no-results-search.component.tsx
+++ public-ui/src/app/components/db-no-results/no-results-search.component.tsx
@@ -1,7 +1,12 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { BaseReactWrapper } from 'app/data-browser/base-react/base-react.wrapper';
-import { Spinner } from 'app/utils/spinner';
+import { LoadingDots } from 'app/utils/spinner';
+import { environment } from 'environments/environment';
+import { Configuration, DataBrowserApi } from 'publicGenerated/fetch';
 import * as React from 'react';
+import {useHistory} from 'react-router-dom';
+
+const api = new DataBrowserApi(new Configuration({ basePath: environment.publicApiUrl }));
 
 const styleCss =
 `
@@ -9,29 +14,133 @@ const styleCss =
   margin-top:-1rem;
   padding:1em;
 }
+h5{
+  padding-bottom:1em;
+}
+p {
+  font-family: "GothamBook", "Arial", sans-serif;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.5;
+  font-size: 16px;
+  letter-spacing: normal;
+  text-align: left;
+  color: #262262;
+}
+.loading-dots {
+    width:2rem;
+    display:flex;
+    height:auto;
+    justify-content:space-between;
+  }
+  .loading-dots .dot {
+    width:.25rem;
+    height:.25rem;
+    background:transparent;
+    border-radius: 50%;
+    animation:load 1s linear infinite alternate;
+  }
+  .loading-dots .dot:first-of-type{
+    animation-delay: .25s;
+  }
+  .loading-dots .dot:nth-of-type(2){
+    animation-delay: .5s;
+  }
+  .loading-dots .dot:nth-of-type(3){
+    animation-delay: .75s;
+  }
+  .loading-dots .dot:nth-of-type(4){
+    animation-delay: 1s;
+  }
+  a:link,a:visited,a{
+    color:#2aa3d8;
+  }
+
+  @keyframes load {
+    from{background:transparent}
+    to{background:#302c70}
+  }
 `;
 
 interface Props {
     searchValue: string;
+    measurementTestFilter: number;
+    measurementOrderFilter: number;
     // domainMatch: Function;
 }
 
-export const NoResultSearchComponent = (class extends React.Component<Props, {}> {
+interface State {
+    domainInfoResults: Array<any>;
+    surveyModuleResults: Array<any>;
+    pmResults: Array<any>;
+    loading: boolean;
+}
+
+export const NoResultSearchComponent = (class extends React.Component<Props, State> {
     constructor(props) {
         super(props);
+        this.state = {
+            domainInfoResults: [],
+            surveyModuleResults: [],
+            pmResults: [],
+            loading: false
+        };
+    }
+
+    componentDidMount() {
+        this.fetchDomainTotals();
+    }
+
+    fetchDomainTotals() {
+        const {searchValue, measurementTestFilter, measurementOrderFilter} = this.props;
+        this.setState({loading: true});
+        api.getDomainTotals(searchValue, measurementTestFilter, measurementOrderFilter).then(
+                result => {
+                    result.domainInfos = result.domainInfos.filter(domain =>
+                                        domain.standardConceptCount > 0);
+                    this.setState({domainInfoResults: result.domainInfos.filter(
+                    domain => domain.name.toLowerCase() !== 'physical measurements' &&
+                    domain.name.toLowerCase() !== 'fitbit'),
+                    surveyModuleResults: result.surveyModules,
+                    pmResults: result.domainInfos.filter(
+                    domain => domain.name.toLowerCase() !== 'physical measurements')});
+                    this.setState({loading: false});
+                    console.log(result);
+                });
+    }
+
+    handleOnClick() {
+        // const history = useHistory();
+        // useCallback(() => history.push('/ehr/conditions'), [history]);
+        console.log('handle on click');
     }
 
     render() {
         const {searchValue} = this.props;
-        const loading = true;
-        console.log('am i here not search rq3hetcw ');
         return (
             <React.Fragment>
             <style>{styleCss}</style>
-            <p>Test Text</p>
             <div className='no-results'>
-                { loading ? <p>Searching whole site for <strong>searchValue</strong></p> : null }
-                <Spinner/>
+                { this.state.loading ?
+                <div>
+                    <p>Searching whole site for <strong>{searchValue}</strong></p>
+                    <LoadingDots />
+                </div>
+                : null
+                }
+                {
+                 this.state.domainInfoResults.map((domainInfo, index) => {
+                    const key = domainInfo.name + index;
+                    return <div key={key}>{domainInfo.standardConceptCount} results available in the domain: <a onClick={handleOnClick}>{domainInfo.name}</a></div>;
+                 })
+                }
+                {
+                this.state.surveyModuleResults.map((surveyInfo, index) => {
+                    const key = surveyInfo.name + index;
+                    return <div key={key}>{surveyInfo.questionCount} related questions in survey: <a>{surveyInfo.name}</a></div>;
+                })
+                }
             </div>
             </React.Fragment>
         );
@@ -46,9 +155,11 @@ export const NoResultSearchComponent = (class extends React.Component<Props, {}>
 
 export class NoResultSearchWrapperComponent extends BaseReactWrapper {
   @Input() public searchValue: string;
+  @Input() public measurementTestFilter: number;
+  @Input() public measurementOrderFilter: number;
   // @Input() public domainMatch: function;
 
   constructor() {
-    super(NoResultSearchComponent, ['searchValue']);
+    super(NoResultSearchComponent, ['searchValue', 'measurementTestFilter', 'measurementOrderFilter']);
   }
 }
