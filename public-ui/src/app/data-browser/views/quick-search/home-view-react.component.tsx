a8a31f1636beddd8822a7101d6448194f284b77d
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2021-05-10 14:36:44 -0500

    [RW-5991][risk=no] Home-view (#848)
    * changing to reactStyles
    
    * lint
    
    * WIP
    
    * created global reactStyles
    
    * env
    
    * lints
    
    * change to root
    
    * popup wip
    
    * fixing HighlightReactComponent
    
    * popUP compoent reactStyles
    
    * wip
    
    * btn hover
    
    * WIP Adding CDR info component
    
    * removed old css
    
    * env
    
    * Lint
    
    * camel case
    
    * Update public-ui/src/app/data-browser/views/quick-search/home-view-react.component.tsx
    
    Co-authored-by: dolbeew <40036095+dolbeew@users.noreply.github.com>
    
    * Update public-ui/src/app/data-browser/views/quick-search/home-view-react.component.tsx
    
    Co-authored-by: dolbeew <40036095+dolbeew@users.noreply.github.com>
    
    * Update public-ui/src/app/data-browser/views/quick-search/home-view-react.component.tsx
    
    Co-authored-by: dolbeew <40036095+dolbeew@users.noreply.github.com>
    
    * Update public-ui/src/app/data-browser/views/quick-search/home-view-react.component.tsx
    
    Co-authored-by: dolbeew <40036095+dolbeew@users.noreply.github.com>
    
    * Update public-ui/src/app/data-browser/views/quick-search/home-view-react.component.tsx
    
    Co-authored-by: dolbeew <40036095+dolbeew@users.noreply.github.com>
    
    * Update public-ui/src/app/data-browser/views/quick-search/home-view-react.component.tsx
    
    Co-authored-by: dolbeew <40036095+dolbeew@users.noreply.github.com>
    
    * Update public-ui/src/app/data-browser/views/quick-search/home-view-react.component.tsx
    
    Co-authored-by: dolbeew <40036095+dolbeew@users.noreply.github.com>
    
    * lint
    
    Co-authored-by: dolbeew <40036095+dolbeew@users.noreply.github.com>

--- public-ui/src/app/data-browser/views/quick-search/home-view-react.component.tsx
+++ public-ui/src/app/data-browser/views/quick-search/home-view-react.component.tsx
@@ -1,21 +1,155 @@
-import {
-    Component,
-    ElementRef,
-    ViewChild,
-    ViewEncapsulation
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { BaseReactWrapper } from 'app/data-browser/base-react/base-react.wrapper';
+import { CdrVersionReactComponent } from 'app/data-browser/cdr-version/cdr-version-info';
+import { TooltipReactComponent } from 'app/data-browser/components/tooltip/tooltip-react.component';
 import { SearchComponent } from 'app/data-browser/search/home-search.component';
+import { PopUpReactComponent } from 'app/shared/components/pop-up/PopUpReactComponent';
+import { reactStyles } from 'app/utils';
+import { globalStyles } from 'app/utils/global-styles';
 import { environment } from 'environments/environment';
 import _ from 'lodash';
 import { Configuration, DataBrowserApi } from 'publicGenerated/fetch';
 import * as React from 'react';
 import { FunctionComponent } from 'react';
 
-import { TooltipReactComponent } from 'app/data-browser/components/tooltip/tooltip-react.component';
-
-const containerElementName = 'myReactComponentContainer';
 const api = new DataBrowserApi(new Configuration({ basePath: environment.publicApiUrl }));
+
+
+
+const css = `
+.disclaimer-btn {
+    padding: 1rem 2rem;
+    color: #f9f9fa;
+    text-transform: uppercase;
+    border-radius: 0.3rem;
+    background: #816492;
+}
+
+.disclaimer-btn:hover {
+    background: #262262;
+    color: #fff;
+  }
+
+  .icons img {
+    width: 100px;
+}
+
+.icons:first-of-type {
+    margin-left: 0;
+}
+.icons {
+    width: 100px;
+    margin-left: 1.5rem;
+    text-align: center;
+}
+.icons img {
+    text-align: center;
+}
+.result-bottom-link {
+    font-size: 15px;
+    color: #337ab7;
+    cursor: pointer;
+}
+
+.result-bottom-link:hover {
+    color: #262262;
+}
+    `;
+const styles = reactStyles({
+    searchIconLayout: {
+        display: 'grid',
+        gridTemplateColumns: '50% 50%',
+        padding: '1em'
+    },
+    results: {
+        padding: '18px'
+    },
+    resultBoxes: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'flex-start',
+        marginBottom: '48px',
+        flexGrow: 1
+    },
+    resultBox: {
+        cursor: 'pointer',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        width: 'calc(((100% / 12) * 3) - 18px)',
+        marginRight: '18px',
+        marginBottom: '18px',
+        borderRadius: '5px',
+        backgroundColor: '#ffffff',
+        boxShadow: '0 4px 6px 0 rgba(0, 0, 0, 0.15)'
+    },
+    resultBoxTitle: {
+        color: '#337ab7',
+        fontFamily: 'Arial, sans-serif',
+        display: 'flex',
+        padding: '18px',
+        paddingBottom: '3px',
+        margin: '0',
+        fontSize: '16px'
+    },
+    resultBody: {
+        color: '#302c71',
+        fontFamily: 'GothamBook, Arial, sans-serif',
+        fontSize: '14px',
+        padding: '18px',
+        paddingTop: '0px',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    resultBoxLink: {
+        padding: '18px',
+        paddingTop: '0'
+    },
+    resultHeading: {
+        fontSize: '23px',
+        margin: '0',
+        padding: '9px',
+        paddingLeft: '0'
+    },
+    resultBodyItem: {
+        padding: '6px 0'
+    },
+    resultStat: {
+        color: '#302c71',
+        fontFamily: 'GothamBook, Arial, sans-serif',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        fontSize: '35px',
+        lineHeight: '1em'
+    },
+    dBTitle: {
+        textAlign: 'center',
+        margin: 0,
+        padding: '18px'
+    },
+    dBDesc: {
+        padding: '18px',
+        paddingBottom: '63px',
+        margin: '0 auto',
+        lineHeight: '2',
+        fontFamily: 'GothamBook, Arial, sans-serif',
+        fontSize: '20px'
+    },
+    dbSubDesc: {
+        padding: '2rem',
+        textAlign: 'center'
+    },
+    iconLinks: {
+        position: 'relative',
+        top: '2rem',
+        display: 'flex',
+        alignItems: 'baseline',
+        justifyContent: 'center',
+        width: '100%'
+    }
+
+
+});
 
 export const ResultLinksComponent: FunctionComponent<any> =
     ({
@@ -25,11 +159,11 @@ export const ResultLinksComponent: FunctionComponent<any> =
         standardConceptCount,
         participantCount
     }): any => {
-        return <div className='result-box'>
-            <p className='result-box-title'>{name}</p>
-            <div className='result-body'>
-                <span className='result-body-item'>
-                    <div className='result-stat'>
+        return <div style={styles.resultBox}>
+            <p style={styles.resultBoxTitle}>{name}</p>
+            <div style={styles.resultBody}>
+                <span style={styles.resultBodyItem}>
+                    <div style={styles.resultStat}>
                         {standardConceptCount}{questionCount}
                     </div>
                     <span>matching medical concepts</span>
@@ -37,15 +171,15 @@ export const ResultLinksComponent: FunctionComponent<any> =
                 </span>
                 {
                     (questionCount &&
-                        <div className='result-body-item'>
+                        <div style={styles.resultBodyItem}>
                             <span>{description}</span>
                         </div>)
                 }
-                <span className='result-body-item result-body-stat' >
+                <span style={styles.resultBodyItem} >
                     <span><strong> {participantCount}</strong> participants in this domain</span>
                 </span>
             </div>
-            <div className='result-box-link'>
+            <div style={styles.resultBoxLink}>
                 {(questionCount ? <a className='result-bottom-link'>View Complete Survey</a> :
                     <a className='result-bottom-link'>View {name}</a>)}
             </div>
@@ -55,10 +189,11 @@ export const ResultLinksComponent: FunctionComponent<any> =
     };
 
 interface State {
-    surveyInfo: Array<any>;
-    domainInfo: Array<any>;
-    physicalMeasurementsInfo: Array<any>;
+    surveyInfo: any[];
+    domainInfo: any[];
+    physicalMeasurementsInfo: any[];
     searchWord: string;
+    popUp: boolean;
 }
 
 export const dBHomeComponent = (
@@ -69,15 +204,20 @@ export const dBHomeComponent = (
                 surveyInfo: [],
                 domainInfo: [],
                 physicalMeasurementsInfo: [],
-                searchWord: ''
+                searchWord: '',
+                popUp: false
             };
         }
 
         search = _.debounce((val) => this.getDomainInfos(), 1000);
 
         handleChange(val) {
-          this.setState({searchWord: val});
-          this.search(val);
+            this.setState({ searchWord: val });
+            this.search(val);
+        }
+        iconClickEvent(iconString: string) {
+            // dbc.triggerEvent('HelpEvent', 'Help', 'Click',
+            //     iconString, null, null);
         }
 
         // life cycle hook
@@ -90,7 +230,7 @@ export const dBHomeComponent = (
             return api.getDomainTotals(this.state.searchWord, 1, 1).then(
                 result => {
                     result.domainInfos = result.domainInfos.filter(domain =>
-                    domain.standardConceptCount > 0);
+                        domain.standardConceptCount > 0);
                     const domainInfo = result.domainInfos.filter(
                         domain => domain.name.toLowerCase() !== 'physical measurements' &&
                             domain.name.toLowerCase() !== 'fitbit');
@@ -98,56 +238,109 @@ export const dBHomeComponent = (
                         return domain.name.toLowerCase() === 'physical measurements'
                             || domain.name.toLowerCase() === 'fitbit';
                     });
-                    this.setState({ domainInfo: domainInfo, surveyInfo: result.surveyModules,
-                    physicalMeasurementsInfo: physicalMeasurementsInfo });
+                    this.setState({
+                        domainInfo: domainInfo, surveyInfo: result.surveyModules,
+                        physicalMeasurementsInfo: physicalMeasurementsInfo
+                    });
                 }
             );
         }
+        closePopUp() {
+            this.setState({
+                popUp: !this.state.popUp
+            });
+        }
 
         render() {
-            return <React.Fragment> <div style={{margin: '0 1em'}}>
-            <SearchComponent value={this.state.searchWord} onChange={(val) => {
-            this.handleChange(val); }}
-            onClear={() => { this.handleChange(''); }} />
-            </div>
-            <section className='results'>
-                <h5 className='result-heading secondary-display'> EHR Domains:</h5>
-                <TooltipReactComponent
-                    label='Homepage Tooltip Hover'
-                    searchTerm={this.state.searchWord}
-                    action='Tooltip Home Page EHR Domains'
-                    tooltipKey='ehrDomainHelpText' />
-                <div id='survey' className='result-boxes'>
-                    {
-                        this.state.domainInfo.map((domain, index) => {
-                            const key = 'domain' + index;
-                            return <ResultLinksComponent key={key} {...domain} />;
+            const { domainInfo, physicalMeasurementsInfo, surveyInfo, searchWord, popUp } = this.state;
+            return <React.Fragment>
+                <style>{css}</style>
+                <h1 style={{ ...globalStyles.primaryDisplay, ...styles.dBTitle }}>Data Browser</h1>
+                <p style={{ ...styles.dBDesc, ...globalStyles.bodyLead }}>
+                    The Data Browser provides interactive views of the publicly available<i>All of Us </i>
+                    Research Program participant data. Currently, participant provided information, including surveys and physical
+                    measurements taken at the time of participant enrollment, as well as electronic health record data (EHR) are available.
+                EHR data are reported by health care providers and are not participant reported. The <i>All of Us </i>
+                    Research Program data will include more data types over time.<br/><br/>
+                    In order to protect participant privacy, the data are de-identified, limited to aggregate counts rounded up to counts of
+                20, and summary demographic information. For more information, please visit our FAQ page.<br/><br/>
+                    Please read the public data use statement available below for additional information about our unique dataset and how to
+                acknowledge the <i>All of Us</i> Research Program in any presentations or publications.<br/><br/>
 
-                        })
+                    <button onClick={() => this.closePopUp()} className='disclaimer-btn'>public data use statement</button>
+                </p>
+                <div style={styles.searchIconLayout}>
+                    <div>
+                        <SearchComponent value={searchWord}
+                                                           onChange={(val) => this.handleChange(val)}
+                                                           onClear={() => this.handleChange('')} />
+                        <CdrVersionReactComponent />
+                    </div>
+                    <div style={styles.iconLinks}>
+                        <div className='icons' onClick={() => this.iconClickEvent('FAQ')}>
+                            <a href={environment.researchAllOfUsUrl + '/frequently-asked-questions/#data-browser-faqs'}>
+                                <img alt='FAQs'
+                                    src='/assets/icons/icons_faqs.png' />
+                                <span className='icon-link'>FAQs</span>
+                            </a>
+                        </div>
+                        <div className='icons' onClick={() => this.iconClickEvent('Intro-Videos')}>
+                            <a>
+                                <img alt='Introductory Videos'
+                                    src='/assets/icons/icons_introductoryvideo.png' />
+                                <span className='icon-link'>Introductory Videos</span>
+                            </a>
+                        </div>
+                        <div className='icons' onClick={() => this.iconClickEvent('User-Guide')}>
+                            <a href='/assets/pdf/Databrowser_User_Guide_in_RH 5_18_20.pdf' target='_blank' ><img
+                                alt='User Guide' src='/assets/icons/icons_userguide.png' /><span
+                                    className='icon-link'>User Guide</span>
+                                </a>
+                        </div>
+                    </div>
+                </div>
 
-                    }
-                </div>
-                <h5 className='result-heading secondary-display'>Survey Questions:</h5>
-                <div className='result-boxes'>
-                    {
-                        this.state.surveyInfo.map((survey, index) => {
-                            const key = 'survey' + index;
-                            return <ResultLinksComponent key={key} {...survey} />;
-                        })
+                <section style={styles.results}>
+                    <h5 style={{ ...globalStyles.secondaryDisplay, ...styles.resultHeading }}>
+                        EHR Domains:<TooltipReactComponent
+                            label='Homepage Tooltip Hover'
+                            searchTerm={searchWord}
+                            action='Tooltip Home Page EHR Domains'
+                            tooltipKey='ehrDomainHelpText' /></h5>
 
-                    }
-                </div>
-                <h5 className='result-heading secondary-display'>
-                    Physical Measurements and Wearables:</h5>
-                <div className='result-boxes'>
-                    {
-                        this.state.physicalMeasurementsInfo.map((phyMeasurements, index) => {
-                            const key = 'phyMeasurements' + index;
-                            return <ResultLinksComponent key={key} {...phyMeasurements} />;
-                        })
-                    }
-                </div>
-            </section></React.Fragment>;
+                    <div style={styles.resultBoxes}>
+                        {
+                            domainInfo.map((domain, index) => {
+                                const key = 'domain' + index;
+                                return <ResultLinksComponent key={key} {...domain} />;
+
+                            })
+
+                        }
+                    </div>
+                    <h5 style={{ ...globalStyles.secondaryDisplay, ...styles.resultHeading }}>Survey Questions:</h5>
+                    <div style={styles.resultBoxes}>
+                        {
+                            surveyInfo.map((survey, index) => {
+                                const key = 'survey' + index;
+                                return <ResultLinksComponent key={key} {...survey} />;
+                            })
+
+                        }
+                    </div>
+                    <h5 style={{ ...globalStyles.secondaryDisplay, ...styles.resultHeading }}>
+                        Physical Measurements and Wearables:</h5>
+                    <div style={styles.resultBoxes}>
+                        {
+                            physicalMeasurementsInfo.map((phyMeasurements, index) => {
+                                const key = 'phyMeasurements' + index;
+                                return <ResultLinksComponent key={key} {...phyMeasurements} />;
+                            })
+                        }
+                    </div>
+                </section>
+                {popUp && <PopUpReactComponent helpText='HomeViewPopup' onClose={() => this.closePopUp()} />}
+            </React.Fragment >;
         }
     }
 );
@@ -155,13 +348,10 @@ export const dBHomeComponent = (
 @Component({
     // tslint:disable-next-line: component-selector
     selector: 'react-db-home',
-    template: `<span #${containerElementName}></span>`,
-    styleUrls: ['../../../styles/template.css', './quick-search.component.css'],
-    encapsulation: ViewEncapsulation.None,
+    template: `<span #root></span>`
 })
 
 export class DbHomeWrapperComponent extends BaseReactWrapper {
-    @ViewChild(containerElementName, { static: false }) containerRef: ElementRef;
 
     constructor() {
         super(dBHomeComponent, []);
--- public-ui/src/app/shared/components/pop-up/PopUpReactComponent.tsx
+++ public-ui/src/app/shared/components/pop-up/PopUpReactComponent.tsx
@@ -1,71 +1,155 @@
 import {
   Component,
   Input,
-  ViewEncapsulation
 } from '@angular/core';
 import { BaseReactWrapper } from 'app/data-browser/base-react/base-react.wrapper';
+import { reactStyles } from 'app/utils';
 import * as React from 'react';
-const containerElementName = 'root';
 
-const helptexts = { 'QuickSearchPopup': {
+const css = `
+.disclaimer-btn {
+    padding: 1rem 2rem;
+    color: #f9f9fa;
+    text-transform: uppercase;
+    border-radius: 0.3rem;
+    background: #816492;
+    float: right;
+}
+
+.disclaimer-btn:hover {
+    background: #262262;
+    color: #fff;
+  }
+    `;
+
+const styles = reactStyles({
+  dataStatement: {
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: 0,
+    left: 0,
+    background: 'rgba(0, 0, 0, 0.5)',
+    zIndex: 115,
+    fontWeight: 700,
+    color: '#302c71'
+  },
+  cardTitle: {
+    fontSze: '1em',
+    textAign: 'left',
+    paddingBottom: '1em',
+    fontWeight: 'bold',
+    color: '#302c71'
+  },
+  card: {
+    width: 'contain',
+    height: 'contain',
+    margin: '15%',
+    background: 'white',
+    position: 'relative',
+    padding: '1rem'
+  },
+  close: {
+    position: 'absolute',
+    right: '1rem',
+    top: '.5rem',
+    fontSize: '1.5em'
+  },
+  btnContainer: {
+    width: '100%',
+    position: 'relative',
+    padding: '1rem',
+    marginTop: '1.5rem'
+  },
+  disclaimerBtn: {
+    position: 'absolute',
+    right: '0',
+    color: 'white',
+    padding: '1em 2.5em',
+    background: '#262262',
+    border: 'none'
+  },
+  /* covid disclaimer */
+  copeStatement: {
+    fontSize: '0.8em'
+  },
+  copeStatementBody: {
+    paddingBottom: '1rem',
+    display: 'inline-block'
+  },
+  copeStatementBox: {
+    display: 'inline-block',
+    background: '#dae6ed',
+    borderRadius: '3px',
+    padding: '1rem'
+  }
+});
+
+const helptexts = {
+  'HomeViewPopup': {
     title: 'PUBLIC DATA USE STATEMENT',
-    statement: <div className='card-body'>
-               <i>All of Us</i> Research Program data are not representative of the population
+    statement: <div>
+      <i>All of Us</i> Research Program data are not representative of the population
                of the United States. If you present, publish, or distribute <i>All of Us</i> data,
                please include the following disclaimer:<br /> "The <i>All of Us</i> Research Program
-               includes a demographically, geographically, and medically diverse group of
-               participants, however, it is not a representative sample of the population of the
+includes a demographically, geographically, and medically diverse group of
+participants, however, it is not a representative sample of the population of the
                United States. Enrollment in the <i>All of Us</i> Research program is open to all who
-               choose to participate, and the program is committed to engaging with and encouraging
-               participation of minority groups that are historically underrepresented in biomedical
+choose to participate, and the program is committed to engaging with and encouraging
+participation of minority groups that are historically underrepresented in biomedical
                research." </div>
   },
   'EhrViewPopUp': {
     title: 'DATA DISCLAIMER',
-    statement: <div className='card-body'>
-               The <i>All of Us</i> Research Program includes a demographically, geographically, and
-               medically diverse group of participants, however, it is not a representative sample
+    statement: <div>
+      The <i>All of Us</i> Research Program includes a demographically, geographically, and
+      medically diverse group of participants, however, it is not a representative sample
                of the population of the United States. Enrollment in the <i>All of Us</i> Research
-               program is open to all who choose to participate, and the program is committed to
-               engaging with and encouraging participation of minority groups that are historically
+program is open to all who choose to participate, and the program is committed to
+engaging with and encouraging participation of minority groups that are historically
                underrepresented in biomedical research. </div>
   },
   'CopePopUp': {
     title: 'COvid-19 Participant Experience (COPE) survey',
-    statement: <div className='card-body'>
-               <div className='cope-statement'>
-               <span className='cope-statement-body'>This optional survey was released to
-               participants for completion at multiple time points during the COVID-19 pandemic.
-               As a result, a participant may have multiple data points if they completed more than
-               one survey. </span> <span className='cope-statement-body'>This survey has multiple
-               versions. Even though most of the content is consistent between versions, some
-               questions were modified. </span> <span className='cope-statement-box'><strong>Please
+    statement: <div>
+      <div style={styles.copeStatement}>
+        <span style={styles.copeStatementBody}>This optional survey was released to
+        participants for completion at multiple time points during the COVID-19 pandemic.
+        As a result, a participant may have multiple data points if they completed more than
+               one survey. </span> <span style={styles.copeStatementBody}>This survey has multiple
+versions. Even though most of the content is consistent between versions, some
+               questions were modified. </span> <span style={styles.copeStatementBox}><strong>Please
                Note:</strong><br /> While these aggregate data are available in the Data Browser
-               tool, to protect participant privacy, only select data will be available in the
-               Registered Tier dataset (i.e., data describing COVID positive status will not be
+tool, to protect participant privacy, only select data will be available in the
+Registered Tier dataset (i.e., data describing COVID positive status will not be
                made available) </span> </div> </div>
-  }};
+  }
+};
 
 
-const PopUpReactComponent =
-    (props) => {
-    return <div className='data-statement'>
-            <div className='card'>
-                <div onClick={props.onClose} className='close'>x</div>
-                   <h2 className='card-title'>{helptexts[props.helpText].title}</h2>
-                   <div className='card-body'>{helptexts[props.helpText].statement}</div>
-                   <div className='btn-container'>
-                  <button onClick={props.onClose} className='disclaimer-btn'>OK</button>
-                  </div>
-                </div>
-          </div>;
-    };
+export const PopUpReactComponent =
+  (props) => {
+    return <React.Fragment>
+      <style>{css}</style>
+      <div style={styles.dataStatement}>
+        <div style={styles.card}>
+          <div onClick={props.onClose} style={styles.close}>x</div>
+          <h2 style={styles.cardTitle}>{helptexts[props.helpText].title}</h2>
+          <div>{helptexts[props.helpText].statement}</div>
+          <div style={styles.btnContainer}>
+            <button onClick={props.onClose} className='disclaimer-btn'>OK</button>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>;
+  };
 
 @Component({
   selector: 'app-popup-react',
-  template: `<span #${containerElementName}></span>`,
-  styleUrls: ['../../../styles/template.css', './pop-up.component.css'],
-  encapsulation: ViewEncapsulation.None,
+  template: `<span #root></span>`,
 })
 
 export class PopUpWrapperComponent extends BaseReactWrapper {
