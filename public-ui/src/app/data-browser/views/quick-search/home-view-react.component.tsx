6b18f1274ed45e17ab3debf74b3a437e0f66b80b
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2021-05-10 10:35:37 -0500

    Update public-ui/src/app/data-browser/views/quick-search/home-view-react.component.tsx
    Co-authored-by: dolbeew <40036095+dolbeew@users.noreply.github.com>

--- public-ui/src/app/data-browser/views/quick-search/home-view-react.component.tsx
+++ public-ui/src/app/data-browser/views/quick-search/home-view-react.component.tsx
@@ -294,7 +294,8 @@ export const dBHomeComponent = (
                         <div className='icons' onClick={() => this.iconClickEvent('User-Guide')}>
                             <a href='/assets/pdf/Databrowser_User_Guide_in_RH 5_18_20.pdf' target='_blank' ><img
                                 alt='User Guide' src='/assets/icons/icons_userguide.png' /><span
-                                    className='icon-link'>User Guide</span></a>
+                                    className='icon-link'>User Guide</span>
+                                </a>
                         </div>
                     </div>
                 </div>
