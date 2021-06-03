c123c79242ab6a799ef71a0f4610610aa9f0ce35
srushti <srushti.gangireddy@gmail.com>
2021-06-03 16:07:03 -0500

    converting react route and search to route param

--- public-ui/src/app/app-routing.tsx
+++ public-ui/src/app/app-routing.tsx
@@ -1,9 +1,12 @@
 import {Component as AComponent} from '@angular/core';
 import * as React from 'react';
 
-import {AppRoute, AppRouter} from 'app/components/app-router';
+import {AppRoute, AppRouter, withRouteData} from 'app/components/app-router';
 import {BaseReactWrapper} from 'app/data-browser/base-react/base-react.wrapper';
 import {IntroVidReactComponent} from './data-browser/views/intro-vids/intro-vids-react.component';
+import {SurveyViewReactComponent} from 'app/data-browser/views/survey-view/survey-react-view.component';
+
+const SurveyViewPage = withRouteData(SurveyViewReactComponent);
 
 export const AppRoutingComponent: React.FunctionComponent = () => {
   return <AppRouter>
@@ -13,6 +16,22 @@ export const AppRoutingComponent: React.FunctionComponent = () => {
         {routeData: {
             title: 'Introductory Videos',
             breadcrumb: {value: 'Introductory Videos'}
+          }})}
+    />
+    <AppRoute
+      path='/survey/:id/'
+      component={() => SurveyViewPage(
+        {routeData: {
+            title: 'View Survey Questions and Answers',
+            breadcrumb: {value: ':id survey'}
+          }})}
+    />
+    <AppRoute
+      path='/survey/:id/:search'
+      component={() => SurveyViewPage(
+        {routeData: {
+            title: 'View Survey Questions and Answers',
+            breadcrumb: {value: ':id survey'}
           }})}
     />
   </AppRouter>;
