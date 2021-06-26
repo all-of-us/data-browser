011996dd160953ec16f2e0ddbc0fa9e361a734fd
srushti <srushti.gangireddy@gmail.com>
2021-06-26 00:09:03 -0500

    left join to fetch domain irrespective of result count

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/DomainInfoDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/DomainInfoDao.java
@@ -23,7 +23,7 @@ public interface DomainInfoDao extends CrudRepository<DbDomainInfo, Long> {
                   "d.domain, d.domain_id, d.name, d.description, d.concept_id, " +
                   "0 all_concept_count, c.count standard_concept_count, d.participant_count participant_count " +
                   "from domain_info d " +
-                  "join (" +
+                  "left outer join (" +
                   "  select c1.domain_id, count(*) as count " +
                   "  from (" +
                   "    (select domain_id, concept_id from concept " +
@@ -38,7 +38,7 @@ public interface DomainInfoDao extends CrudRepository<DbDomainInfo, Long> {
                   "    (select domain_id, concept_id from concept " +
                   "     where (concept_id in (?3) or concept_code = ?2) and can_select=1 and has_counts=1)) c1 " +
                   "  group by c1.domain_id) c " +
-                  "ON d.domain_id = c.domain_id and d.domain != 4" +
+                  "ON d.domain_id = c.domain_id where d.domain not in (4, 8, 10)" +
                   "    UNION DISTINCT " +
                   "    select domain,domain_id,name,description,concept_id,8 as all_concept_count," +
                   "    (select count(*) from achilles_results where analysis_id=100 and stratum_2 like CONCAT('%', ?2, '%')) as standard_concept_count,participant_count" +
