a28a19333cbf6eddb42e1fd8d5acb6d3e529e956
srushti <srushti.gangireddy@gmail.com>
2021-07-01 12:17:15 -0500

    converting styles

--- public-ui/src/app/data-browser/views/fitbit-view/fitbit-view-react.component.tsx
+++ public-ui/src/app/data-browser/views/fitbit-view/fitbit-view-react.component.tsx
@@ -21,78 +21,78 @@ const styles = reactStyles({
         padding: '2rem',
         paddingTop: '0'
     },
+    fas: {
+        paddingRight: '0.25em',
+        fontSize: '2em'
+    },
+    fmMenuItemDisplay: {
+        color: '#0079b8',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%'
+    },
+    fmMenuItem: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0.5rem',
+        fontSize: '0.8em',
+        /* border-bottom: 1px solid #262262 ; */
+        cursor: 'pointer'
+    },
+    fmMenuItemActive: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0.5rem',
+        fontSize: '0.8em',
+        /* border-bottom: 1px solid #262262 ; */
+        cursor: 'pointer',
+        background: '#dae6ed',
+        borderRadius: '3px',
+        fontFamily: 'GothamBold'
+    },
+    selectedDisplayH: {
+        paddingBottom: '1rem',
+        textTransform: 'capitalize'
+    },
     fmBodyTop: {
         display: 'flex',
         justifyContent: 'space-between'
     },
-    fmChart: {
+    fmBodyBottom: {
         paddingTop: '1rem',
+        display: 'flex',
+        justifyContent: 'center',
+        width: '100%'
+    },
+    fmBottomChart: {
         background: '#216fb40d',
-        width: '100%',
         padding: '1rem',
         borderRadius: '3px',
         minWidth: '15rem',
+        paddingTop: '1rem',
+        width: '100%'
+    },
+    fmMenuItemContainer: {
+        padding: '0.25rem 0rem',
+        borderBottom: '1px solid #262262',
+        cursor: 'pointer'
+    },
+    chartDisplayBody: {
         paddingBottom: '1rem'
+    },
+    fmChart: {
+        background: '#216fb40d',
+        width: 'calc(50% - 0.5rem)',
+        padding: '1rem',
+        borderRadius: '3px',
+        minWidth: '15rem'
     }
 });
 
 const styleCss = `
-.loading-layout {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-.fm-body h2 {
-    padding-bottom: 1rem;
-    text-transform: capitalize;
-}
-.fm-body-bottom {
-    padding-top: 1rem;
-    display: flex;
-    justify-content: center;
-}
-.fm-body-bottom {
-    padding-top: 1rem;
-    width: 100%;
-}
-
-.display-body {
-    padding-bottom: 1rem;
-}
-.fm-menu-item {
-    display: flex;
-    align-items: center;
-    padding: 0.5rem;
-    font-size: 0.8em;
-    /* border-bottom: 1px solid #262262 ; */
-    cursor: pointer;
-}
-
 .fm-menu-item-display span::first-letter {
     text-transform: uppercase;
-}
-
-.fm-menu-item-container {
-    padding: 0.25rem 0rem;
-    border-bottom: 1px solid #262262;
-    cursor: pointer;
-}
-
-.active {
-    background: #dae6ed;
-    border-radius: 3px;
-    font-family: GothamBold;
-}
-.fm-menu-item-display {
-    color: #0079b8;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-}
-.fas {
-    padding-right: 0.25em;
-    font-size: 2em;
 }
 
 @media (max-width: 900px) {
@@ -186,11 +186,11 @@ export const FitbitReactComponent = withRouteData(class extends React.Component<
                 <div className='fm-menu'>
                     { concepts && concepts.map((concept, index) => {
                         let iconClass = 'fas ' + concept.icon;
-                        let conceptClass = selectedItem.toLowerCase() === concept.displayName.toLowerCase() ? 'fm-menu-item active' : 'fm-menu-item';
-                        return <div className='fm-menu-item-container' key={index}>
-                            <div tabIndex={tabIndex} className={conceptClass} onClick={() => this.setGraphs(concept)}>
-                                <i className={iconClass}></i>
-                                <div className='fm-menu-item-display'><span>{concept.displayName}</span>
+                        let conceptClass = selectedItem.toLowerCase() === concept.displayName.toLowerCase() ? styles.fmMenuItemActive : styles.fmMenuItem;
+                        return <div className='fm-menu-item-container' style={styles.fmMenuItemContainer} key={index}>
+                            <div tabIndex={tabIndex} style={conceptClass} onClick={() => this.setGraphs(concept)}>
+                                <i className={iconClass} style={styles.fas}></i>
+                                <div className='fm-menu-item-display' style={styles.fmMenuItemDisplay}><span>{concept.displayName}</span>
                                     <TooltipReactComponent tooltipKey={concept.tooltipKey} label='Fitbit concept Hover' searchTerm={searchText} action='Fitbit concept hover'/>
                                 </div>
                             </div>
@@ -199,25 +199,25 @@ export const FitbitReactComponent = withRouteData(class extends React.Component<
                     }
                 </div>
                 <div className='fm-body' style={styles.fmBody}>
-                    <h2>{selectedDisplay} </h2>
+                    <h2 style={styles.selectedDisplayH}>{selectedDisplay} </h2>
                     <div className='fm-body-top' style={styles.fmBodyTop}>
                         <div className='fm-chart' style={styles.fmChart}>
-                            <div className='display-body'>Participants with {selectedDisplay}</div>
+                            <div className='display-body' style={styles.chartDisplayBody}>Participants with {selectedDisplay}</div>
                             {selectedAnalyses && totalCountAnalysis &&
                                 <ChartFitbitReactComponent concepts={selectedAnalyses.participantCountAnalysis} countAnalysis={totalCountAnalysis}/>
                             }
                         </div>
                         <div className='fm-chart' style={styles.fmChart}>
-                        <div className='display-body'>Sex assigned at birth</div>
+                        <div className='display-body' style={styles.chartDisplayBody}>Sex assigned at birth</div>
                         {selectedAnalyses && totalCountAnalysis && domainCountAnalysis &&
                         <BioSexChartReactComponent genderAnalysis={selectedAnalyses.genderAnalysis} genderCountAnalysis={domainCountAnalysis.genderCountAnalysis}
                                                 domain='fitbit' selectedResult={selectedResult}/>
                         }
                         </div>
                     </div>
-                    <div className='fm-body-bottom'>
-                        <div className='fm-chart' style={styles.fmChart}>
-                            <div className='display-body'>Age when physical measurement taken</div>
+                    <div className='fm-body-bottom' style={styles.fmBodyBottom}>
+                        <div className='fm-chart' style={styles.fmBottomChart}>
+                            <div className='display-body' style={styles.chartDisplayBody}>Age when physical measurement taken</div>
                             {selectedAnalyses && domainCountAnalysis &&
                                 <AgeChartReactComponent ageAnalysis={selectedAnalyses.ageAnalysis}
                                 ageCountAnalysis={domainCountAnalysis.ageCountAnalysis} domain='fitbit' selectedResult={selectedResult}/>
