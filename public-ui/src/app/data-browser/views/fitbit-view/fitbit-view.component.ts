fdc4c6b20297ae657e51cef7ac6ba389d126f371
srushti <srushti.gangireddy@gmail.com>
2021-07-01 12:39:00 -0500

    placing ang code back till we clean up code

--- public-ui/src/app/data-browser/views/fitbit-view/fitbit-view.component.ts
+++ public-ui/src/app/data-browser/views/fitbit-view/fitbit-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TooltipService } from 'app/data-browser/services/tooltip.service';
+import { DbConfigService } from 'app/utils/db-config.service';
+import { DataBrowserService } from 'publicGenerated/api/dataBrowser.service';
+import { Subscription as ISubscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-fitbit-view',
@@ -6,9 +10,109 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../../styles/template.css', './fitbit-view.component.css']
 })
 export class FitbitViewComponent implements OnInit {
-  constructor() {
+  private subscriptions: ISubscription[] = [];
+  loadingStack: any = [];
+  fitbitConcepts: any = [];
+  searchText: string = null;
+  analyses: any;
+  selectedAnalyses: any;
+  selectedItem: string;
+  selectedDisplay: string;
+  domainCountAnalysis: any;
+  totalCountAnalysis: any;
+  constructor(private api: DataBrowserService, public dbc: DbConfigService,
+    public tooltipService: TooltipService) {
+    this.selectedItem = 'any Fitbit data';
+    this.selectedDisplay = 'any Fitbit data';
+  }
+
+  loading() {
+    return this.loadingStack.length > 0;
   }
 
   ngOnInit() {
+    this.searchText = localStorage.getItem('searchText');
+    this.fitbitConcepts.push({
+      id: 1, displayName: 'any Fitbit data',
+      conceptName: 'Any Fitbit Data', icon: 'fa-watch-fitness',
+      tooltipKey: 'fitbitAllDataHelpText'
+    });
+    this.fitbitConcepts.push({
+      id: 2, displayName: 'heart rate by zone summary',
+      conceptName: 'Heart Rate (Summary)', icon: 'fa-heartbeat',
+      tooltipKey: 'fitbitHeartZoneHelpText'
+    });
+    this.fitbitConcepts.push({
+      id: 3, displayName: 'heart rate (minute-level)',
+      conceptName: 'Heart rate (minute-level)', icon: 'fa-monitor-heart-rate',
+      tooltipKey: 'fitbitHeartRateHelpText'
+    });
+    this.fitbitConcepts.push({
+      id: 4, displayName: 'activity (daily summary)',
+      conceptName: 'Activity (daily summary)', icon: 'fa-running',
+      tooltipKey: 'fitbitActivityDailyHelpText'
+    });
+    this.fitbitConcepts.push({
+      id: 5, displayName: 'activity intraday steps (minute-level)',
+      conceptName: 'Activity intraday steps (minute-level)', icon: 'fa-walking',
+      tooltipKey: 'fitbitActivityStepsHelpText'
+    });
+
+    this.loadingStack.push(true);
+    this.subscriptions.push(this.api.getFitbitAnalysisResults(this.dbc.FITBIT_MEASUREMENTS)
+      .subscribe({
+        next: result => {
+          this.analyses = result.items;
+          for (const item of result.items) {
+            const fitbitConcept = this.fitbitConcepts.filter(concept =>
+              concept.conceptName.toLowerCase().includes(item.conceptId.toLowerCase()))[0];
+            fitbitConcept['ageAnalysis'] = item.ageAnalysis;
+            fitbitConcept['genderAnalysis'] = item.genderAnalysis;
+            fitbitConcept['countAnalysis'] = item.countAnalysis;
+            this.totalCountAnalysis = item.countAnalysis;
+            fitbitConcept['participantCountAnalysis'] = item.participantCountAnalysis;
+          }
+          if (this.searchText) {
+            const matchingConcepts = this.fitbitConcepts.filter(concept =>
+              concept.conceptName.toLowerCase().includes(this.searchText.toLowerCase()));
+            if (matchingConcepts && matchingConcepts.length > 0) {
+              this.selectedItem = matchingConcepts[0].conceptName;
+              this.selectedDisplay = matchingConcepts[0].displayName;
+              this.selectedAnalyses = matchingConcepts[0];
+            }
+          }
+          this.selectedAnalyses = result.items[0];
+          // Process fitbit results
+          this.loadingStack.pop();
+        },
+        error: err => {
+          this.loadingStack.pop();
+          console.log('Error: ', err);
+        }
+      }));
+
+      this.loadingStack.push(true);
+      this.subscriptions.push(this.api.getCountAnalysis('Fitbit', 'fitbit')
+            .subscribe({
+              next: result => {
+                this.domainCountAnalysis = result;
+                this.loadingStack.pop();
+              },
+              error: err => {
+                this.loadingStack.pop();
+                console.log('Error: ', err);
+              }
+            }));
   }
+
+  setGraphs(conceptObj) {
+    this.analyses.forEach(concept => {
+      if (conceptObj.conceptName === concept.conceptId) {
+        this.selectedAnalyses = concept;
+      }
+    });
+    this.selectedItem = conceptObj.displayName;
+    this.selectedDisplay = conceptObj.displayName;
+  }
+
 }
