b65a18250893b594e6e92c3b461430ff44b63e33
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2021-09-13 13:41:59 -0500

    adding xframe options (#903)

--- public-api/src/main/java/org/pmiops/workbench/config/WebSecurityConfig.java
+++ public-api/src/main/java/org/pmiops/workbench/config/WebSecurityConfig.java
@@ -14,6 +14,7 @@ public class WebSecurityConfig extends
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.headers().contentSecurityPolicy("script-src 'self' https://trustedscripts.example.com; object-src https://trustedplugins.example.com; report-uri /csp-report-endpoint/");
+        http.headers().frameOptions().sameOrigin();
         http.csrf().disable();
         http.headers().xssProtection();
     }
