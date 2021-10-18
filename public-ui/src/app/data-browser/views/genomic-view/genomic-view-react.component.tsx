1a7e583cdba533efd0bf7d83aad2e3b565498765
srushti <srushti.gangireddy@gmail.com>
2021-10-18 11:44:01 -0500

    fetching demo data

--- public-ui/src/app/data-browser/views/genomic-view/genomic-view-react.component.tsx
+++ public-ui/src/app/data-browser/views/genomic-view/genomic-view-react.component.tsx
@@ -14,6 +14,13 @@ export const GenomicViewReactComponent = withRouteData(class extends React.Compo
                 <style>{styleCss}</style>
                 <div className='gene-container'>
                     <h1>Genomic Data</h1>
+                    <div>
+                    This section provides an overview of genomic data within the current <i>All of Us</i> dataset.
+                    Researchers can use the Participants with Genomic Data page to view currently available genomic data by
+                    participant-reported race/ethnicity, sex assigned at birth, and age. The Variant Search can be used for
+                    preliminary exploration of genetic variant allele frequencies by with select annotations and genetic
+                    ancestry associations.
+                     </div>
                 </div>
                </React.Fragment>;
        }
