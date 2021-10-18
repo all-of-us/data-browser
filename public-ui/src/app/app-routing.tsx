da328888f2ce1b773343c039595ec961a0f34ca3
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2021-10-18 09:11:40 -0500

    [no-ticket][risk=no]Reactdebt (#916)
    * routoing to home and EHR
    
    * ehr cleanup
    
    * Survey + searches react routed
    
    * linting
    
    * rewind
    
    * rewind agin
    
    * addressing comments
    
    * lint and bugs

--- public-ui/src/app/app-routing.tsx
+++ public-ui/src/app/app-routing.tsx
@@ -1,55 +1,119 @@
-import {Component as AComponent} from '@angular/core';
+import { Component as AComponent } from '@angular/core';
+import { AppRoute, AppRouter } from 'app/components/app-router';
+import { BaseReactWrapper } from 'app/data-browser/base-react/base-react.wrapper';
+import { EhrViewReactComponent } from 'app/data-browser/views/ehr-view/ehr-view-react.component';
+import { FitbitReactComponent } from 'app/data-browser/views/fitbit-view/fitbit-view-react.component';
+import { IntroVidReactComponent } from 'app/data-browser/views/intro-vids/intro-vids-react.component';
+import { PMReactComponent } from 'app/data-browser/views/pm/pm-react.component';
+import { dBHomeComponent } from 'app/data-browser/views/quick-search/home-view-react.component';
 import * as React from 'react';
-
-import {AppRoute, AppRouter} from 'app/components/app-router';
-import {BaseReactWrapper} from 'app/data-browser/base-react/base-react.wrapper';
-import {FitbitReactComponent} from 'app/data-browser/views/fitbit-view/fitbit-view-react.component';
-import {IntroVidReactComponent} from 'app/data-browser/views/intro-vids/intro-vids-react.component';
-import {PMReactComponent} from 'app/data-browser/views/pm/pm-react.component';
+import { SurveyViewReactComponent } from './data-browser/views/survey-view/survey-react-view.component';
 
 export const AppRoutingComponent: React.FunctionComponent = () => {
+
   return <AppRouter>
+    <AppRoute
+      path='/'
+      component={() => dBHomeComponent(
+        {
+          routeData: {
+            title: 'Data Browser',
+            breadcrumb: { value: '' }
+          }
+        })}
+    />
     <AppRoute
       path='/introductory-videos'
       component={() => IntroVidReactComponent(
-        {routeData: {
+        {
+          routeData: {
             title: 'Introductory Videos',
-            breadcrumb: {value: 'Introductory Videos'}
-          }})}
+            breadcrumb: { value: 'Introductory Videos' }
+          }
+        })}
+    />
+    <AppRoute
+      path='/ehr/:id'
+      component={() => EhrViewReactComponent(
+        {
+          routeData: {
+            title: 'View Full Results',
+            breadcrumb: { value: ':id Domain' }
+          }
+        })}
+    />
+    <AppRoute
+      path='/ehr/:id/:search'
+      component={() => EhrViewReactComponent(
+        {
+          routeData: {
+            title: 'View Full Results',
+            breadcrumb: { value: ':id Domain' }
+          }
+        })}
+    />
+    <AppRoute
+      path='/survey/:id'
+      component={() => SurveyViewReactComponent(
+        {
+          routeData: {
+            title: 'View Survey Questions and Answers',
+            breadcrumb: { value: ':id Domain' }
+          }
+        })}
+    />
+    <AppRoute
+      path='/survey/:id/:search'
+      component={() => SurveyViewReactComponent(
+        {
+          routeData: {
+            title: 'View Survey Questions and Answers',
+            breadcrumb: { value: ':id Domain' }
+          }
+        })}
     />
     <AppRoute
       path='/fitbit'
       component={() => FitbitReactComponent(
-        {routeData: {
+        {
+          routeData: {
             title: 'Fitbit Data',
-            breadcrumb: {value: 'Fitbit Data'}
-          }})}
+            breadcrumb: { value: 'Fitbit Data' }
+          }
+        })}
     />
     <AppRoute
       path='/fitbit/:search'
       component={() => FitbitReactComponent(
-        {routeData: {
+        {
+          routeData: {
             title: 'Fitbit Data',
-            breadcrumb: {value: 'Fitbit Data'}
-          }})}
+            breadcrumb: { value: 'Fitbit Data' }
+          }
+        })}
     />
     <AppRoute
-      path='/physical-measurements'
+      path='/physical-measurements/'
       component={() => PMReactComponent(
-        {routeData: {
+        {
+          routeData: {
             title: 'Physical Measurements',
-            breadcrumb: {value: 'physical measurements'}
-          }})}
+            breadcrumb: { value: 'physical measurements' }
+          }
+        })}
     />
     <AppRoute
       path='/physical-measurements/:search'
       component={() => PMReactComponent(
-        {routeData: {
+        {
+          routeData: {
             title: 'Physical Measurements',
-            breadcrumb: {value: 'physical measurements'}
-          }})}
+            breadcrumb: { value: 'physical measurements' }
+          }
+        })}
     />
   </AppRouter>;
+
 };
 
 @AComponent({
