f8c23406b631d607b5f5066d1c0b51eb10f4e349
srushti <srushti.gangireddy@gmail.com>
2021-10-19 16:15:21 -0500

    getting page count from api'

--- public-api/src/main/java/org/pmiops/workbench/publicapi/GenomicsController.java
+++ public-api/src/main/java/org/pmiops/workbench/publicapi/GenomicsController.java
@@ -15,6 +15,7 @@ import org.pmiops.workbench.service.BigQueryService;
 import com.google.cloud.bigquery.FieldValue;
 import com.google.cloud.bigquery.QueryJobConfiguration;
 import com.google.cloud.bigquery.TableResult;
+import com.google.common.base.Strings;
 
 @RestController
 public class GenomicsController implements GenomicsApiDelegate {
@@ -52,9 +53,39 @@ public class GenomicsController implements GenomicsApiDelegate {
         } catch(NullPointerException ie) {
             throw new ServerErrorException("Cannot set default cdr version");
         }
-        // TODO Add search check and filter count
-        String COUNT_SQL_TEMPLATE = "SELECT count(*) as count\n" +
-                "FROM `${projectId}.${dataSetId}.wgs_variant_mv`\n";
+        String COUNT_SQL_TEMPLATE = "";
+        String genomicRegionRegex = "(chr([0-9]{1,})*[XY]*:).*";
+        String variantIdRegex = "\\d{1,}-\\d{5,}-[A,C,T,G]{1,}-[A,C,T,G]{1,}";
+
+        // Make sure the search term is not empty
+        if (!Strings.isNullOrEmpty(variantSearchTerm)) {
+            // Check if the search term matches genomic region search term pattern
+            if (variantSearchTerm.matches(genomicRegionRegex)) {
+                String[] regionTermSplit = variantSearchTerm.split(":");
+                COUNT_SQL_TEMPLATE = "SELECT count(*) as count\n" + "FROM `${projectId}.${dataSetId}.wgs_variant_mv` where contig= \"" + regionTermSplit[0] + "\"";
+                if (regionTermSplit.length > 1) {
+                    String[] rangeSplit = regionTermSplit[1].split("-");
+                    try {
+                        if (rangeSplit.length == 2) {
+                            Long low = Math.min(Long.valueOf(rangeSplit[0]), Long.valueOf(rangeSplit[1]));
+                            Long high = Math.max(Long.valueOf(rangeSplit[0]), Long.valueOf(rangeSplit[1]));
+                            COUNT_SQL_TEMPLATE = "SELECT count(*) as count\n" +
+                                    "FROM `${projectId}.${dataSetId}.wgs_variant_mv` where contig= \"" + regionTermSplit[0] + "\" and position <= " + high + " and position >= " + low + "\n";
+                        }
+                    } catch(NumberFormatException e) {
+                        System.out.println("Trying to convert bad number.");
+                    }
+                }
+            } else if (variantSearchTerm.matches(variantIdRegex)) {
+                // Check if the search term matches variant id pattern
+                COUNT_SQL_TEMPLATE = "SELECT count(*) as count\n" + "FROM `${projectId}.${dataSetId}.wgs_variant_mv` where variant_id= \"" + variantSearchTerm + "\"";
+            } else {// Check if the search term matches gene coding pattern
+                COUNT_SQL_TEMPLATE = "SELECT count(*) as count\n" + "FROM `${projectId}.${dataSetId}.wgs_variant_mv` where REGEXP_CONTAINS(genes, \"" + variantSearchTerm + "\")";
+            }
+        } else {
+            COUNT_SQL_TEMPLATE = "SELECT count(*) as count\n" +
+                    "FROM `${projectId}.${dataSetId}.wgs_variant_mv`\n";
+        }
         QueryJobConfiguration qjc = QueryJobConfiguration.newBuilder(COUNT_SQL_TEMPLATE.toString())
                 .setUseLegacySql(false)
                 .build();
@@ -72,26 +103,15 @@ public class GenomicsController implements GenomicsApiDelegate {
         } catch(NullPointerException ie) {
             throw new ServerErrorException("Cannot set default cdr version");
         }
-        String COUNT_SQL_TEMPLATE = "SELECT DISTINCT variant_id, contig, position, ref_allele, alt_allele, genes\n" +
-                "FROM `${projectId}.${dataSetId}.wgs_variant_mv`\n" +
-                "WHERE contig = 'chr5'\n" +
-                "AND position >= 6000000\n" +
-                "AND position <= 8010000\n" +
-                "limit 50;";
+        String COUNT_SQL_TEMPLATE = "SELECT variant_id, gene_symbol, genes, consequence, protein_change, clinical_significance, dna_change, transcript, rs_number, allele_frequency, allele_count, allele_number\n" +
+                "FROM `${projectId}.${dataSetId}.wgs_variant_mv` \n" +
+                "limit 50 offset 0;";
         QueryJobConfiguration qjc = QueryJobConfiguration.newBuilder(COUNT_SQL_TEMPLATE.toString())
                 .setUseLegacySql(false)
                 .build();
         qjc = bigQueryService.filterBigQueryConfig(qjc);
         TableResult result = bigQueryService.executeQuery(qjc);
         Map<String, Integer> rm = bigQueryService.getResultMapper(result);
-        int count = 0;
-        for (List<FieldValue> row : result.iterateAll()) {
-            System.out.println("************ Printing results **************");
-            System.out.println(row.size());
-            count += 1;
-            System.out.println("************ Printing results **************");
-        }
-        System.out.println("Row count = " + count);
         return null;
     }
 }
