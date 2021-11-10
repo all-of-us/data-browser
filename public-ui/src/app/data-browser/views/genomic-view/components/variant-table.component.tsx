226fe9384b128684f461e45b6e79f35f7a88149f
srushti <srushti.gangireddy@gmail.com>
2021-11-10 12:20:29 -0600

    added spinner and scrolling table ino view

--- public-ui/src/app/data-browser/views/genomic-view/components/variant-table.component.tsx
+++ public-ui/src/app/data-browser/views/genomic-view/components/variant-table.component.tsx
@@ -75,7 +75,7 @@ export class VariantTableComponent extends React.Component<Props, State> {
         this.state = {
             numPages: Math.ceil(props.variantListSize / 50),
             page: 1,
-            loading: props.loading,
+            loading: props.loading ? props.loading : true,
             searchResults: props.searchResults,
             currentPage: 1
         };
