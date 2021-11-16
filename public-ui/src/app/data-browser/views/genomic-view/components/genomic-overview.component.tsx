1b2ba233c1b5773d69f272c6c403cea5e1899c82
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2021-11-15 18:49:57 -0600

    [RW-7419][risk=no] genomic overview charts (#939)
    * logic for sidebar toggle
    
    * clean up template
    
    * sideBar styled and functional
    
    * linting
    
    * cleaning up
    
    * lint
    
    * getting participant counts
    
    * fixed homebug
    
    * lint
    
    * lint
    
    * styled heading
    
    * testing
    
    * Chart styling WIP
    
    * highchart option wrk
    
    * added participantCounts to geno-chart
    
    * setup  & legend
    
    * legend layout+style
    
    * tooltip helptext set
    
    * linting
    
    * moved genomic chart options to base option
    serivice
    
    * lint

--- public-ui/src/app/data-browser/views/genomic-view/components/genomic-overview.component.tsx
+++ public-ui/src/app/data-browser/views/genomic-view/components/genomic-overview.component.tsx
@@ -4,10 +4,25 @@ import * as React from 'react';
 import { GenomicChartComponent } from './genomic-chart.component';
 
 const styles = reactStyles({
-    border: {
-        border: '1px solid',
-        margin: '1rem'
+    innerContainer: {
+        padding: '1em'
+    },
+    title: {
+        margin: '0',
+
+    },
+    desc: {
+        color: '#302C71',
+        margin: '0',
+        padding: '1em',
+        paddingLeft: '0',
+        fontSize: '.8em'
+    },
+    headingLayout: {
+        display: 'flex',
+        justifyContent: 'space-between'
     }
+
 });
 
 // tslint:disable-next-line:no-empty-interface
@@ -16,31 +31,95 @@ interface Props {
 }
 // tslint:disable-next-line:no-empty-interface
 interface State {
+    loading: boolean;
+    participantCount: string;
+    raceEthData: any;
+    sexAtBirthData: any;
+    currentAgeData: any;
+    participantCounts: any[];
 }
-
-
 
 export class GenomicOverviewComponent extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
+        this.state = {
+            loading: true,
+            participantCount: null,
+            raceEthData: [],
+            sexAtBirthData: [],
+            currentAgeData: [],
+            participantCounts: []
+        };
     }
-    getGenomicParticipantCounts() {
-        return genomicsApi().getParticipantCounts().then(result => {
-            // console.log(result);
+
+    raceEthArr: any[] = [];
+    sexAtBirthArr: any[] = [];
+    currentAgeArr: any[] = [];
+    participantCountsArr: any[] = [];
+
+    async getGenomicParticipantCounts() {
+        const result_1 = await genomicsApi().getParticipantCounts();
+        result_1.results.forEach(type => {
+            if (type.stratum4 === null) {
+                this.setState({
+                    participantCount: type.countValue.toLocaleString()
+                });
+            }
         });
     }
 
     componentDidMount() {
         this.getGenomicParticipantCounts();
+        this.getGenomicChartData();
+
+    }
+    getGenomicChartData() {
+        return genomicsApi().getChartData().then(result => {
+            result.items.forEach(item => {
+                switch (item.analysisId) {
+                    case 3503:
+                        this.raceEthArr.push(item);
+                        break;
+                    case 3501:
+                        this.sexAtBirthArr.push(item);
+                        break;
+                    case 3502:
+                        this.currentAgeArr.push(item);
+                        break;
+                    case 3000:
+                        this.participantCountsArr.push(item);
+
+                }
+            });
+            this.setState({
+                raceEthData: this.raceEthArr,
+                sexAtBirthData: this.sexAtBirthArr,
+                currentAgeData: this.currentAgeArr,
+                participantCounts: this.participantCountsArr,
+                loading: false
+            });
+        });
     }
 
+
     render() {
+        const { participantCount, raceEthData, sexAtBirthData, currentAgeData, participantCounts, loading } = this.state;
         return <React.Fragment>
-            <div style={styles.border}>
-                <p>I am Genomic Overview</p>
-                <GenomicChartComponent />
-                <GenomicChartComponent />
-                <GenomicChartComponent />
+            <div style={styles.innerContainer}>
+                <div style={styles.headingLayout}>
+                    <div>
+                        <h3 style={styles.title}>Participant Demographics</h3>
+                        <p style={styles.desc}>Demographic data is self-reported by participants</p>
+                    </div>
+                    <div>
+                        <span>{participantCount} participants</span>
+                    </div>
+                </div>
+                {!loading && <React.Fragment>
+                    <GenomicChartComponent counts={participantCounts[0]} title='Race/ethnicity' data={raceEthData[0]} />
+                    <GenomicChartComponent counts={participantCounts[0]} title='Sex assigned at birth' data={sexAtBirthData[0]} />
+                    <GenomicChartComponent counts={participantCounts[0]} title='Current age' data={currentAgeData[0]} />
+                </React.Fragment>}
             </div>
         </React.Fragment>;
     }
