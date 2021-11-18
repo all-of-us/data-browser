02c7cfe4f8bb2e12a6d1ff7228253ae7a5f4d9cc
srushti <srushti.gangireddy@gmail.com>
2021-11-18 14:11:36 -0600

    nits

--- public-ui/src/app/data-browser/views/genomic-view/components/variant-expanded.component.tsx
+++ public-ui/src/app/data-browser/views/genomic-view/components/variant-expanded.component.tsx
@@ -136,7 +136,8 @@ export class VariantExpandedComponent extends React.Component<Props, State> {
                     <span style={styles.variantId}><strong>Variant ID: </strong> {!loading ?
                     <span style={{ paddingLeft: '1em', overflowWrap: 'anywhere' }}>
                         {variant.variantId}</span> : <div style={styles.loading}><Spinner /></div>} </span>
-                    <div ><ClrIcon onClick={(e) => this.props.closed()} className='exit' shape='window-close' style={styles.closeIcon}/></div>
+                    <div ><ClrIcon onClick={(e) => this.props.closed()} className='exit' shape='window-close'
+                    style={styles.closeIcon}/></div>
                 </div>
                 {!loading && <React.Fragment><div style={styles.body}>
                     <div>
