f2fa4e1fae84ad2ae609c0a37d3b0b753dfd484a
srushti <srushti.gangireddy@gmail.com>
2021-11-22 11:20:44 -0600

    nits

--- public-ui/src/app/data-browser/views/genomic-view/components/genomic-overview.component.tsx
+++ public-ui/src/app/data-browser/views/genomic-view/components/genomic-overview.component.tsx
@@ -56,7 +56,7 @@ export class GenomicOverviewComponent extends React.Component<Props, State> {
     participantCountsArr: any[] = [];
 
     componentDidMount() {
-        //{ this.props.chartData && this.getGenomicChartData(); }
+        // { this.props.chartData && this.getGenomicChartData(); }
         this.getGenomicChartData();
     }
 
