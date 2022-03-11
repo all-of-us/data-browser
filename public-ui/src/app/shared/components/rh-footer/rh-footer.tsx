165e918431b47e9084be258275c215b1c4328864
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2022-03-11 14:48:40 -0600

    lint

--- public-ui/src/app/shared/components/rh-footer/rh-footer.tsx
+++ public-ui/src/app/shared/components/rh-footer/rh-footer.tsx
@@ -26,7 +26,7 @@ const styles = reactStyles({
   },
   nihLogo: {
     marginTop: '1rem',
-    marginLeft:' -0.5rem',
+    marginLeft: ' -0.5rem',
     maxWidth: '11rem'
   },
   siteLogo: {
@@ -132,21 +132,21 @@ const RhFooter: React.FunctionComponent = () => {
   return <footer style={styles.footer}>
     <style>{css}</style>
     <div className='db-container'>
-      <div style={{width: '100%'}}>
-        <div style={{background: 'transparent'}}>
+      <div style={{ width: '100%' }}>
+        <div style={{ background: 'transparent' }}>
           <div className='footer-logo'>
             <div style={styles.siteLogo}>
               <a href={environment.researchAllOfUsUrl} className='custom-logo-link'>
                 <img src='/assets/db-images/allofus_research-hub_white.png'
-                     style={{minWidth: '9rem'}}
-                     alt='All of Us Research Hub'/>
+                  style={{ minWidth: '9rem' }}
+                  alt='All of Us Research Hub' />
               </a>
             </div>
             <div style={styles.nihLogo}>
               <a href='https://allofus.nih.gov/' target='_blank'>
                 <img alt='National Institute of Health All of Us Research Program'
-                     style={{minWidth: '9rem'}}
-                     src='/assets/db-images/nih_allofus_white.png'/>
+                  style={{ minWidth: '9rem' }}
+                  src='/assets/db-images/nih_allofus_white.png' />
               </a>
             </div>
           </div>
@@ -185,16 +185,16 @@ const RhFooter: React.FunctionComponent = () => {
           <div className='cta-col'>
             <p>
               Learn more about becoming a participant at <a href='https://joinallofus.org'
-                                                            target='_blank'
-                                                            rel='noopener'>JoinAllofUs.org</a>
+                target='_blank'
+                rel='noopener'>JoinAllofUs.org</a>
             </p>
           </div>
           <div className='cta-col'>
             <p>
               Learn more about the program protocol, leadership, and governance at&nbsp;
               <a href='https://allofus.nih.gov'
-                 target='_blank'
-                 rel='noopener'>AllofUs.nih.gov</a>
+                target='_blank'
+                rel='noopener'>AllofUs.nih.gov</a>
             </p>
           </div>
           <div className='cta-col'>
@@ -203,7 +203,7 @@ const RhFooter: React.FunctionComponent = () => {
               {socialLinks.map((item, i) => <li key={i}>
                 <a target='_blank' rel='noopener noreferrer' href={item.url}>
                   <span className='screen-reader-text'>{item.name}</span>
-                  <img src={item.image} alt={item.name}/>
+                  <img src={item.image} alt={item.name} />
                 </a>
               </li>)}
             </ul>
@@ -215,8 +215,8 @@ const RhFooter: React.FunctionComponent = () => {
         <div className='footer-boiler'>
           <section>
             <p>Precision Medicine Initiative, PMI,&nbsp;
-               <i>All of Us</i>, the <i>All of Us</i> logo, and &#8220;The
-               Future of Health Begins With You&#8221; are service marks of the&nbsp;
+              <i>All of Us</i>, the <i>All of Us</i> logo, and &#8220;The
+              Future of Health Begins With You&#8221; are service marks of the&nbsp;
               <a href='https://www.hhs.gov/' target='_blank' rel='noopener noreferrer'>
                 U.S. Department of Health and Human Services
               </a>. The <i>All of Us</i> platform is for research only and does not&nbsp;
@@ -224,12 +224,12 @@ const RhFooter: React.FunctionComponent = () => {
             </p>
           </section>
         </div>
-        <div className='site-info'><br/>
-          Copyright { new Date().getFullYear()} |&nbsp;
+        <div className='site-info'><br />
+          Copyright {new Date().getFullYear()} |&nbsp;
           <a href='https://www.researchallofus.org/privacy-policy/'>
             Privacy Policy
           </a>
-          <span role='separator' aria-hidden='true'/>
+          <span role='separator' aria-hidden='true' />
         </div>
       </div>
     </div>
