cb791b58e03038eabf9e3b0e6bd78f60cb22c828
Srushti Gangireddy <srushtigangireddy@NLDMIMWL0RJHV2T.local>
2022-03-16 20:19:03 -0500

    query fix

--- public-api/db-cdr/generate-cdr/generate-survey-counts.sh
+++ public-api/db-cdr/generate-cdr/generate-survey-counts.sh
@@ -69,7 +69,7 @@ and
 (exists (select * from \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.v_full_observation\` where questionnaire_response_id=o.questionnaire_response_id
 and observation_source_concept_id=cast(SPLIT(sq.path, '.')[OFFSET(0)] as int64) and value_source_concept_id=cast(SPLIT(sq.path, '.')[OFFSET(1)] as int64) )
 or sq.concept_id in (1333105, 1332735, 1332734, 1310134, 1332738, 1332762, 1333324, 1333235, 1310148, 1332769, 713888, 596885, 596888, 1310135, 1310136, 1310138, 1310141, 1310144, 1310148, 1332737, 1332769, 1332793, 1332796, 1332830, 1332831,
-1333014, 1333020, 1333021, 1333022, 1333235, 1333326))
+1333014, 1333020, 1333021, 1333022, 1333235, 1333326, 836800, 836798, 836799, 836848))
 group by o.observation_source_concept_id,o.value_source_concept_id,c.concept_name,sq.survey_concept_id,sq.order_number,sq.path,9
 order by CAST(sq.order_number as int64) asc),
 sub_2_questions_count as
@@ -132,7 +132,7 @@ and (exists
 (select * from \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.v_full_observation\` where questionnaire_response_id=o.questionnaire_response_id
 and observation_source_concept_id=cast(SPLIT(sq.path, '.')[OFFSET(0)] as int64) and value_source_concept_id=cast(SPLIT(sq.path, '.')[OFFSET(1)] as int64) )
 or sq.concept_id in (1333105, 1332735, 1332734, 1310134, 1332738, 1332762, 1333324, 1333235, 1310148, 1332769, 713888, 596885, 596888, 1310135, 1310136, 1310138, 1310141, 1310144, 1310148, 1332737, 1332769, 1332793, 1332796, 1332830, 1332831,
-1333014, 1333020, 1333021, 1333022, 1333235, 1333326) or sq.path in ('1585838.1585841.1585348', '1585838.1585842.1585348'))
+1333014, 1333020, 1333021, 1333022, 1333235, 1333326, 836800, 836798, 836799, 836848) or sq.path in ('1585838.1585841.1585348', '1585838.1585842.1585348'))
 group by o.observation_source_concept_id,o.value_source_concept_id,c.concept_name,sq.survey_concept_id,sq.order_number,sq.path
 order by CAST(sq.order_number as int64) asc),
 sub_2_questions_count as
@@ -268,7 +268,7 @@ and
 (exists (select * from \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.v_full_observation\` where questionnaire_response_id=o.questionnaire_response_id
 and observation_source_concept_id=cast(SPLIT(sq.path, '.')[OFFSET(0)] as int64) and value_source_concept_id=cast(SPLIT(sq.path, '.')[OFFSET(1)] as int64) )
 or sq.concept_id in (1333105, 1332735, 1332734, 1310134, 1332738, 1332762, 1333324, 1333235, 1310148, 1332769, 713888, 596885, 596888, 1310135, 1310136, 1310138, 1310141, 1310144, 1310148, 1332737, 1332769, 1332793, 1332796, 1332830, 1332831,
-1333014, 1333020, 1333021, 1333022, 1333235, 1333326) or sq.path in ('1585838.1585841.1585348', '1585838.1585842.1585348'))
+1333014, 1333020, 1333021, 1333022, 1333235, 1333326, 836800, 836798, 836799, 836848) or sq.path in ('1585838.1585841.1585348', '1585838.1585842.1585348'))
 group by sq.survey_concept_id,o.observation_source_concept_id,o.value_source_concept_id,c.concept_name,p.gender_concept_id,sq.order_number,sq.path
 order by CAST(sq.order_number as int64) asc
 ),
@@ -415,7 +415,7 @@ and
 (exists (select * from \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.v_full_observation\` where questionnaire_response_id=o.questionnaire_response_id
 and observation_source_concept_id=cast(SPLIT(sq.path, '.')[OFFSET(0)] as int64) and value_source_concept_id=cast(SPLIT(sq.path, '.')[OFFSET(1)] as int64) )
 or sq.concept_id in (1333105, 1332735, 1332734, 1310134, 1332738, 1332762, 1333324, 1333235, 1310148, 1332769, 713888, 596885, 596888, 1310135, 1310136, 1310138, 1310141, 1310144, 1310148, 1332737, 1332769, 1332793, 1332796, 1332830, 1332831,
-1333014, 1333020, 1333021, 1333022, 1333235, 1333326) or sq.path in ('1585838.1585841.1585348', '1585838.1585842.1585348'))
+1333014, 1333020, 1333021, 1333022, 1333235, 1333326, 836800, 836798, 836799, 836848) or sq.path in ('1585838.1585841.1585348', '1585838.1585842.1585348'))
 group by sq.survey_concept_id,o.observation_source_concept_id,o.value_source_concept_id,c.concept_name,stratum_5,sq.order_number,sq.path
 order by CAST(sq.order_number as int64) asc),
 sub_2_questions_count as
@@ -640,7 +640,7 @@ and
 (exists (select * from \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.v_full_observation\` where questionnaire_response_id=o.questionnaire_response_id
 and observation_source_concept_id=cast(SPLIT(sq.path, '.')[OFFSET(0)] as int64) )
 or sq.concept_id in (1333105, 1332735, 1332734, 1310134, 1332738, 1332762, 1333324, 1333235, 1310148, 1332769, 713888, 596885, 596888, 1310135, 1310136, 1310138, 1310141, 1310144, 1310148, 1332737, 1332769, 1332793, 1332796, 1332830, 1332831,
-1333014, 1333020, 1333021, 1333022, 1333235, 1333326))
+1333014, 1333020, 1333021, 1333022, 1333235, 1333326, 836800, 836798, 836799, 836848))
 group by sq.survey_concept_id,o.observation_source_concept_id,p.gender_concept_id,sq.order_number,sq.path
 order by CAST(sq.order_number as int64) asc
 ),
@@ -703,7 +703,7 @@ and
 (exists (select * from \`${WORKBENCH_PROJECT}.${WORKBENCH_DATASET}.v_full_observation\` where questionnaire_response_id=o.questionnaire_response_id
 and observation_source_concept_id=cast(SPLIT(sq.path, '.')[OFFSET(0)] as int64) )
 or sq.concept_id in (1333105, 1332735, 1332734, 1310134, 1332738, 1332762, 1333324, 1333235, 1310148, 1332769, 713888, 596885, 596888, 1310135, 1310136, 1310138, 1310141, 1310144, 1310148, 1332737, 1332769, 1332793, 1332796, 1332830, 1332831,
-1333014, 1333020, 1333021, 1333022, 1333235, 1333326))
+1333014, 1333020, 1333021, 1333022, 1333235, 1333326, 836800, 836798, 836799, 836848))
 group by sq.survey_concept_id,o.observation_source_concept_id,stratum_5,sq.order_number,sq.path
 order by CAST(sq.order_number as int64) asc),
 sub_2_questions_count as
