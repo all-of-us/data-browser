26a423fc4de80dcd27231564ecddb0b8af0ed4b7
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2022-04-12 20:00:22 +0530

    [risk=no] Connecting to updated VAT table (#1048)
    * Vat table name update
    
    * cdr version update
    
    Co-authored-by: Srushti Gangireddy <srushtigangireddy@NLDMIMWL0RJHV2T.local>

--- public-api/db-cdr/generate-cdr/make-bq-data.sh
+++ public-api/db-cdr/generate-cdr/make-bq-data.sh
@@ -134,12 +134,12 @@ if [ "$SEARCH_VAT" = true ]; then
          END
          ASC
   )  AS row_number
-  FROM \`$OUTPUT_PROJECT.$GENOMICS_DATASET.vat_99k_v1\`
+  FROM \`$OUTPUT_PROJECT.$GENOMICS_DATASET.vat_v3\`
   WHERE is_canonical_transcript OR transcript is NULL
   ORDER BY vid, row_number),
   genes as (
      SELECT vid, ARRAY_TO_STRING(array_agg(distinct gene_symbol ignore nulls ORDER BY gene_symbol), ', ') as genes
-     FROM \`$OUTPUT_PROJECT.$GENOMICS_DATASET.vat_99k_v1\`
+     FROM \`$OUTPUT_PROJECT.$GENOMICS_DATASET.vat_v3\`
      GROUP BY vid
   )
   SELECT
