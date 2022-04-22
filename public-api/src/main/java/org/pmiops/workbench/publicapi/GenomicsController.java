023fd90d36d0c9a11ec13ed4c7f893ef15a6a532
Srushti Gangireddy <srushtigangireddy@NLDMIMWL0RJHV2T.local>
2022-04-22 21:34:08 +0530

    wip

--- public-api/src/main/java/org/pmiops/workbench/publicapi/GenomicsController.java
+++ public-api/src/main/java/org/pmiops/workbench/publicapi/GenomicsController.java
@@ -278,6 +278,9 @@ public class GenomicsController implements GenomicsApiDelegate {
         if (ALLELE_FREQUENCY_FILTER.length() > 0) {
             finalSql += ALLELE_FREQUENCY_FILTER;
         }
+        System.out.println("************************ +++++++++ ****************************");
+        System.out.println(finalSql);
+        System.out.println("************************ +++++++++ ****************************");
         QueryJobConfiguration qjc = QueryJobConfiguration.newBuilder(finalSql)
                 .addNamedParameter("contig", QueryParameterValue.string(contig))
                 .addNamedParameter("high", QueryParameterValue.int64(high))
@@ -629,6 +632,9 @@ public class GenomicsController implements GenomicsApiDelegate {
                 }
             }
         }
+        System.out.println("************************ +++++++++ ****************************");
+        System.out.println(finalSql);
+        System.out.println("************************ +++++++++ ****************************");
         QueryJobConfiguration qjc = QueryJobConfiguration.newBuilder(finalSql)
                 .addNamedParameter("contig", QueryParameterValue.string(contig))
                 .addNamedParameter("high", QueryParameterValue.int64(high))
