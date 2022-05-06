46a395c33145e92a199e3c5e4d4a085313db1ba5
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2022-05-06 19:47:20 +0530

    removing unused var client id (#1055)
    Co-authored-by: Srushti Gangireddy <srushtigangireddy@NLDMIMWL0RJHV2T.local>

--- public-ui/src/environments/environment.prod.ts
+++ public-ui/src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   displayTag: 'Prod',
   publicApiUrl: 'https://public.api.researchallofus.org',
   researchAllOfUsUrl: 'https://researchallofus.org',
-  clientId: '684273740878-d7i68in5d9hqr6n9mfvrdh53snekp79f.apps.googleusercontent.com',
   debug: false,
   gaId: 'UA-116298798-6',
   gtmId: 'GTM-NQ9XDTW',
