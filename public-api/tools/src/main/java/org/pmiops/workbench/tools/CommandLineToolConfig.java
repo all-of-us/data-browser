9f6acf0b316a868bb21248ea11a606c299593a69
Srushti Gangireddy <srushtigangireddy@NLDMIMWL0RJHV2T.local>
2022-05-26 11:10:11 -0500

    upgrading spring version

--- public-api/tools/src/main/java/org/pmiops/workbench/tools/CommandLineToolConfig.java
+++ public-api/tools/src/main/java/org/pmiops/workbench/tools/CommandLineToolConfig.java
@@ -1,6 +1,11 @@
+package org.pmiops.workbench.tools;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.WebApplicationType;
 
 @Configuration
 @EnableAutoConfiguration
