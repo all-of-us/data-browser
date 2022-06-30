30df6d0ab39295aba75f2f9e0a2cd4f0c036cffb
Srushti Gangireddy <srushtigangireddy@NLDMIMWL0RJHV2T.local>
2022-06-30 14:17:50 -0500

    changing test cdr

---
+++ public-ui/src/app/data-browser/views/survey-view/survey-view.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed, waitForAsync as  } from '@angular/core/testing';
+
+import { SurveyViewComponent } from './survey-view.component';
+
+describe('SurveyViewComponent', () => {
+  let component: SurveyViewComponent;
+  let fixture: ComponentFixture<SurveyViewComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SurveyViewComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SurveyViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
