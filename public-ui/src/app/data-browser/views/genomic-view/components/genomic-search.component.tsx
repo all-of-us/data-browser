6a419bfd14041c83b34e22962fb648700a3a336c
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2022-10-20 12:42:14 -0500

    added new copy + link

--- public-ui/src/app/data-browser/views/genomic-view/components/genomic-search.component.tsx
+++ public-ui/src/app/data-browser/views/genomic-view/components/genomic-search.component.tsx
@@ -7,28 +7,28 @@ import { VariantSearchComponent } from "./variant-search.component";
 import { VariantTableComponent } from "./variant-table.component";
 
 const styles = reactStyles({
-    border: {
-        background: 'white',
-        padding: '2em',
-        paddingTop: '1em',
-    },
-    titleBox: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'flex-start',
-    },
-    boxHeading: {
-        margin: 0,
-        fontFamily: 'GothamBook, Arial, sans-serif',
-        fontWeight: 100,
-        fontStyle: 'normal',
-        fontSize: '.8em',
-        fontStretch: 'normal',
-        lineHeight: '1.47em',
-        letterSpacing: 'normal',
-        textAlign: 'left',
-        color: '#262262',
-    },
+  border: {
+    background: 'white',
+    padding: '2em',
+    paddingTop: '1em',
+  },
+  titleBox: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+  },
+  boxHeading: {
+    margin: 0,
+    fontFamily: 'GothamBook, Arial, sans-serif',
+    fontWeight: 100,
+    fontStyle: 'normal',
+    fontSize: '.8em',
+    fontStretch: 'normal',
+    lineHeight: '1.47em',
+    letterSpacing: 'normal',
+    textAlign: 'left',
+    color: '#262262',
+  },
 });
 
 interface Props {
@@ -70,7 +70,7 @@ export class GenomicSearchComponent extends React.Component<Props, State> {
       this.setState({ searchTerm: searchTerm });
     }
     if (prevProps.filterMetadata !== filterMetadata) {
-        this.setState({filterMetadata: filterMetadata});
+      this.setState({ filterMetadata: filterMetadata });
     }
   }
 
@@ -91,36 +91,41 @@ export class GenomicSearchComponent extends React.Component<Props, State> {
     this.props.onFilterSubmit(filteredMetadata);
   }
 
-    render() {
-        const { searchTerm, filterMetadata } = this.state;
-        const { currentPage, loadingResults, searchResults, variantListSize, loadingVariantListSize, onSearchInput,
-            rowCount } = this.props;
+  render() {
+    const { searchTerm, filterMetadata } = this.state;
+    const { currentPage, loadingResults, searchResults, variantListSize, loadingVariantListSize, onSearchInput,
+      rowCount } = this.props;
 
-        return <React.Fragment>
-            <div style={styles.titleBox}>
-                <p style={styles.boxHeading} ref={this.scrollDiv}>
-                    Use the Variant Search to explore allele frequencies for a gene or genomic region. Drill down into
-                    specific variants to view select annotations and genetic ancestry associations. </p>
-            </div>
-            <VariantSearchComponent
-                onSearchTerm={(searchWord: string) => { onSearchInput(searchWord); this.setState({ searchTerm: searchWord }); }}
-                onFilterSubmit={(filteredMetadata) => { this.handleFilterSubmit(filteredMetadata); }}
-                loading={loadingVariantListSize}
-                searchTerm={searchTerm}
-                variantListSize={variantListSize}
-                filterMetadata={filterMetadata} />
-            <VariantTableComponent
-                loadingResults={loadingResults}
-                loadingVariantListSize={loadingVariantListSize}
-                variantListSize={variantListSize}
-                searchResults={searchResults}
-                searchTerm={searchTerm}
-                onSearchTerm={(searchWord: string) => { onSearchInput(searchWord); this.setState({ searchTerm: searchWord }); }}
-                onRowCountChange={(info: any) => this.handleRowCountChange(info)}
-                onPageChange={(info: any) => this.handlePageChange(info)}
-                onSortClick={(sortMetadata: any) => this.handleSortClick(sortMetadata)}
-                currentPage={currentPage}
-                rowCount={rowCount} />
-        </React.Fragment>;
-    }
+    return <React.Fragment>
+      <div style={styles.titleBox}>
+        <p style={styles.boxHeading} ref={this.scrollDiv}>
+          Use the Variant Search to explore allele frequencies for a gene or genomic region. Drill down into
+          specific variants to view select annotations and genetic ancestry associations.
+          Variants are called against the GRCh38/hg38 genome reference and stored with associated metadata via a&#32;
+          <a style={{color:'#1f79b8'}}href='https://aousupporthelp.zendesk.com/hc/en-us/articles/4615256690836-Variant-Annotation-Table'>
+          Variant Annotation Table.
+          </a>
+        </p>
+      </div>
+      <VariantSearchComponent
+        onSearchTerm={(searchWord: string) => { onSearchInput(searchWord); this.setState({ searchTerm: searchWord }); }}
+        onFilterSubmit={(filteredMetadata) => { this.handleFilterSubmit(filteredMetadata); }}
+        loading={loadingVariantListSize}
+        searchTerm={searchTerm}
+        variantListSize={variantListSize}
+        filterMetadata={filterMetadata} />
+      <VariantTableComponent
+        loadingResults={loadingResults}
+        loadingVariantListSize={loadingVariantListSize}
+        variantListSize={variantListSize}
+        searchResults={searchResults}
+        searchTerm={searchTerm}
+        onSearchTerm={(searchWord: string) => { onSearchInput(searchWord); this.setState({ searchTerm: searchWord }); }}
+        onRowCountChange={(info: any) => this.handleRowCountChange(info)}
+        onPageChange={(info: any) => this.handlePageChange(info)}
+        onSortClick={(sortMetadata: any) => this.handleSortClick(sortMetadata)}
+        currentPage={currentPage}
+        rowCount={rowCount} />
+    </React.Fragment>;
+  }
 }
