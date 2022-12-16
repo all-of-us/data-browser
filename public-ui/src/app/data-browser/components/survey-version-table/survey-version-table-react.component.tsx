52ec27b2f9a7dc663dd74761ee7e0f722c57f37e
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2022-12-16 15:21:38 -0600

    [risk=no] fixing cope New Year tooltip and survey pdf (#1186)
    * fixing tooltip and surve pdf
    
    * fixing tooltip and surve pdf
    
    Co-authored-by: Srushti Gangireddy <shrushigangireddy@NLDMIMX3N6D59RJ.local>

--- public-ui/src/app/data-browser/components/survey-version-table/survey-version-table-react.component.tsx
+++ public-ui/src/app/data-browser/components/survey-version-table/survey-version-table-react.component.tsx
@@ -79,6 +79,9 @@ export class SurveyVersionTableReactComponent extends React.Component<
           <div className="version-box-body">
             {!!surveyVersions &&
               surveyVersions.map((survey) => {
+                if (survey.monthName== 'New Year') {
+                    survey.pdfLink = survey.pdfLink.replace(' ', '_');
+                }
                 return (
                   <div className="version-box-row" key={survey.monthName}>
                     <span className="version-box-item">{survey.monthName}</span>
