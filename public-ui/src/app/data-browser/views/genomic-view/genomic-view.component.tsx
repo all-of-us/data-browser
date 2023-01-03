0ebfe8b16f6eb3a941b0405b5759891dc2a5c311
Srushti Gangireddy <shrushigangireddy@NLDMIMX3N6D59RJ.local>
2023-01-03 12:17:50 -0600

    fixing filter bug

--- public-ui/src/app/data-browser/views/genomic-view/genomic-view.component.tsx
+++ public-ui/src/app/data-browser/views/genomic-view/genomic-view.component.tsx
@@ -312,6 +312,7 @@ export const GenomicViewComponent = withRouteData(
     }
 
     handlePageChange(info) {
+      console.log('Changing page ...');
       this.setState(
         { loadingResults: true, currentPage: info.selectedPage },
         () => {
@@ -333,13 +334,14 @@ export const GenomicViewComponent = withRouteData(
     }
 
     fetchVariantData() {
-      const { searchTerm, currentPage, sortMetadata, rowCount } = this.state;
+      const { searchTerm, currentPage, sortMetadata, rowCount, filterMetadata } = this.state;
 
       const searchRequest: SearchVariantsRequest = {
         query: searchTerm,
         pageNumber: currentPage,
         rowCount: rowCount,
         sortMetadata: sortMetadata,
+        filterMetadata: filterMetadata,
       };
       
       genomicsApi()
