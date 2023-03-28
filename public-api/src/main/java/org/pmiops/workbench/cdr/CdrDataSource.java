05c4dcb509ea152f634ca0ec166cb7965233f266
Srushti Gangireddy <shrushigangireddy@NLDMIMX3N6D59RJ.local>
2023-03-28 11:18:20 -0500

    wip

--- public-api/src/main/java/org/pmiops/workbench/cdr/CdrDataSource.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/CdrDataSource.java
@@ -48,23 +48,26 @@ public class CdrDataSource extends AbstractRoutingDataSource {
         resetTargetDataSources();
     }
 
-    List<DbCdrVersion> getCdrVersions() {
-        EntityManager em = emFactory.createEntityManager();
-        String jpql = "SELECT v FROM DbCdrVersion v";
-        TypedQuery<DbCdrVersion> query = em.createQuery(jpql, DbCdrVersion.class);
-        try {
-            return query.getResultList();
-        } catch (PersistenceException e) {
-            // This emits "Table 'CDR_VERSION' not found" before throwing to this catch, so it is not
-            // necessary to warn further.
-            return new ArrayList<>();
-        }
+    DataSource createDataSource(String dbName) {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.setJdbcUrl(String.format("jdbc:mysql:///%s", dbName));
+        config.setUsername(getRequiredEnv("PUBLIC_DB_USER"));
+        config.setPassword(getRequiredEnv("PUBLIC_DB_PASSWORD"));
+        config.addDataSourceProperty("socketFactory", "com.google.cloud.sql.mysql.SocketFactory");
+        config.addDataSourceProperty("cloudSqlInstance", getRequiredEnv("CLOUD_SQL_INSTANCE"));
+        config.addDataSourceProperty("useSSL", false);
+        return new HikariDataSource(config);
+    }
+
+    String getRequiredEnv(String name) {
+        return Optional.ofNullable(System.getenv(name)).map(s -> s.trim()).filter(s -> s != "")
+                .orElseThrow(() -> new IllegalStateException(name + " not defined"));
     }
 
     void resetTargetDataSources() {
-        String dbUser = cdrPoolConfig.getUsername();
-        String dbPassword = cdrPoolConfig.getPassword();
-        String originalDbUrl = cdrPoolConfig.getUrl();
+        String dbUser = getRequiredEnv("CDR_DB_USER");
+        String dbPassword = getRequiredEnv("CDR_DB_PASSWORD");
 
         // Build a map of CDR version ID -> DataSource for use later, based on all the entries in the
         // cdr_version table. Note that if new CDR versions are inserted, we need to restart the
@@ -72,52 +75,42 @@ public class CdrDataSource extends AbstractRoutingDataSource {
         // TODO: find a way to make sure CDR versions aren't shown in the UI until they are in use by
         // all servers.
         Map<Object, Object> cdrVersionDataSourceMap = new HashMap<>();
-        for (DbCdrVersion cdrVersion : getCdrVersions()) {
-            int slashIndex = originalDbUrl.lastIndexOf('/');
-            String dbUrl =
-                    originalDbUrl.substring(0, slashIndex + 1) + cdrVersion.getPublicDbName() + "?useSSL=false";
+        for (DbCdrVersion cdrVersion : cdrVersionDao.findAll()) {
+            try {
+                DataSource dataSource = createDataSource(cdrVersion.getPublicDbName());
+                if (dataSource instanceof org.apache.tomcat.jdbc.pool.DataSource) {
+                    org.apache.tomcat.jdbc.pool.DataSource tomcatSource =
+                            (org.apache.tomcat.jdbc.pool.DataSource) dataSource;
+                    // A Tomcat DataSource implements PoolConfiguration, therefore these pool parameters can
+                    // normally be populated via @ConfigurationProperties. Since we are directly initializing
+                    // DataSources here without a hook to @ConfigurationProperties, we instead need to
+                    // explicitly initialize the pool parameters here. We override the primary connection
+                    // info, as the autowired PoolConfiguration is initialized from the same set of properties
+                    // as the workbench DB.
+                    PoolConfiguration cdrPool = new PoolProperties();
+                    BeanUtils.copyProperties(basePoolConfig, cdrPool);
+                    cdrPool.setUsername(dbUser);
+                    cdrPool.setPassword(dbPassword);
+                    cdrPool.setUrl(String.format("jdbc:mysql:///%s", cdrVersion.getPublicDbName()));
+                    tomcatSource.setPoolProperties(cdrPool);
 
+                    // The Spring autowiring is a bit of a maze here, log something concrete which will allow
+                    // verification that the DB settings in application.properties are actually being loaded.
+                    log.info("using Tomcat pool for CDR data source, with minIdle: " + cdrPool.getMinIdle());
+                } else {
+                    log.warning(
+                            "not using Tomcat pool or initializing pool configuration; "
+                                    + "this should only happen within tests");
+                }
 
-            System.out.println("******************* TEST **********************");
-            System.out.println(dbUrl);
-
-            DataSource dataSource =
-                    DataSourceBuilder.create()
-                            .driverClassName(basePoolConfig.getDriverClassName())
-                            .username(dbUser)
-                            .password(dbPassword)
-                            .url(dbUrl)
-                            .build();
-            if (dataSource instanceof org.apache.tomcat.jdbc.pool.DataSource) {
-                org.apache.tomcat.jdbc.pool.DataSource tomcatSource =
-                        (org.apache.tomcat.jdbc.pool.DataSource) dataSource;
-                // A Tomcat DataSource implements PoolConfiguration, therefore these pool parameters can
-                // normally be populated via @ConfigurationProperties. Since we are directly initializing
-                // DataSources here without a hook to @ConfigurationProperties, we instead need to
-                // explicitly initialize the pool parameters here. We override the primary connection
-                // info, as the autowired PoolConfiguration is initialized from the same set of properties
-                // as the workbench DB.
-                PoolConfiguration cdrPool = new PoolProperties();
-                BeanUtils.copyProperties(basePoolConfig, cdrPool);
-                cdrPool.setUsername(dbUser);
-                cdrPool.setPassword(dbPassword);
-                cdrPool.setUrl(dbUrl);
-                tomcatSource.setPoolProperties(cdrPool);
-
-                // The Spring autowiring is a bit of a maze here, log something concrete which will allow
-                // verification that the DB settings in application.properties are actually being loaded.
-                log.info("using Tomcat pool for CDR data source, with minIdle: " + cdrPool.getMinIdle());
-            } else {
-                log.warning(
-                        "not using Tomcat pool or initializing pool configuration; "
-                                + "this should only happen within tests");
+                cdrVersionDataSourceMap.put(cdrVersion.getCdrVersionId(), dataSource);
+            } catch (HikariPool.PoolInitializationException e) {
+                // If this is caught, java.sql.SQLSyntaxErrorException: Unknown database 'name'
+                // was thrown, so no need to repeat here.
             }
-
-            cdrVersionDataSourceMap.put(cdrVersion.getCdrVersionId(), dataSource);
         }
         setTargetDataSources(cdrVersionDataSourceMap);
     }
-
     @Override
     protected Object determineCurrentLookupKey() {
         return CdrVersionContext.getCdrVersion().getCdrVersionId();
