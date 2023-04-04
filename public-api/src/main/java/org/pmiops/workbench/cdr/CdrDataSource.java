43aca61b0e409734da1ee9463ecbf3caa9b9386b
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2023-04-04 12:51:52 -0500

    [risk=no] Working data source hikari rewrite (#1237)
    * wip
    
    * working data source creation with hikari
    
    * wip
    
    * Removing console log
    
    * cleaning up env
    
    ---------
    
    Co-authored-by: Srushti Gangireddy <shrushigangireddy@NLDMIMX3N6D59RJ.local>

---
+++ public-api/src/main/java/org/pmiops/workbench/cdr/CdrDataSource.java
@@ -0,0 +1,119 @@
+package org.pmiops.workbench.cdr;
+
+import static org.pmiops.workbench.db.WorkbenchDbConfig.createConfig;
+
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.pool.HikariPool;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
+import javax.sql.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolConfiguration;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+import org.pmiops.workbench.db.model.DbCdrVersion;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.stereotype.Service;
+
+@Service("cdrDataSource")
+public class CdrDataSource extends AbstractRoutingDataSource {
+
+    private static final Logger log = Logger.getLogger(CdrDataSource.class.getName());
+
+    private final PoolConfiguration basePoolConfig;
+    private final PoolConfiguration cdrPoolConfig;
+    private final EntityManagerFactory emFactory;
+
+    CdrDataSource(
+            @Qualifier("poolConfiguration") PoolConfiguration basePoolConfig,
+            @Qualifier("cdrPoolConfiguration") PoolConfiguration cdrPoolConfig,
+            // Using CdrDbConfig.cdrEntityManagerFactory would cause a circular dependency.
+            @Qualifier("entityManagerFactory") EntityManagerFactory emFactory) {
+        this.basePoolConfig = basePoolConfig;
+        this.cdrPoolConfig = cdrPoolConfig;
+        this.emFactory = emFactory;
+        resetTargetDataSources();
+    }
+
+    List<DbCdrVersion> getCdrVersions() {
+        EntityManager em = emFactory.createEntityManager();
+        String jpql = "SELECT v FROM DbCdrVersion v";
+        TypedQuery<DbCdrVersion> query = em.createQuery(jpql, DbCdrVersion.class);
+        try {
+            return query.getResultList();
+        } catch (PersistenceException e) {
+            // This emits "Table 'CDR_VERSION' not found" before throwing to this catch, so it is not
+            // necessary to warn further.
+            return new ArrayList<>();
+        }
+    }
+
+    DataSource createCdrDs(String dbName) {
+        return new HikariDataSource(createConfig(dbName));
+    }
+
+    void resetTargetDataSources() {
+        // Build a map of CDR version ID -> DataSource for use later, based on all the entries in the
+        // cdr_version table. Note that if new CDR versions are inserted, we need to restart the
+        // server in order for it to be used.
+        // TODO: find a way to make sure CDR versions aren't shown in the UI until they are in use by
+        // all servers.
+        Map<Object, Object> cdrVersionDataSourceMap = new HashMap<>();
+        for (DbCdrVersion cdrVersion : getCdrVersions()) {
+            try {
+                DataSource dataSource = createCdrDs(cdrVersion.getPublicDbName());
+                if (dataSource instanceof org.apache.tomcat.jdbc.pool.DataSource) {
+                    org.apache.tomcat.jdbc.pool.DataSource tomcatSource =
+                            (org.apache.tomcat.jdbc.pool.DataSource) dataSource;
+                    // A Tomcat DataSource implements PoolConfiguration, therefore these pool parameters can
+                    // normally be populated via @ConfigurationProperties. Since we are directly initializing
+                    // DataSources here without a hook to @ConfigurationProperties, we instead need to
+                    // explicitly initialize the pool parameters here. We override the primary connection
+                    // info, as the autowired PoolConfiguration is initialized from the same set of properties
+                    // as the workbench DB.
+                    PoolConfiguration cdrPool = new PoolProperties();
+                    BeanUtils.copyProperties(basePoolConfig, cdrPool);
+                    cdrPool.setUsername("databrowser"); // consistent across environments
+                    cdrPool.setPassword(getEnvRequired("DATABROWSER_DB_PASSWORD"));
+                    cdrPool.setUrl(String.format("jdbc:mysql:///%s", cdrVersion.getPublicDbName()));
+                    tomcatSource.setPoolProperties(cdrPool);
+
+                    // The Spring autowiring is a bit of a maze here, log something concrete which will allow
+                    // verification that the DB settings in application.properties are actually being loaded.
+                    log.info("using Tomcat pool for CDR data source, with minIdle: " + cdrPool.getMinIdle());
+                } else {
+                    log.warning(
+                            "not using Tomcat pool or initializing pool configuration; "
+                                    + "this should only happen within tests");
+                }
+
+                cdrVersionDataSourceMap.put(cdrVersion.getCdrVersionId(), dataSource);
+            } catch (HikariPool.PoolInitializationException e) {
+                // If this is caught, java.sql.SQLSyntaxErrorException: Unknown database 'name'
+                // was thrown, so no need to repeat here.
+            }
+        }
+        setTargetDataSources(cdrVersionDataSourceMap);
+    }
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return CdrVersionContext.getCdrVersion().getCdrVersionId();
+    }
+
+    Optional<String> getEnv(String name) {
+        return Optional.ofNullable(System.getenv(name)).map(s -> s.trim()).filter(s -> s != "");
+    }
+
+    String getEnvRequired(String name) {
+        return getEnv(name).orElseThrow(() -> new IllegalStateException(name + " not defined"));
+    }
+}
--- public-api/src/main/java/org/pmiops/workbench/db/WorkbenchDbConfig.java
+++ public-api/src/main/java/org/pmiops/workbench/db/WorkbenchDbConfig.java
@@ -1,58 +1,75 @@
 package org.pmiops.workbench.db;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.Optional;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.apache.tomcat.jdbc.pool.PoolConfiguration;
 import org.apache.tomcat.jdbc.pool.PoolProperties;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * Spring configuration for our workbench database. Uses the spring.datasource.* properties from
+ * application.properties to configure the connection. Applies to the model and DAO objects under
+ * this package.
+ */
 @Configuration
 @EnableTransactionManagement
+@EnableJpaAuditing
 @EnableJpaRepositories(
         entityManagerFactoryRef = "entityManagerFactory",
         transactionManagerRef = "transactionManager",
-        basePackages = { "org.pmiops.workbench.db" }
-)
-/**
- * Spring configuration for our workbench database. Uses the spring.datasource.* properties
- * from application.properties to configure the connection. Applies to the model and DAO objects
- * under this package.
- */
+        basePackages = {"org.pmiops.workbench.db"})
 public class WorkbenchDbConfig {
 
-  @Primary
-  @Bean(name = "dataSourceProperties")
-  @ConfigurationProperties(prefix = "spring.datasource")
-  public DataSourceProperties dataSourceProperties() {
-    return new DataSourceProperties();
+  public static HikariConfig createConfig(String dbName) {
+    HikariConfig config = new HikariConfig();
+
+    System.out.println("debug - 1");
+
+    Optional<String> dbHost = getEnv("DB_HOST");
+    boolean connectViaAppEngine = !dbHost.isPresent();
+    config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    config.setJdbcUrl(
+            String.format("jdbc:mysql://%s/%s", connectViaAppEngine ? "" : dbHost.get(), dbName));
+    config.setUsername("databrowser"); // consistent across environments
+    config.setPassword(getEnvRequired("DATABROWSER_DB_PASSWORD"));
+    if (connectViaAppEngine) {
+      config.addDataSourceProperty("socketFactory", "com.google.cloud.sql.mysql.SocketFactory");
+      config.addDataSourceProperty("cloudSqlInstance", getEnvRequired("CLOUD_SQL_INSTANCE_NAME"));
+    }
+    config.addDataSourceProperty("useSSL", false);
+
+    System.out.println("debug - 2");
+
+    return config;
   }
 
   @Primary
   @Bean(name = "dataSource")
-  @ConfigurationProperties(prefix = "spring.datasource")
   public DataSource dataSource() {
-    return dataSourceProperties().initializeDataSourceBuilder().build();
+    // main database name is consistent across environments
+    return new HikariDataSource(createConfig("databrowser"));
   }
 
   @Primary
   @Bean(name = "entityManagerFactory")
   public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-          EntityManagerFactoryBuilder builder,
-          @Qualifier("dataSource") DataSource dataSource
-  ) {
+          EntityManagerFactoryBuilder builder, @Qualifier("dataSource") DataSource dataSource) {
     return builder
             .dataSource(dataSource)
             .packages("org.pmiops.workbench.db")
@@ -76,5 +93,13 @@ public class WorkbenchDbConfig {
   @ConfigurationProperties(prefix = "spring.datasource")
   public PoolConfiguration poolConfig() {
     return new PoolProperties();
+  }
+
+  static Optional<String> getEnv(String name) {
+    return Optional.ofNullable(System.getenv(name)).map(s -> s.trim()).filter(s -> s != "");
+  }
+
+  static String getEnvRequired(String name) {
+    return getEnv(name).orElseThrow(() -> new IllegalStateException(name + " not defined"));
   }
 }
