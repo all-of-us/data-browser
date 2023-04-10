7dba9723ef935424ecbd71b4c54f3b3873a9cfa5
Srushti Gangireddy <shrushigangireddy@NLDMIMX3N6D59RJ.local>
2023-04-10 16:41:38 -0500

    changing db to hikari

--- public-api/src/main/java/org/pmiops/workbench/db/WorkbenchDbConfig.java
+++ public-api/src/main/java/org/pmiops/workbench/db/WorkbenchDbConfig.java
@@ -1,58 +1,58 @@
 package org.pmiops.workbench.db;
 
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.Optional;
+import java.util.logging.Logger;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.apache.tomcat.jdbc.pool.PoolConfiguration;
 import org.apache.tomcat.jdbc.pool.PoolProperties;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * Spring configuration for our workbench database. Uses the spring.datasource.* properties from
+ * application.properties to configure the connection. Applies to the model and DAO objects under
+ * this package.
+ */
 @Configuration
 @EnableTransactionManagement
+@EnableJpaAuditing
 @EnableJpaRepositories(
         entityManagerFactoryRef = "entityManagerFactory",
         transactionManagerRef = "transactionManager",
-        basePackages = { "org.pmiops.workbench.db" }
-)
-/**
- * Spring configuration for our workbench database. Uses the spring.datasource.* properties
- * from application.properties to configure the connection. Applies to the model and DAO objects
- * under this package.
- */
+        basePackages = {"org.pmiops.workbench.db"})
 public class WorkbenchDbConfig {
+  private static final Logger log = Logger.getLogger(WorkbenchDbConfig.class.getName());
 
-  @Primary
-  @Bean(name = "dataSourceProperties")
-  @ConfigurationProperties(prefix = "spring.datasource")
-  public DataSourceProperties dataSourceProperties() {
-    return new DataSourceProperties();
+  final Params params;
+
+  public WorkbenchDbConfig(Params params) {
+    this.params = params;
   }
 
   @Primary
   @Bean(name = "dataSource")
-  @ConfigurationProperties(prefix = "spring.datasource")
   public DataSource dataSource() {
-    return dataSourceProperties().initializeDataSourceBuilder().build();
+    // main database name is consistent across environments
+    return new HikariDataSource(params.createConfig("workbench"));
   }
 
   @Primary
   @Bean(name = "entityManagerFactory")
   public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-          EntityManagerFactoryBuilder builder,
-          @Qualifier("dataSource") DataSource dataSource
-  ) {
+          EntityManagerFactoryBuilder builder, @Qualifier("dataSource") DataSource dataSource) {
     return builder
             .dataSource(dataSource)
             .packages("org.pmiops.workbench.db")
@@ -73,8 +73,15 @@ public class WorkbenchDbConfig {
    */
   @Bean("poolConfiguration")
   @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-  @ConfigurationProperties(prefix = "spring.datasource")
   public PoolConfiguration poolConfig() {
     return new PoolProperties();
+  }
+
+  static Optional<String> getEnv(String name) {
+    return Optional.ofNullable(System.getenv(name)).map(s -> s.trim()).filter(s -> s != "");
+  }
+
+  static String getEnvRequired(String name) {
+    return getEnv(name).orElseThrow(() -> new IllegalStateException(name + " not defined"));
   }
 }
