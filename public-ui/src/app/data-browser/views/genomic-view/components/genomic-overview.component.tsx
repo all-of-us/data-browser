a89c08b1cebd7703fa953bdb9c17fb92ff16b434
Srushti Gangireddy <shrushigangireddy@NLDMIMX3N6D59RJ.local>
2023-04-11 18:23:41 -0500

    adjusting space between radio buttons

--- public-ui/src/app/data-browser/views/genomic-view/components/genomic-overview.component.tsx
+++ public-ui/src/app/data-browser/views/genomic-view/components/genomic-overview.component.tsx
@@ -56,6 +56,20 @@ input[type='radio']:before {
     cursor: pointer;
 }
 
+@media only screen and (max-width: 1400px) {
+    #selectGenotypeDataForm {
+        font-size: 14px;
+        gap: 1em;
+    }
+}
+
+@media only screen and (max-width: 1235px) {
+    #selectGenotypeDataForm {
+        font-size: 12px;
+        gap: 1em;
+    }
+}
+
 `;
 const styles = reactStyles({
   innerContainer: {
@@ -80,6 +94,7 @@ const styles = reactStyles({
     justifyContent:"space-between",
     flexDirection: "row",
     marginBottom: "1em",
+    gap: "1em",
     width:'80%'
   }
 
@@ -215,7 +230,7 @@ export class GenomicOverviewComponent extends React.Component<Props, State> {
         <style>{css}</style>
         <div style={styles.innerContainer}>
           {!loading && (
-            <form style={styles.selectGenotypeData} onChange={this.onGenotypeSelect}>
+            <form style={styles.selectGenotypeData} onChange={this.onGenotypeSelect} id="selectGenotypeDataForm">
               {wgsSRParticipantCount > 0 && (<React.Fragment>
                 <label className="radio-label"> <input id="radio-teal" type="radio" value="wgs_shortread" name="genotype" defaultChecked={selectedGenotype === 'wgs_shortread'} /> Short-Read WGS</label>
               </React.Fragment>)}
