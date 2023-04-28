68613720a1cbb5bb99eafbb50593a12f7b9754e3
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2023-04-28 12:33:12 -0500

    mobile sub menus

--- public-ui/src/app/shared/components/rh-header/rh-header.tsx
+++ public-ui/src/app/shared/components/rh-header/rh-header.tsx
@@ -8,7 +8,10 @@ import { menuItems } from "app/shared/services/header-footer.service";
 interface State {
   menuOpen: boolean;
   searchTerms: string;
-  submenusOpen: Array<string>;
+  submenusOpen1: boolean;
+  submenusOpen2: boolean;
+  submenusOpen3: boolean;
+  submenusOpen4: boolean;
 }
 
 export class RhHeader extends React.Component<{}, State> {
@@ -17,23 +20,19 @@ export class RhHeader extends React.Component<{}, State> {
     this.state = {
       menuOpen: false,
       searchTerms: "",
-      submenusOpen: [],
+      submenusOpen1: false,
+      submenusOpen2: false,
+      submenusOpen3: false,
+      submenusOpen4: false,
     };
   }
 
-  toggleMobileSubmenu(title: string) {
-    const { submenusOpen } = this.state;
-    const updatedSubmenusOpen = submenusOpen.includes(title)
-      ? submenusOpen.filter((sub) => sub !== title)
-      : [...submenusOpen, title];
-    this.setState({ submenusOpen: updatedSubmenusOpen });
-  }
 
   render() {
-    const { menuOpen, searchTerms, submenusOpen } = this.state;
+    const { menuOpen, searchTerms, submenusOpen1,submenusOpen2,submenusOpen3,submenusOpen4 } = this.state;
     return (
       <React.Fragment>
-        <header id="rh-nav" className={"rh__nav site-header " + (menuOpen? "nav--open":"")}>
+        <header id="rh-nav" className={"rh__nav site-header " + (menuOpen ? "nav--open" : "")}>
           <div>
             <div className="nav__login show--screen">
               <a href="https://workbench.researchallofus.org/login">RESEARCHER LOGIN</a>
@@ -47,7 +46,7 @@ export class RhHeader extends React.Component<{}, State> {
               <nav id="rh-site-navigation" className="nav__" aria-label="Top Menu">
                 <div className="menu-primary-container">
                   <ul id="menu-primary" className="nav__items main-menu">
-                    <li id="menu-item-4088" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4088"><a href="https://www.researchallofus.org/about/">About</a><div className="item-expand" />
+                    <li onClick={() => this.setState({ submenusOpen1: !submenusOpen1 })} id="menu-item-4088" className={"menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4088 " + (submenusOpen1 ? "sub--nav--open" : "")} ><a href="https://www.researchallofus.org/about/">About</a><div className="item-expand" />
                       <ul className="sub-menu">
                         <li id="menu-item-1833" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1833"><a href="https://www.researchallofus.org/about-the-research-hub/">About the Research Hub</a><div className="item-expand" /></li>
                         <li id="menu-item-93" className="icon_researchers-as-partners menu-item menu-item-type-post_type menu-item-object-page menu-item-93"><a href="https://www.researchallofus.org/researchers-as-partners/">Researchers as Partners</a><div className="item-expand" /></li>
@@ -56,7 +55,7 @@ export class RhHeader extends React.Component<{}, State> {
                         <li id="menu-item-100" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-100"><a href="https://www.researchallofus.org/frequently-asked-questions/">FAQ</a><div className="item-expand" /></li>
                       </ul>
                     </li>
-                    <li id="menu-item-3760" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-3760"><a href="https://www.researchallofus.org/data-tools/">Data &amp; Tools</a><div className="item-expand" />
+                    <li onClick={() => this.setState({ submenusOpen2: !submenusOpen2 })} id="menu-item-3760" className={"menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-3760 " + (submenusOpen2 ? "sub--nav--open" : "")}><a href="https://www.researchallofus.org/data-tools/">Data &amp; Tools</a><div className="item-expand" />
                       <ul className="sub-menu">
                         <li id="menu-item-1465" className="menu-item  menu-item-type-custom menu-item-object-custom menu-item-1465 current-menu-item"><a href="/">Data Browser</a><div className="item-expand" /></li>
                         <li id="menu-item-1834" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1834"><a href="https://www.researchallofus.org/data-tools/data-snapshots/">Data Snapshots</a><div className="item-expand" /></li>
@@ -67,7 +66,7 @@ export class RhHeader extends React.Component<{}, State> {
                         <li id="menu-item-1462" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1462"><a href="https://www.researchallofus.org/data-tools/workbench/">Researcher Workbench</a><div className="item-expand" /></li>
                       </ul>
                     </li>
-                    <li id="menu-item-4089" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4089"><a href="https://www.researchallofus.org/discover/">Discover</a><div className="item-expand" />
+                    <li onClick={() => this.setState({ submenusOpen3: !submenusOpen3 })} id="menu-item-4089" className={"menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4089 " + (submenusOpen3 ? "sub--nav--open" : "")}><a href="https://www.researchallofus.org/discover/">Discover</a><div className="item-expand" />
                       <ul className="sub-menu">
                         <li id="menu-item-3525" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-3525"><a href="https://www.researchallofus.org/research-projects-directory/">Research Projects Directory</a><div className="item-expand" /></li>
                         <li id="menu-item-4073" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-4073"><a href="/spotlight">Spotlights</a><div className="item-expand" /></li>
@@ -75,7 +74,7 @@ export class RhHeader extends React.Component<{}, State> {
                         <li id="menu-item-3277" className="menu-item menu-item-type-post_type_archive menu-item-object-publication menu-item-3277"><a href="https://www.researchallofus.org/publications/">Publications</a><div className="item-expand" /></li>
                       </ul>
                     </li>
-                    <li id="menu-item-5423" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-5423"><a href="https://aousupporthelp.zendesk.com/hc/en-us">Support</a><div className="item-expand" /></li>
+                    <li onClick={() => this.setState({ submenusOpen4: !submenusOpen4 })} id="menu-item-5423" className={"menu-item menu-item-type-custom menu-item-object-custom menu-item-5423 " + (submenusOpen4 ? "sub--nav--open" : "")}><a href="https://aousupporthelp.zendesk.com/hc/en-us">Support</a><div className="item-expand" /></li>
                     <li className="menu-item menu-item-has-children item--search">
                       <a><div /></a>
                       <ul className="sub-menu">
@@ -88,14 +87,14 @@ export class RhHeader extends React.Component<{}, State> {
                       </ul>
                     </li>
                     <li className="menu-item item--apply">
-                      <a href="/register">register</a>
+                      <a href="https://www.researchallofus.org/register">register</a>
                     </li>
                     <li className="menu-item item--login show--mobile">
                       <a href="https://workbench.researchallofus.org/login">RESEARCHER LOGIN</a>
                     </li>
                   </ul></div>
               </nav>
-              <div onClick={()=>this.setState({menuOpen:!menuOpen})} id="nav-mobile" className="nav__mobile" />
+              <div onClick={() => this.setState({ menuOpen: !menuOpen })} id="nav-mobile" className="nav__mobile" />
             </div>
           </div>
         </header>
