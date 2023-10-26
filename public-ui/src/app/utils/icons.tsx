9749f87ca389a106b92a27fae75f71aec1fed44e
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2023-10-26 09:40:16 -0500

    [risk=no] remove fitbit icons (#1331)
    * remove fitbit icons
    
    * remove fitbit icons

--- public-ui/src/app/utils/icons.tsx
+++ public-ui/src/app/utils/icons.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWatchFitness, faMonitorWaveform } from '@fortawesome/pro-solid-svg-icons';
-import { faHeartPulse, faPersonRunning, faPersonWalking, faBedPulse } from '@fortawesome/free-solid-svg-icons';
-import { faBedPulse as faBedPulseDuo } from '@fortawesome/pro-duotone-svg-icons';
 
 const Icon = ({ shape, size, style, color, ...props }) => {
   return (
@@ -14,31 +11,3 @@ const Icon = ({ shape, size, style, color, ...props }) => {
     />
   );
 };
-
-export const WatchFitness = (props) => (
-  <Icon shape={faWatchFitness} {...props} />
-);
-
-export const MonitorWaveForm = (props) => (
- <Icon shape={faMonitorWaveform} {...props} />
-);
-
-export const PersonRunning = (props) => (
- <Icon shape={faPersonRunning} {...props} />
-);
-
-export const PersonWalking = (props) => (
- <Icon shape={faPersonWalking} {...props} />
-);
-
-export const HeartPulse = (props) => (
- <Icon shape={faHeartPulse} {...props} />
-);
-
-export const BedPulse = (props) => (
- <Icon shape={faBedPulse} {...props} />
-);
-
-export const BedPulseDuo = (props) => (
- <Icon shape={faBedPulseDuo} {...props} />
-);
