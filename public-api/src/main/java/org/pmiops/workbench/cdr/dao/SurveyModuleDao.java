b1805fcbcf02434165d199a08393254412b4a336
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2023-11-07 13:18:17 -0600

    allow user to search by concept code (#1334)

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/SurveyModuleDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/SurveyModuleDao.java
@@ -20,6 +20,16 @@ public interface SurveyModuleDao extends CrudRepository<DbSurveyModule, Long> {
           "union distinct \n" +
           "select distinct c.* from survey_metadata c where concept_id in (select distinct SUBSTRING_INDEX(sqm.path, '.', 1) from survey_metadata sqm \n" +
           "where (match(sqm.question_string) against (?1 in boolean mode) > 0 or match(sqm.concept_name) against (?1 in boolean mode) > 0) and sqm.sub=1 and sqm.generate_counts=1)\n" +
+          "union distinct \n" +
+          "SELECT DISTINCT c.*\n" +
+          "FROM survey_metadata c\n" +
+          "JOIN (\n" +
+          "    SELECT DISTINCT SUBSTRING_INDEX(ar.stratum_6, '.', 1) AS question_id\n" +
+          "    FROM achilles_results ar\n" +
+          "    WHERE ar.analysis_id = 3110\n" +
+          "    AND (ar.stratum_3 = ?1 OR ar.stratum_2 = ?1)\n" +
+          ") AS subquery\n" +
+          "ON c.concept_id = subquery.question_id \n" +
           ") a \n" +
           "on sqm2.concept_id=a.concept_id group by m.name, m.description, m.concept_id order by m.order_number;")
   List<DbSurveyModule> findSurveyModuleQuestionCounts(String matchExpression);
