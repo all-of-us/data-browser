a767f541bb7f3cab2bb0201ae7b6d5f2bbd9df1f
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2023-12-05 12:28:11 -0600

    commas tooltips

--- public-ui/src/app/data-browser/charts/chart-top-results/chart-top-results-react.component.tsx
+++ public-ui/src/app/data-browser/charts/chart-top-results/chart-top-results-react.component.tsx
@@ -90,7 +90,7 @@ export class TopResultsChartReactComponent extends React.Component<
     if (concept.countValue <= 20) {
       count = "&le; 20";
     } else {
-      count = concept.countValue.toString();
+      count = concept.countValue.toLocaleString();
     }
     const toolTipText =
       '<div class="chart-tooltip top-results-tooltip">' +
