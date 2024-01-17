87d25b3052bc6875b161b6df814f13880324c294
SrushtiGangireddy <srushti.gangireddy@gmail.com>
2024-01-17 17:40:40 -0600

    changing port in ui

--- public-ui/src/environments/environment.local.ts
+++ public-ui/src/environments/environment.local.ts
@@ -3,7 +3,7 @@ import { testEnvironmentBase } from "environments/test-env-base";
 export const environment = {
   ...testEnvironmentBase,
   displayTag: "Local->Local",
-  publicApiUrl: "http://localhost:8083",
+  publicApiUrl: "http://localhost:8085",
   debug: true,
   gaId: "UA-116298798-4",
   gtmId: "GTM-NQ9XDTW",
