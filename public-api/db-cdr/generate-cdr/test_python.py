637cffb6e4dfea8bddcae241e75b9b1753f5c19b
Srushti Gangireddy <shrushigangireddy@NLDMIMX3N6D59RJ.local>
2024-02-27 14:40:19 -0600

    wip

--- public-api/db-cdr/generate-cdr/test_python.py
+++ public-api/db-cdr/generate-cdr/test_python.py
@@ -6,7 +6,7 @@ from google.cloud import bigquery
 from io import StringIO
 
 def init_bigquery_client():
-    bigquery_client = bigquery.Client(project="aou-db-prod")
+    bigquery_client = bigquery.Client(project="aou-res-curation-prod")
     return bigquery_client
 
 def main():
