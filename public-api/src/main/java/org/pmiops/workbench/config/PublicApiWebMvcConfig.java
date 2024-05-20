59e5b7d3eae52b6fa8b26e1e9f7a34b35066275c
Srushti Gangireddy <shrushigangireddy@NLDMIMX3N6D59RJ.local>
2024-05-20 17:29:47 -0500

    wip

--- public-api/src/main/java/org/pmiops/workbench/config/PublicApiWebMvcConfig.java
+++ public-api/src/main/java/org/pmiops/workbench/config/PublicApiWebMvcConfig.java
@@ -3,17 +3,27 @@ package org.pmiops.workbench.config;
 import org.pmiops.workbench.interceptors.ClearCdrVersionContextInterceptor;
 import org.pmiops.workbench.interceptors.CorsInterceptor;
 import org.pmiops.workbench.interceptors.SecurityHeadersInterceptor;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-public class PublicApiWebMvcConfig extends WebMvcConfigurer {
+public class PublicApiWebMvcConfig implements WebMvcConfigurer {
 
   @Autowired
   private CorsInterceptor corsInterceptor;
