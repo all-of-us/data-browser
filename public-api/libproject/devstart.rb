91baa234a8f461ee014e836fa1c5d1e5342f4177
Srushti Gangireddy <shrushigangireddy@NLDMIMX3N6D59RJ.local>
2024-06-11 10:25:31 -0500

    adding more logging

--- public-api/libproject/devstart.rb
+++ public-api/libproject/devstart.rb
@@ -212,6 +212,19 @@ Common.register_command({
 
 def run_local_public_api_tests()
   common = Common.new
+
+  require 'open3'
+
+  command = %W{
+    curl --silent --show-error --verbose --fail http://localhost:8083/
+  }
+
+  stdout_str, stderr_str, status = Open3.capture3(*command)
+
+  puts "Standard Output:\n#{stdout_str}"
+  puts "Error Output (Verbose Logging):\n#{stderr_str}"
+  puts "Exit Status: #{status.exitstatus}"
+
   status = common.capture_stdout %W{curl --silent --show-error -v --fail http://localhost:8083/}
   if status != 'AllOfUs Public API'
     common.error "Error probing public-api; received: #{status}"
