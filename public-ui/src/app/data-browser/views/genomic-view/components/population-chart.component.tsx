9c40b92c059e8d56d3bc73a6665ef4ab5a73694d
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2025-01-06 22:00:45 +0530

    [risk=no] [DB-1463] Update values category styling in Labs & Measurements (#1483)
    * Update values category styling in Labs & Measurements
    
    * adding in change Update genomic ring chart tooltip styling (SNV/Indel and SV sections)
    
    * adding in change Update genomic ring chart tooltip styling (SNV/Indel and SV sections)
    
    * adding in change Update genomic ring chart tooltip styling (SNV/Indel and SV sections)

--- public-ui/src/app/data-browser/views/genomic-view/components/population-chart.component.tsx
+++ public-ui/src/app/data-browser/views/genomic-view/components/population-chart.component.tsx
@@ -37,73 +37,77 @@ export class PopulationChartReactComponent extends React.Component<
     const newBaseOptions = getBaseOptions();
     newBaseOptions.chart.type = "pie";
     newBaseOptions.title.text =
-      '<div style=" color:#262262;text-align:center">PERCENTAGE <br/> OF ALLELES</div>';
+        '<div style="color:#262262;text-align:center">PERCENTAGE <br/> OF ALLELES</div>';
     newBaseOptions.title.verticalAlign = "middle";
     newBaseOptions.title.style = {
-      color: "black",
-      fontSize: "15px",
-      wordBreak: "break-word",
-      zIndex: 0,
-      fontFamily: "GothamBook",
-      fontWeight: "normal",
+          color: "black",
+          fontSize: "15px",
+          wordBreak: "break-word",
+          zIndex: 0,
+          fontFamily: "GothamBook",
+          fontWeight: "normal",
     };
     newBaseOptions.tooltip.outside = true;
     newBaseOptions.tooltip.style = {
-      color: "black",
-      whiteSpace: "normal",
-      zIndex: 9998,
+          color: "black",
+          whiteSpace: "nowrap", // Prevent wrapping in the tooltip
+          zIndex: 9998,
+    };
+    newBaseOptions.tooltip.useHTML = true; // Enable custom HTML in tooltips
+    newBaseOptions.tooltip.formatter = function () {
+        return this.point.toolTipHelpText;
     };
     const chartData = [];
     const totalAlleleCount = variantPopulationDetails.filter(
-      (v) => v.Ancestry === "Total"
-    )[0].AlleleCount;
+        (v) => v.Ancestry === "Total"
+        )[0].AlleleCount;
     for (const variantDet of variantPopulationDetails) {
-      if (variantDet.Ancestry !== "Total") {
-        const roundedPercentage = (
-          (variantDet.AlleleCount / totalAlleleCount) *
-          100
-        ).toFixed(2);
-        chartData.push({
-          name: variantDet.Ancestry,
-          y: variantDet.AlleleCount,
-          color: variantDet.color,
-          totalCount: totalAlleleCount,
-          percentage: roundedPercentage,
-          toolTipHelpText: this.getTooltipHelpText(
-            variantDet.Ancestry,
-            roundedPercentage,
-            variantDet.AlleleCount
-          ),
-        });
-      }
+        if (variantDet.Ancestry !== "Total") {
+            const roundedPercentage = (
+              (variantDet.AlleleCount / totalAlleleCount) *
+              100
+            ).toFixed(2);
+            chartData.push({
+              name: variantDet.Ancestry,
+              y: variantDet.AlleleCount,
+              color: variantDet.color,
+              totalCount: totalAlleleCount,
+              percentage: roundedPercentage,
+              toolTipHelpText: this.getTooltipHelpText(
+                variantDet.Ancestry,
+                roundedPercentage,
+                variantDet.AlleleCount
+              ),
+            });
+          }
     }
     newBaseOptions.series = [
-      {
-        name: "Alleles",
-        data: chartData,
-        size: "80%",
-        shadow: false,
-        innerSize: "60%",
-        showInLegend: true,
-        dataLabels: {
-          enabled: false,
-        },
-      },
+          {
+            name: "Alleles",
+            data: chartData,
+            size: "80%",
+            shadow: false,
+            innerSize: "60%",
+            showInLegend: true,
+            dataLabels: {
+              enabled: false,
+            },
+          },
     ];
     this.setState({ options: newBaseOptions });
   }
 
   getTooltipHelpText(name: string, percentage: any, count: number) {
-    return (
-      '<div class="pop-chart-tooltip">' +
-      "<strong>" +
-      name +
-      "</strong> <br /> " +
-      percentage +
-      " % | Allele Count: " +
-      count.toLocaleString() +
-      "</div>"
-    );
+        return (
+          '<div class="pop-chart-tooltip" style="white-space: nowrap; text-align: center;">' +
+          "<strong>" +
+          name +
+          "</strong><br />" +
+          percentage +
+          "% | AC: " +
+          count.toLocaleString() +
+          "</div>"
+        );
   }
 
   render() {
