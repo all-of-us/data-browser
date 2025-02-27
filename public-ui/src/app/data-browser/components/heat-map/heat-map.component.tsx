d00cb7dd9031ec7983299331ce6d133d4b36edb1
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2025-02-27 17:39:00 -0600

    territories now are grouped and hover as wanted

--- public-ui/src/app/data-browser/components/heat-map/heat-map.component.tsx
+++ public-ui/src/app/data-browser/components/heat-map/heat-map.component.tsx
@@ -5,31 +5,42 @@ import Highcharts from 'highcharts/highmaps';
 import HighchartsReact from "highcharts-react-official";
 import HighchartsMap from 'highcharts/modules/map';
 import mapData from '../../../../assets/maps/us_and_terr.json';
-import viMapData from '@highcharts/map-collection/countries/vi/vi-all.geo.json'
+// import mapData from '@highcharts/map-collection/countries/us/custom/us-all-territories.topo.json';
 
 HighchartsMap(Highcharts);
 interface Props {
     locationAnalysis: any
 }
 interface State {
-    options: any;
+
 
 }
 
 const style = `
 .highcharts-mapview-inset-border {
-  display: none;
+  display:none;
 }
-  .highcharts-name-district-of-columbia {
-  transform: scale(1051.3%);
-  transform-origin: 16px 47.9px;
-  }
 `
 
 
 export const HeatMapReactComponent =
     class extends React.Component<Props, State> {
         territories: string[];
+        domPathsByGroup = {
+            VI_KEYS: [],
+            NMI_KEYS: [],
+            AS_KEYS: [],
+            PR_KEYS: [],
+            GU_KEYS: []
+        };
+        chartRef = React.createRef<HighchartsReact.RefObject>();
+        path6399 = null;
+        path3617 = null;
+        path6398 = null;
+
+
+        static originalColor: string;
+
         constructor(props) {
             super(props);
             this.territories = [
@@ -40,166 +51,207 @@ export const HeatMapReactComponent =
                 'as-6514',
                 'as-6515',
                 'pr-3614',
-
             ]
+        }
 
-            const VI_KEYS = ['vi-6398', 'vi-3617', 'vi-6399'];
-            const NMI_KEYS= [  'mp-ti','mp-sa','mp-ro',]
-            this.state = {
-                options: {
-                    chart: {
-                        map: mapData,
-                        panning: true,
+        static keyGroups = {
+            VI_KEYS: ['vi-6398', 'vi-3617', 'vi-6399'],
+            NMI_KEYS: ['mp-ti', 'mp-sa', 'mp-ro'],
+            AS_KEYS: ['as-6514', 'as-6515'],
+            PR_KEYS: ['pr-3614'],
+            GU_KEYS: ['gu-3605']
+        };
 
+        options = {
+            chart: {
+                map: mapData,
+                panning: false,
+                animation: { duration: 10 }
+
+            },
+            colorAxis: {
+                stops: [
+                    [0, '#d0eafc'],  // Low value color 
+                    [1, '#006699']  // High value color 
+                ]
+            },
+            tooltip: {
+                formatter: function () {
+                    const tooltipText = `${this.point.name} <br> ${this.point.value} `
+                    return tooltipText;
+                }
+            },
+            title: {
+                text: undefined
+            },
+            mapNavigation: {
+                enabled: false,
+                buttonOptions: {
+                    verticalAlign: 'bottom'
+                }
+            },
+            plotOptions: {
+                series: {
+                    states: {
+                        hover: {
+                            enabled: false  // We'll do it manually 
+                        },
+                        inactive: {
+                            enabled: false, // Disable the inactive state globally
+                        },
                     },
-                    xAxis: {
-                        visible: false, // Completely hide the X-axis (ticks, line, labels, etc.)
-                    },
-                    yAxis: {
-                        visible: false, // Completely hide the X-axis (ticks, line, labels, etc.)
-                    },
-                    tooltip: {
-                        formatter: function () {
-                            const tooltipText = `${this.point.name} <br> ${this.point.value} `
-                            return tooltipText;
-                        }
-                    },
-                    title: {
-                        text: undefined
-                    },
-                    mapNavigation: {
-                        enabled: true,
-                        buttonOptions: {
-                            verticalAlign: 'bottom'
-                        }
-                    },
-                    colorAxis: {
-                        min: 0
-                    },
-                    plotOptions: {
-                        series: {
-                            states: {
-                                hover: {
-                                    enabled: false  // We'll do it manually
+                    point: {
+                        events: {
+                            mouseOver: function () {
+                                // Check if the key is in any of the key groups
+                                const matchedKey = Object.keys(HeatMapReactComponent.keyGroups).find(group => HeatMapReactComponent.keyGroups[group].includes(this['hc-key']));
+                                if (matchedKey) {
+                                    // If it is, save the original color
+                                    this.originalColor = this.color;
+                                } else {
+                                    // If it's not, change the color to the default hover color
+                                    this.update({ borderWidth: undefined, borderColor: undefined, color: '#262262' }, false);
+                                    this.series.chart.redraw();
                                 }
                             },
-                            point: {
-                                events: {
-                                    mouseOver: function () {
-                                        // If we just hovered over a VI polygon...
-                                        if (VI_KEYS.includes(this['hc-key'])) {
-                                            // ...then force all VI polygons into the hover state
-                                            VI_KEYS.forEach(key => {
-                                                const point = this.series.data.find(pt => pt['hc-key'] === key);
-                                                if (point) {
-                                                    // point.setState('hover');
-                                                    point.update(
-                                                        { borderWidth: 2, borderColor: 'black' },
-                                                        false // <- Don't redraw immediately
-                                                    );
-                                                }
-                                                this.series.chart.redraw();
-                                            });
-                                        }
-                                    },
-                                    mouseOut: function () {
-                                        // If we just left a VI polygon...
-                                        if (VI_KEYS.includes(this['hc-key'])) {
-                                            // ...then clear the hover state for all VI polygons
-                                            VI_KEYS.forEach(key => {
-                                                const point = this.series.data.find(pt => pt['hc-key'] === key);
-                                                if (point) {
-                                                    // point.setState('');
-                                                    point.update(
-                                                        { borderWidth: 0.5, borderColor: undefined },
-                                                        false
-                                                    );
-                                                }
-                                            });
-                                            this.series.chart.redraw();
-                                        }
-                                    }
+                            mouseOut: function () {
+                                const matchedKey = Object.keys(HeatMapReactComponent.keyGroups).find(group => HeatMapReactComponent.keyGroups[group].includes(this['hc-key']));
+                                if (matchedKey) {
+                                    HeatMapReactComponent.getOriginalColor(this.series, HeatMapReactComponent.keyGroups[matchedKey]);
+                                } else {
+                                    this.update({ borderWidth: undefined, borderColor: undefined, color: undefined }, true);
+                                    this.series.chart.redraw();
                                 }
                             }
-                        },
+                        }
+                    }
+                },
 
+            },
+            legend: {
+                enabled: false
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                data: this.formatLocationData(this.props.locationAnalysis.results),
+                borderColor: '#ffffff',
+                borderWidth: .1,
+                states: {
+                    hover: {
+                        color: '#BADA55'
+                    },
+                    inactive: {
+                        enabled: false, // Disable the inactive state
+                    },
+                },
+                dataLabels: {
+                    enabled: false,
+                    format: '{point.name}'
+                },
 
-                        states: {
-                            inactive: {
-                                enabled: false, // Disable the inactive state globally
-                            },
-                        },
-                    },
-                    legend: {
-                        enabled: false
-                    },
-                    credits: {
-                        enabled: false
-                    },
-                    scatter: {
-                        dataLabels: {
-                            enabled: false, // Disable globally for scatter plots
-                        },
-                    },
-                    series: [{
-                        data: this.formatLocationData(this.props.locationAnalysis.results),
-                        borderColor: null,
-                        states: {
-                            hover: {
-                                color: '#BADA55'
-                            },
-                            inactive: {
-                                enabled: false, // Disable the inactive state
-                            },
-                        },
-                        dataLabels: {
-                            enabled: false,
-                            format: '{point.name}'
-                        },
-                        borderWidth: .5
-                    },
-                    ]
-                }
-            };
+            },
+            ]
         }
 
-        chartRef = React.createRef();
 
         componentDidMount() {
-            if (this.chartRef.current) {
-                // this.chartRef.current.chart.reflow();
-                // console.log(this.chartRef.current,'wfjwofjwo');
+            // Save a reference to the Highcharts chart object
+            const chartObj = this.chartRef.current?.chart;
+            if (!chartObj) return;
 
-            }
+            const chartContainer = chartObj.container;
+
+            // For each group in keyGroups...
+            Object.keys(HeatMapReactComponent.keyGroups).forEach(groupName => {
+                const hcKeys = HeatMapReactComponent.keyGroups[groupName]; // e.g. ['vi-6398','vi-6399','vi-3617']
+
+                hcKeys.forEach(hcKey => {
+                    // Build a CSS selector. The "Highcharts map" paths typically have "highcharts-key-<hc-key>" as a class.
+                    // e.g. "path.highcharts-key-vi-6398"
+                    // We'll use this to find the paths in the DOM
+                    const selector = `path.highcharts-key-${hcKey}`;
+                    const foundPaths = chartContainer.querySelectorAll(selector);
+
+                    foundPaths.forEach(pathEl => {
+                        // Add to our store
+                        this.domPathsByGroup[groupName].push(pathEl);
+
+                        // Attach listeners. We'll use the same handler for all in this group:
+                        pathEl.addEventListener('mouseenter', () => this.handleMouseEnterGroup(groupName));
+                        pathEl.addEventListener('mouseleave', () => this.handleMouseLeaveGroup(groupName));
+                    });
+                });
+            });
+
         }
+        componentWillUnmount() {
+            // Loop over all groups
+            Object.keys(this.domPathsByGroup).forEach(groupName => {
+                const paths = this.domPathsByGroup[groupName];
+                paths.forEach(pathEl => {
+                    // Easiest is to replace them with clones that have no listeners:
+                    pathEl.replaceWith(pathEl.cloneNode(true));
+                });
+            });
+        }
+        
+        handleMouseEnterGroup(groupName) {
+            // Loop over all paths in the group and change their color
+            const pathsInGroup = this.domPathsByGroup[groupName];
+            pathsInGroup.forEach(pathEl => {
+                pathEl.style.fill = '#262262';
+            });
+        }
+        handleMouseLeaveGroup(groupName) {
+            const pathsInGroup = this.domPathsByGroup[groupName];
+            pathsInGroup.forEach(pathEl => {
+                // Reset to original or any color you wish
+                pathEl.style.fill = HeatMapReactComponent.originalColor;
+            });
+        }
+
+
         formatLocationData(data) {
             const output: any[] = [];
+            const keyGroups = {
+                'us-vi': { keys: HeatMapReactComponent.keyGroups.VI_KEYS, name: 'U.S. Virgin Islands' },
+                'us-mp': { keys: HeatMapReactComponent.keyGroups.NMI_KEYS, name: 'Northern Mariana Islands' },
+                'us-as': { keys: HeatMapReactComponent.keyGroups.AS_KEYS, name: 'American Samoa' },
+                'us-pr': { keys: HeatMapReactComponent.keyGroups.PR_KEYS, name: 'Puerto Rico' },
+                'us-gu': { keys: HeatMapReactComponent.keyGroups.GU_KEYS, name: 'Guam' },
+            };
 
             for (const item of data) {
-                if (item.stratum2 === 'us-vi') {
-                    // List only the three Virgin Islands keys you want to group
-                    const viKeys = ['vi-6398', 'vi-3617', 'vi-6399'];
-
-                    viKeys.forEach(territory => {
+                if (keyGroups[item.stratum2]) {
+                    keyGroups[item.stratum2].keys.forEach(territory => {
                         output.push([territory, item.countValue]);
-                        // Or, if you want a custom label:
-
                         output.push({
                             'hc-key': territory,
                             value: item.countValue,
-                            name: 'U.S. Virgin Islands'
+                            name: keyGroups[item.stratum2].name
                         });
-
                     });
-
                 } else {
                     // Normal flow for all other stratum2 codes
                     output.push([item.stratum2, item.countValue]);
                 }
             }
-
             return output;
         }
+        static getOriginalColor(series, keys: string[]) {
+            keys.forEach(key => {
+                const point = series.data.find(pt => pt['hc-key'] === key);
+                if (point) {
+                    this.originalColor = point.color;
+                }
+            });
+
+            // Now redraw once after updating all points
+            series.chart.redraw();
+        }
+
 
 
         render() {
@@ -209,9 +261,9 @@ export const HeatMapReactComponent =
                     <HighchartsReact
                         style={{ height: "50rem" }}
                         highcharts={Highcharts}
-                        options={this.state.options}
+                        options={this.options}
                         constructorType={'mapChart'}
-                    // ref={this.chartRef}
+                        ref={this.chartRef}
                     />
                 </div>
             </>
