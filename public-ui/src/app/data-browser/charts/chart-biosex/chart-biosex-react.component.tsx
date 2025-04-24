3aaf223927ba6254d25048f56cfaf5f2f9f48a4f
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2025-04-24 16:45:12 -0500

    changing the label

--- public-ui/src/app/data-browser/charts/chart-biosex/chart-biosex-react.component.tsx
+++ public-ui/src/app/data-browser/charts/chart-biosex/chart-biosex-react.component.tsx
@@ -23,6 +23,7 @@ interface Props {
 export class BioSexChartReactComponent extends React.Component<Props, State> {
   constructor(props) {
     super(props);
+    console.log(props);
     this.state = { options: null };
   }
 
@@ -95,7 +96,7 @@ export class BioSexChartReactComponent extends React.Component<Props, State> {
 
   getFitbitChartOptions() {
     const newBaseOptions = getBaseOptions();
-    newBaseOptions.chart.type = "bar";
+    newBaseOptions.chart.type = "column";
     newBaseOptions.plotOptions.series.pointWidth = 50;
     newBaseOptions.yAxis.title.text = "Participant Count";
     newBaseOptions.xAxis.title.text = "";
